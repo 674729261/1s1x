4c33b71e194ecae3df7ba6b03909187af4ae64f0
tracer-ysyx <tracer@ysyx.org>
2025-08-09 01:47:37 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:47:37 up  8:45,  1 user,  load average: 0.08, 0.05, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -128,7 +128,10 @@ void test_eval() {
                     3888},
                    {"(9--10+-11*-+12+(+13--14++15--(16++17*--18++19)))  "
                     "*(1--2++3--4++5--6++7--8)",
-                    9468}};
+                    19224},
+                   {"(9--10+-11*-+12+(+13--14++15--(16++17*--18++19)))  "
+                    "/    (1--2++3--4++5--6++7--8)",
+                    19224}};
   for (int i = 0; i < sizeof(testcases) / sizeof(testcases[0]); i++) {
     bool success = true;
     long long result = expr(testcases[i].input, &success);
