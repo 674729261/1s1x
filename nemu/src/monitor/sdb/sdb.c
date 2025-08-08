81f5728c3bb5c39c6e612438c138284abf141196
tracer-ysyx <tracer@ysyx.org>
2025-08-08 18:16:12 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:16:12 up  1:14,  1 user,  load average: 0.08, 0.02, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,7 +70,7 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args) {
   char *parameter = strtok(NULL, " ");
   if (parameter == NULL) {
-    printf("Expecting parameter :\n'r' for registers\n 'w' for watcher\n");
+    printf("Expecting parameter :\n'r' for registers\n'w' for watcher\n");
     return 0;
   }
 
