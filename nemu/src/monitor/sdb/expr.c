587545bc13761980f92e0e7e3555507c0baef012
tracer-ysyx <tracer@ysyx.org>
2025-08-11 20:28:38 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:28:38 up 57 min,  1 user,  load average: 0.26, 0.11, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -211,12 +211,8 @@ int find_main_token(int p, int q, TokenST *st) {
   // 非运算符不选
   // 先选优先级低的
   // 先选靠右的
-  for (int i = p; i <= q; i++)
-    printf("%s", tokens[i].str);
-  putchar('\n');
   int ret = query_ST(st, p, q);
   int cur_layer = tokens[p].layer;
-  printf("%d %d\n", ret, cur_layer);
   if (ret == -1 || cur_layer != tokens[ret].layer)
     return -1;
   return ret;
