f370d701122fc005881162564ffc273a636ad19d
tracer-ysyx <tracer@ysyx.org>
2025-08-11 22:32:06 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:32:06 up  3:01,  1 user,  load average: 0.04, 0.05, 0.04

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -13,6 +13,7 @@
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
+#include "sdb/ST.h"
 #include "sdb/sdb.h"
 #include <debug.h>
 #include <isa.h>
@@ -219,6 +220,8 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize the simple debugger. */
   init_sdb();
+
+  pre_log2();
   unit_tests();
   IFDEF(CONFIG_ITRACE, init_disasm());
 
