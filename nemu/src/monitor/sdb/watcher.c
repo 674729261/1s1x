c7754b7493b7afb2ef462c96a9b76031008646a9
tracer-ysyx <tracer@ysyx.org>
2025-08-12 23:04:59 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:04:59 up 18 min,  1 user,  load average: 0.09, 0.02, 0.01

--- nemu/src/monitor/sdb/watcher.c
+++ nemu/src/monitor/sdb/watcher.c
@@ -109,7 +109,7 @@ bool exam_watchers(void) {
       printf("Watcher changed : #%d = ", iter->NO);
       show_expr(iter->tokens, iter->st.n);
       if (iter->is_hex)
-        printf("\nfrom : 0x%llx\nto   : %lld\n", iter->old_value, now_value);
+        printf("\nfrom : 0x%llx\nto   : %llx\n", iter->old_value, now_value);
       else
         printf("\nfrom : %lld\nto   : %lld\n", iter->old_value, now_value);
       iter->old_value = now_value;
