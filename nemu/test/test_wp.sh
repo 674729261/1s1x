2a5b410f880865655f119d58b080c15c228a3e31
tracer-ysyx <tracer@ysyx.org>
2025-08-15 00:16:59 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:16:59 up  2:43,  1 user,  load average: 0.20, 0.20, 0.14

--- nemu/test/test_wp.sh
+++ nemu/test/test_wp.sh
@@ -1,2 +1,5 @@
 make
 cat ./test/test_wp.tcl | ./build/riscv32-nemu-interpreter --log=./build/nemu-log.txt ./test/test_img.bin
+TIME1=$(cat ./build/nemu-log.txt | grep -o -P "\d+(?= inst/s)")
+
+echo "速度为${TIME1} inst/s"
