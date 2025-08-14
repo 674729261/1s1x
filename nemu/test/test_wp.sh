f6318f08acd2666c1974a06c25efc586ffafd73c
1s1x100016241-Wang Xiangyu <2292683261@qq.com>
2025-08-15 01:16:31 +0800

    better test_wp.sh

--- nemu/test/test_wp.sh
+++ nemu/test/test_wp.sh
@@ -1,5 +1,7 @@
 make
 cat ./test/test_wp.tcl | ./build/riscv32-nemu-interpreter --log=./build/nemu-log.txt ./test/test_img.bin
 TIME1=$(cat ./build/nemu-log.txt | grep -o -P "\d+(?= inst/s)")
-
-echo "速度为${TIME1} inst/s"
+echo -e "c\nq" | ./build/riscv32-nemu-interpreter --log=./build/nemu-log.txt ./test/test_img.bin
+TIME2=$(cat ./build/nemu-log.txt | grep -o -P "\d+(?= inst/s)")
+echo "无监视点时速度为${TIME2} inst/s"
+echo "有监视点时速度为${TIME1} inst/s"
