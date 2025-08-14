40adcae6986893b596663f254630a995cae301d8
tracer-ysyx <tracer@ysyx.org>
2025-08-14 22:33:25 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:33:25 up  1:00,  1 user,  load average: 0.19, 0.11, 0.07

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -15,6 +15,7 @@
 
 #include "sdb.h"
 #include "common.h"
+#include "watcher.h"
 #include <cpu/cpu.h>
 #include <debug.h>
 #include <errno.h>
@@ -31,6 +32,7 @@
 static int is_batch_mode = false;
 
 void init_regex();
+void free_regex();
 void init_wp_pool();
 
 /* We use the `readline' library to provide more flexibility to read from stdin.
@@ -321,4 +323,6 @@ void init_sdb() {
 
   /* Initialize the watchpoint pool. */
   init_wp_pool();
+
+  free_regex();
 }
