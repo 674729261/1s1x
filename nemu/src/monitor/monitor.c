0cae3581c0ba4e41543a4a8b222b37cc2ddabf9d
tracer-ysyx <tracer@ysyx.org>
2025-08-15 22:45:55 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:45:55 up  4:59,  1 user,  load average: 0.14, 0.07, 0.01

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -13,9 +13,7 @@
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
-#include "sdb/ST.h"
 #include "sdb/sdb.h"
-#include "sdb/watcher.h"
 #include <debug.h>
 #include <isa.h>
 #include <memory/paddr.h>
@@ -122,7 +120,7 @@ static int parse_args(int argc, char *argv[]) {
 void test_eval() {
 
   struct {
-    char input[128];
+    const char *input;
     long long result;
   } testcases[] = {{"1 +    2 *  3", 7},
                    {"(1 +    2 ) *  (3+4)", 21},
@@ -152,8 +150,8 @@ void test_eval() {
 
   FILE *fp = fopen("test.txt", "r");
   clock_t begin = clock();
-
   if (fp != NULL) {
+
     while (!feof(fp)) {
       long long result;
       char *expression = NULL;
@@ -221,7 +219,6 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize the simple debugger. */
   init_sdb();
-
   unit_tests();
   IFDEF(CONFIG_ITRACE, init_disasm());
 
