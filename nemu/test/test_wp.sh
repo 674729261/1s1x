58be834c7090e92bc9fbab4836eb541ed1a9365a
tracer-ysyx <tracer@ysyx.org>
2025-08-15 18:05:05 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:05:05 up 18 min,  1 user,  load average: 0.02, 0.05, 0.06

--- nemu/test/test_wp.sh
+++ nemu/test/test_wp.sh
@@ -4,4 +4,5 @@ TIME1=$(cat ./build/nemu-log.txt | grep -o -P "\d+(?= inst/s)")
 echo -e "c\nq" | ./build/riscv32-nemu-interpreter --log=./build/nemu-log.txt ./test/test_img.bin
 TIME2=$(cat ./build/nemu-log.txt | grep -o -P "\d+(?= inst/s)")
 echo "无监视点时速度为${TIME2} inst/s"
-echo "有监视点时速度为${TIME1} inst/s, 是无监视点时的0.$(($TIME1 * 100 / $TIME2))倍"
+ratio=$(echo "$TIME1 / $TIME2" | bc -l)
+echo "有监视点时速度为${TIME1} inst/s, 是无监视点时的${ratio}倍"
