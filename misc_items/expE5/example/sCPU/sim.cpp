9044102f7205b027ed9847936320df28a8f57529
tracer-ysyx <tracer@ysyx.org>
2025-08-15 22:46:10 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:46:10 up  4:59,  1 user,  load average: 0.28, 0.10, 0.02

--- misc_items/expE5/example/sCPU/sim.cpp
+++ misc_items/expE5/example/sCPU/sim.cpp
@@ -5,7 +5,6 @@
 #include <cstdint>
 #include <cstdio>
 #include <nvboard.h>
-#include <random>
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME *top);
@@ -38,6 +37,8 @@ int main() {
     auto now = std::chrono::steady_clock::now();
     if (now - last > std::chrono::milliseconds(200)) {
       last = now;
+      puts("sCPU  使能 : SW0  复位 : 中");
+
       printf("PC = %02X ", dut.rootp->sCPU__DOT__pc);
       printf("GPR = %4d %4d %4d %4d\n",
              dut.rootp->sCPU__DOT__sCPU_GPR__DOT__GPRdata[0],
