6793a4f936609e849c1c3e4130251c5eac2f4ed9
1s1x100016241-Wang Xiangyu <2292683261@qq.com>
2025-08-15 22:47:20 +0800

    small modification 2

--- misc_items/expE5/example/sCPU/sim.cpp
+++ misc_items/expE5/example/sCPU/sim.cpp
@@ -37,6 +37,8 @@ int main() {
     auto now = std::chrono::steady_clock::now();
     if (now - last > std::chrono::milliseconds(200)) {
       last = now;
+      puts("sCPU  使能 : SW0  复位 : 中");
+
       printf("PC = %02X ", dut.rootp->sCPU__DOT__pc);
       printf("GPR = %4d %4d %4d %4d\n",
              dut.rootp->sCPU__DOT__sCPU_GPR__DOT__GPRdata[0],
