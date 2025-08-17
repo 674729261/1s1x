03701f51cd79a6b21ec23a7d8a1b5bd18827f63a
tracer-ysyx <tracer@ysyx.org>
2025-08-18 01:37:55 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:37:55 up  3:09,  1 user,  load average: 0.22, 0.15, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -141,6 +141,9 @@ static int decode_exec(Decode *s) {
               BITS((int64_t)(sword_t)src1 * (int64_t)(sword_t)src2, 63, 32));
   INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu, R,
           R(rd) = BITS((uint64_t)src1 * (uint64_t)src2, 63, 32));
+  INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu, R,
+          if (src2 == 0) R(rd) = src1;
+          else R(rd) = src1 % src2);
   INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem, R,
           if (src2 == 0) R(rd) = src1;
           else if (src2 == -1) R(rd) = 0;
