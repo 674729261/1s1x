e3350ccbe4fae5114bd1b54effa2f973cb1e04cb
tracer-ysyx <tracer@ysyx.org>
2025-08-17 23:11:11 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:11:11 up 42 min,  1 user,  load average: 0.01, 0.03, 0.00

--- abstract-machine/am/src/platform/logisim/trm.c
+++ abstract-machine/am/src/platform/logisim/trm.c
@@ -6,13 +6,11 @@ int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 
-void putch(char ch) {
-  outb(0x10000000, ch);
-}
+void putch(char ch) { outb(0x10000000, ch); }
 
-__attribute__((noinline))
-void halt(int code) {
-  while (1);
+__attribute__((noinline)) void halt(int code) {
+  while (1)
+    ;
 }
 
 void _trm_init() {
