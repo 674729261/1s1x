c0092a66d64d3e7f376c7b5dfc65cec51e38107c
tracer-ysyx <tracer@ysyx.org>
2025-08-17 13:47:53 +0800

    > compile NEMU ysyx_100016241 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:47:53 up  2:17,  1 user,  load average: 0.12, 0.07, 0.03

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -83,6 +83,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
     src2R();
     immS();
     break;
+  case TYPE_R:
+    src1R();
+    src2R();
     break;
   case TYPE_N:
     break;
