2845cb59120e21854a32da8789feaba1ca3524f2
tracer-ysyx <tracer@ysyx.org>
2025-08-23 02:58:33 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:58:33 up 10:29,  1 user,  load average: 0.29, 0.43, 0.35

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -36,7 +36,6 @@ extern "C" int pmem_read(int raddr) {
   return last = M[pos];
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-  printf("Save %d %d %x\n", waddr, wdata, wmask);
   for (int i = 0; i < 4; i++) {
     if ((wmask >> i) & 0x1) {
       uint32_t mask32 =
@@ -46,7 +45,6 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
       M[addr] |= wdata & mask32;
     }
   }
-  printf("%d\n", M[(uint32_t)waddr >> 2]);
 }
 
 int main(int argc, char **argv) {
