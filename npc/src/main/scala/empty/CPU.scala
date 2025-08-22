e10ab9ef0939928fc1834b98392d5243696f97e1
tracer-ysyx <tracer@ysyx.org>
2025-08-22 21:41:51 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:41:51 up  5:13,  1 user,  load average: 0.50, 0.75, 0.76

--- npc/src/main/scala/empty/CPU.scala
+++ npc/src/main/scala/empty/CPU.scala
@@ -32,7 +32,8 @@ class CPU extends Module with RequireAsyncReset {
   val adder = Module(new Adder(32))
   dynamic_pc_next := Mux(is_jalr, adder.io.out, static_pc_next)
 
-  val i_decoded = io.instr.asTypeOf(new IType)
+  val i_decoded = Wire(new IType)
+  i_decoded := io.instr.asTypeOf(new IType)
   printf(p"${i_decoded.opcode} ${i_decoded.imm12}\n")
   val imm32_I = signExt32(i_decoded.imm12)
 
