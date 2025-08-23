8a18d22a026a490b9c6547a44de8ce4cd1a2c282
tracer-ysyx <tracer@ysyx.org>
2025-08-23 12:03:00 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:03:00 up 26 min,  1 user,  load average: 0.06, 0.28, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -77,9 +77,9 @@ int main(int argc, char **argv) {
 
     dut.eval();
     if (dut.io_ebreak) {
-      printf("inst = %07x, EBREAK, a0 = %08x, pc = %08x, cycle = %d\n",
-             M[pc / 4], dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_9_r,
-             pc, cur_cycle);
+      printf("EBREAK, a0 = %08x, pc = %08x, cycle = %d\n",
+             dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_9_r, pc,
+             cur_cycle);
       break;
     }
   }
