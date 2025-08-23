c2472028aaa1a334a2af299f117666a32a115fcf
tracer-ysyx <tracer@ysyx.org>
2025-08-23 11:40:38 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:40:38 up 3 min,  1 user,  load average: 0.26, 0.26, 0.11

--- npc/vsrc/Memory.sv
+++ npc/vsrc/Memory.sv
@@ -18,7 +18,7 @@ module Memory (
   input [31:0] waddr, raddr, wdata;
   input [3:0] wmask;
   output reg [31:0] rdata;
-  always @(*) begin
+  always @(clk, raddr) begin
     if (valid) begin  // 有读写请求时
       rdata = pmem_read(raddr);
     end else begin
@@ -26,9 +26,9 @@ module Memory (
     end
   end
 
-    always @(posedge clk) begin
+  always @(posedge clk) begin
     if (valid & wen) begin
-        pmem_write(waddr, wdata, {4'h0, wmask});
-      end
+      pmem_write(waddr, wdata, {4'h0, wmask});
+    end
   end
 endmodule
