8240ccdd730e56d52127b353913b398189112b66
tracer-ysyx <tracer@ysyx.org>
2025-08-23 17:21:08 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:21:08 up  5:44,  1 user,  load average: 0.57, 0.46, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -30,16 +30,16 @@ void design_init() {
 int trapped;
 extern "C" void trap(int signal) { trapped = signal; }
 extern "C" int pmem_read(int raddr) {
-  uint32_t pos = (uint32_t)(raddr - 0x80000000u) >> 2;
+  uint32_t pos = (uint32_t)(raddr - 0x00000000u) >> 2;
   if (pos > Memory_Size)
     return 0xdeadbeef;
   return M[pos];
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-  if (waddr < 0x80000000u) {
-    printf("waddr : %08x out of range\n", waddr);
-    exit(-1);
-  }
+  // if (waddr < 0x80000000u) {
+  //   printf("waddr : %08x out of range\n", waddr);
+  //   exit(-1);
+  // }
   for (int i = 0; i < 4; i++) {
     if ((wmask >> i) & 0x1) {
       uint32_t mask32 =
@@ -77,10 +77,10 @@ int main(int argc, char **argv) {
   unsigned int cur_cycle;
   for (cur_cycle = 0; cur_cycle < max_cycle; cur_cycle++) {
     uint32_t pc = dut.io_pc;
-    if (pc < 0x80000000u) {
-      printf("pc : %08x out of range\n", pc);
-      exit(-1);
-    }
+    // if (pc < 0x80000000u) {
+    //   printf("pc : %08x out of range\n", pc);
+    //   exit(-1);
+    // }
     dut.io_instr = M[(pc - 0x80000000u) / 4];
     dut.clock = 0;
     dut.eval();
