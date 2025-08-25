20d4df9a880d9d690d203450c4f880eecbd657ea
tracer-ysyx <tracer@ysyx.org>
2025-08-25 17:08:49 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:08:49 up 27 min,  1 user,  load average: 0.13, 0.06, 0.05

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,9 +3,10 @@
 #include <klib.h>
 #include <limits.h>
 #include <stdarg.h>
+#include <stddef.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-typedef char *out_ch_func(char *, char);
+typedef char *out_ch_func(char *, char, char *const begin, size_t max_len);
 
 enum FLAGS_PRINTF {
   FLAGS_ZEROPAD = (1U << 0U),
@@ -22,22 +23,23 @@ enum FLAGS_PRINTF {
 const static size_t Buffer_Size = 32u;
 
 static size_t _out_rev(out_ch_func out, char **buffer, const char *buf,
-                       size_t len, unsigned int width, unsigned int flags) {
+                       size_t len, unsigned int width, unsigned int flags,
+                       char *const begin, size_t max_len) {
   int ret = 0;
   if (!(flags & FLAGS_LEFT) && !(flags & FLAGS_ZEROPAD)) {
     for (size_t i = len; i < width; i++) {
-      *buffer = out(*buffer, ' ');
+      *buffer = out(*buffer, ' ', begin, max_len);
       ret++;
     }
   }
 
   for (size_t i = len; i > 0; i--) {
-    *buffer = out(*buffer, buf[i - 1]);
+    *buffer = out(*buffer, buf[i - 1], begin, max_len);
     ret++;
   }
   if (flags & FLAGS_LEFT) {
     for (int i = 0; i < width - len; i++) {
-      *buffer = out(*buffer, ' ');
+      *buffer = out(*buffer, ' ', begin, max_len);
       ret++;
     }
   }
@@ -47,7 +49,8 @@ static size_t _out_rev(out_ch_func out, char **buffer, const char *buf,
 
 static size_t _ntoa_format(out_ch_func out, char **buffer, char *buf,
                            size_t len, bool negative, unsigned int base,
-                           unsigned int width, unsigned int flags) {
+                           unsigned int width, unsigned int flags,
+                           char *const begin, size_t max_len) {
   if (!(flags & FLAGS_LEFT)) {
     if (width && (flags & FLAGS_ZEROPAD) &&
         (negative || (flags & (FLAGS_PLUS | FLAGS_SPACE)))) {
@@ -88,7 +91,7 @@ static size_t _ntoa_format(out_ch_func out, char **buffer, char *buf,
     }
   }
 
-  return _out_rev(out, buffer, buf, len, width, flags);
+  return _out_rev(out, buffer, buf, len, width, flags, begin, max_len);
 }
 
 static inline bool _is_digit(char ch) { return (ch >= '0') && (ch <= '9'); }
@@ -101,7 +104,8 @@ static unsigned int _atoi(const char **str) {
 }
 static size_t _ntoa_long(out_ch_func out, char **buffer, unsigned long value,
                          bool negative, unsigned long base, unsigned int width,
-                         unsigned int flags) {
+                         unsigned int flags, char *const begin,
+                         size_t max_len) {
 
   char buf[Buffer_Size];
   size_t len = 0U;
@@ -119,7 +123,7 @@ static size_t _ntoa_long(out_ch_func out, char **buffer, unsigned long value,
   } while (value && (len < Buffer_Size));
 
   return _ntoa_format(out, buffer, buf, len, negative, (unsigned int)base,
-                      width, flags);
+                      width, flags, begin, max_len);
 }
 
 static inline unsigned int _strnlen_s(const char *str, size_t maxsize) {
@@ -132,7 +136,8 @@ static inline unsigned int _strnlen_s(const char *str, size_t maxsize) {
 static size_t _ntoa_long_long(out_ch_func out, char **buffer,
                               unsigned long long value, bool negative,
                               unsigned long long base, unsigned int width,
-                              unsigned int flags) {
+                              unsigned int flags, char *const begin,
+                              size_t max_len) {
   char buf[Buffer_Size];
   size_t len = 0U;
 
@@ -149,14 +154,16 @@ static size_t _ntoa_long_long(out_ch_func out, char **buffer,
   } while (value && (len < Buffer_Size));
 
   return _ntoa_format(out, buffer, buf, len, negative, (unsigned int)base,
-                      width, flags);
+                      width, flags, begin, max_len);
 }
 
-int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
+int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va,
+                size_t max_len) {
   int ret_idx = 0;
+  char *const begin = buffer;
   while (*format) {
     if (*format != '%') {
-      buffer = out(buffer, *format);
+      buffer = out(buffer, *format, buffer, max_len);
       ret_idx++;
       format++;
       continue;
@@ -260,37 +267,38 @@ int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
           const long long value = va_arg(va, long long);
           ret_idx += _ntoa_long_long(
               out, &buffer, (unsigned long long)(value > 0 ? value : 0 - value),
-              value < 0, base, width, flags);
+              value < 0, base, width, flags, begin, max_len);
 
         } else if (flags & FLAGS_LONG) {
           const long value = va_arg(va, long);
           ret_idx += _ntoa_long(out, &buffer,
                                 (unsigned long)(value > 0 ? value : 0 - value),
-                                value < 0, base, width, flags);
+                                value < 0, base, width, flags, begin, max_len);
         } else {
           const int value = (flags & FLAGS_CHAR)    ? (char)va_arg(va, int)
                             : (flags & FLAGS_SHORT) ? (short int)va_arg(va, int)
                                                     : va_arg(va, int);
           ret_idx += _ntoa_long(out, &buffer,
                                 (unsigned int)(value > 0 ? value : 0 - value),
-                                value < 0, base, width, flags);
+                                value < 0, base, width, flags, begin, max_len);
         }
       } else {
         if (flags & FLAGS_LONG_LONG) {
           ret_idx +=
               _ntoa_long_long(out, &buffer, va_arg(va, unsigned long long),
-                              false, base, width, flags);
+                              false, base, width, flags, begin, max_len);
 
         } else if (flags & FLAGS_LONG) {
           ret_idx += _ntoa_long(out, &buffer, va_arg(va, unsigned long), false,
-                                base, width, flags);
+                                base, width, flags, begin, max_len);
         } else {
           const unsigned int value =
               (flags & FLAGS_CHAR) ? (unsigned char)va_arg(va, unsigned int)
               : (flags & FLAGS_SHORT)
                   ? (unsigned short int)va_arg(va, unsigned int)
                   : va_arg(va, unsigned int);
-          ret_idx += _ntoa_long(out, &buffer, value, false, base, width, flags);
+          ret_idx += _ntoa_long(out, &buffer, value, false, base, width, flags,
+                                begin, max_len);
         }
       }
       format++;
@@ -300,15 +308,15 @@ int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
       unsigned int l = 1U;
       if (!(flags & FLAGS_LEFT)) {
         while (l++ < width) {
-          buffer = out(buffer, ' ');
+          buffer = out(buffer, ' ', begin, max_len);
           ret_idx++;
         }
       }
-      buffer = out(buffer, (char)va_arg(va, int));
+      buffer = out(buffer, (char)va_arg(va, int), begin, max_len);
       ret_idx++;
       if (flags & FLAGS_LEFT) {
         while (l++ < width) {
-          buffer = out(buffer, ' ');
+          buffer = out(buffer, ' ', begin, max_len);
           ret_idx++;
         }
       }
@@ -321,17 +329,17 @@ int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
       unsigned int l = _strnlen_s(p, (size_t)-1);
       if (!(flags & FLAGS_LEFT)) {
         while (l++ < width) {
-          buffer = out(buffer, ' ');
+          buffer = out(buffer, ' ', begin, max_len);
           ret_idx++;
         }
       }
       while (*p != 0) {
-        buffer = out(buffer, *(p++));
+        buffer = out(buffer, *(p++), begin, max_len);
         ret_idx++;
       }
       if (flags & FLAGS_LEFT) {
         while (l++ < width) {
-          buffer = out(buffer, ' ');
+          buffer = out(buffer, ' ', begin, max_len);
           ret_idx++;
         }
       }
@@ -345,12 +353,12 @@ int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
       const bool is_ll = sizeof(uintptr_t) == sizeof(long long);
       if (is_ll) {
         ret_idx += _ntoa_long_long(out, &buffer, (uintptr_t)va_arg(va, void *),
-                                   false, 16U, width, flags);
+                                   false, 16U, width, flags, begin, max_len);
       } else {
 
         ret_idx += _ntoa_long(out, &buffer,
                               (unsigned long)((uintptr_t)va_arg(va, void *)),
-                              false, 16U, width, flags);
+                              false, 16U, width, flags, begin, max_len);
       }
 
       format++;
@@ -358,22 +366,25 @@ int __vasprintf(out_ch_func out, char *buffer, const char *format, va_list va) {
     }
 
     case '%':
-      buffer = out(buffer, '%');
+      buffer = out(buffer, '%', begin, max_len);
       ret_idx++;
       format++;
       break;
 
     default:
-      buffer = out(buffer, *format);
+      buffer = out(buffer, *format, begin, max_len);
       ret_idx++;
       format++;
       break;
     }
   }
-  buffer = out(buffer, '\0');
+  buffer = out(buffer, '\0', begin, max_len);
   return ret_idx;
 }
-static char *update_to_str(char *addr, char c) {
+static char *update_to_str(char *addr, char c, char *const begin,
+                           size_t max_len) {
+  if (c != '\0' && addr - begin >= max_len)
+    return addr;
   *addr = c;
   return addr + 1;
 }
@@ -381,7 +392,7 @@ static char *update_to_str(char *addr, char c) {
 int sprintf(char *out, const char *fmt, ...) {
   va_list argp;
   va_start(argp, fmt);
-  return __vasprintf(update_to_str, out, fmt, argp);
+  return __vasprintf(update_to_str, out, fmt, argp, -1);
   va_end(argp);
 }
 
@@ -393,11 +404,6 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-static char *update_to_serial(char *addr, char c) {
-  *(volatile char *)addr = c;
-  return addr;
-}
-
 #if defined(__ARCH_X86_NEMU)
 #define DEVICE_BASE 0x0
 #else
@@ -407,11 +413,15 @@ static char *update_to_serial(char *addr, char c) {
 #define MMIO_BASE 0xa0000000
 
 #define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
+static char *update_to_serial(char *addr, char c, char *begin, size_t max_len) {
+  *(volatile char *)addr = c;
+  return addr;
+}
 
 int printf(const char *fmt, ...) {
   va_list argp;
   va_start(argp, fmt);
-  return __vasprintf(update_to_serial, (char *)SERIAL_PORT, fmt, argp);
+  return __vasprintf(update_to_serial, (char *)SERIAL_PORT, fmt, argp, -1);
   va_end(argp);
 }
 
