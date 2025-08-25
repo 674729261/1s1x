d3d605b1955e630535490f135a970cf0cf83da62
ysyx_25080216-Wang Xiangyu <2292683261@qq.com>
2025-08-25 17:11:25 +0800

    implemented some printf

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,21 @@
+#include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
-void __am_timer_init() {
-}
+void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t lower = inl(RTC_ADDR);
+  uint32_t upper = inl(RTC_ADDR + 0x4);
+  uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
