7c69ce8669486464b00eccdf2801472e429aaa0b
tracer-ysyx <tracer@ysyx.org>
2025-08-25 12:45:34 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:45:34 up 55 min,  1 user,  load average: 0.29, 0.12, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,12 +1,14 @@
 #include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  inl(RTC_ADDR);
-  uptime->us = 0;
+  uint32_t lower = inl(RTC_ADDR);
+  uint32_t upper = inl(RTC_ADDR + 0x4);
+  uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -40,7 +40,6 @@ static void timer_intr() {
 
 void init_timer() {
   rtc_port_base = (uint32_t *)new_space(8);
-  printf("RTC : %p\n", rtc_port_base);
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
