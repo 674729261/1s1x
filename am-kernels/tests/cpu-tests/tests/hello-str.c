f7446982f8216e8066a696858ceaa154c8319351
tracer-ysyx <tracer@ysyx.org>
2025-08-25 16:54:29 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:54:29 up 12 min,  1 user,  load average: 0.03, 0.09, 0.07

--- am-kernels/tests/cpu-tests/tests/hello-str.c
+++ am-kernels/tests/cpu-tests/tests/hello-str.c
@@ -6,7 +6,7 @@ int _main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	check(strcmp(buf, "Hello world!\n") == 0);
 
-	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	printf("%d + %d = %d\n", 1, 1, 2);
 	check(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
