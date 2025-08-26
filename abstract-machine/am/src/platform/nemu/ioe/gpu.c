169a334d74dad74ac0f64766e1ac09e9b59a8bf9
tracer-ysyx <tracer@ysyx.org>
2025-08-27 00:09:30 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:09:30 up  4:46,  1 user,  load average: 0.36, 0.24, 0.19

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,9 +27,9 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  for (int i = 0; i < ctl->w + ctl->h; i++) {
-    outl(FB_ADDR, ((uint32_t *)ctl->pixels)[i]);
-  }
+  // for (int i = 0; i < ctl->w + ctl->h; i++) {
+  //   outl(FB_ADDR, ((uint32_t *)ctl->pixels)[i]);
+  // }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
