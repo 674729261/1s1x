fc1e36cd27f4b9556046d25152a38117b1483831
tracer-ysyx <tracer@ysyx.org>
2025-08-26 20:53:13 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:53:13 up  1:29,  1 user,  load average: 0.82, 0.34, 0.19

--- abstract-machine/am/src/native/platform.h
+++ abstract-machine/am/src/native/platform.h
@@ -1,23 +1,22 @@
 #ifndef __PLATFORM_H__
 #define __PLATFORM_H__
-
 #include <am.h>
-#include <unistd.h>
-#include <signal.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
+#include <signal.h>
+#include <unistd.h>
 
 #ifdef __x86_64__
-#define AM_REG_PC(uc)   (uc)->uc_mcontext.gregs[REG_RIP]
-#define AM_REG_SP(uc)   (uc)->uc_mcontext.gregs[REG_RSP]
+#define AM_REG_PC(uc) (uc)->uc_mcontext.gregs[REG_RIP]
+#define AM_REG_SP(uc) (uc)->uc_mcontext.gregs[REG_RSP]
 #define AM_REG_GPR1(uc) (uc)->uc_mcontext.gregs[REG_RDI]
 #elif defined(__aarch64__)
-#define AM_REG_PC(uc)   (uc)->uc_mcontext.pc
-#define AM_REG_SP(uc)   (uc)->uc_mcontext.sp
+#define AM_REG_PC(uc) (uc)->uc_mcontext.pc
+#define AM_REG_SP(uc) (uc)->uc_mcontext.sp
 #define AM_REG_GPR1(uc) (uc)->uc_mcontext.regs[0]
 #elif defined(__riscv) && __riscv_xlen == 64
-#define AM_REG_PC(uc)   (uc)->uc_mcontext.__gregs[REG_PC]
-#define AM_REG_SP(uc)   (uc)->uc_mcontext.__gregs[REG_SP]
+#define AM_REG_PC(uc) (uc)->uc_mcontext.__gregs[REG_PC]
+#define AM_REG_SP(uc) (uc)->uc_mcontext.__gregs[REG_SP]
 #define AM_REG_GPR1(uc) (uc)->uc_mcontext.__gregs[REG_A0]
 #else
 #error Unsupported architecture
