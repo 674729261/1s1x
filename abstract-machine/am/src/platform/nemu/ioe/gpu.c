cf77f513da77e8029b153f1229c1ee4ea9e51d57
tracer-ysyx <tracer@ysyx.org>
2025-08-27 00:06:49 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:06:49 up  4:43,  1 user,  load average: 0.19, 0.20, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,25 +1,38 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t ctl_lower = inl(VGACTL_ADDR);
+  int w = ctl_lower >> 16;
+  int h = ctl_lower & 0xFFFF;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t ctl_lower = inl(VGACTL_ADDR);
+  int w = ctl_lower >> 16;
+  int h = ctl_lower & 0xFFFF;
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = w,
+                           .height = h,
+                           .vmemsz = w * h * sizeof(uint32_t)};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  for (int i = 0; i < ctl->w + ctl->h; i++) {
+    outl(FB_ADDR, ((uint32_t *)ctl->pixels)[i]);
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
