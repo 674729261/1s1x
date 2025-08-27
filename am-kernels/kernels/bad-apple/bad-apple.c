34ec2e5d9f5ddc0f0cc3dead906bab4729f82446
tracer-ysyx <tracer@ysyx.org>
2025-08-27 13:34:52 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:34:52 up  1:46,  1 user,  load average: 9.01, 4.06, 1.75

--- am-kernels/kernels/bad-apple/bad-apple.c
+++ am-kernels/kernels/bad-apple/bad-apple.c
@@ -56,8 +56,7 @@ int main() {
     }
 
     if (has_audio) {
-      int should_play = 0xFFF; //(AUDIO_FREQ / FPS) * sizeof(int16_t)
-                               //* AUDIO_CHANNEL;
+      int should_play = (AUDIO_FREQ / FPS) * sizeof(int16_t) * AUDIO_CHANNEL;
       if (should_play > audio_left)
         should_play = audio_left;
       audio_left -= should_play;
