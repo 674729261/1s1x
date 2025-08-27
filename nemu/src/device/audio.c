9e764c5cf3d82f188f2af8a61623b96b9e531fb1
tracer-ysyx <tracer@ysyx.org>
2025-08-27 13:50:01 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:50:01 up  2:01,  1 user,  load average: 0.39, 0.44, 0.79

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -43,25 +43,24 @@ static void fill_audio_callback(void *udata, Uint8 *stream, int len) {
   // SDL_UnlockAudio();
   // sbuf += len;
   // audio_base[reg_count] -= len;
-  // static int audio_sbuf_flag = 0;
+  static int audio_sbuf_flag = 0;
   int len_ctrl = len;
   if (audio_base[reg_count] < len) {
     len_ctrl = audio_base[reg_count];
   }
   SDL_LockAudio();
-  SDL_MixAudio(stream, sbuf, len_ctrl, SDL_MIX_MAXVOLUME);
-  // if (len_ctrl < len) {
-  //   memset(stream + len_ctrl, 0, len - len_ctrl);
-  // }
-  // if (len_ctrl + audio_sbuf_flag < CONFIG_SB_SIZE) {
-  //   memcpy(stream, sbuf + audio_sbuf_flag, len_ctrl);
-  //   audio_sbuf_flag += len_ctrl;
-  // } else {
-  //   memcpy(stream, sbuf + audio_sbuf_flag, CONFIG_SB_SIZE - audio_sbuf_flag);
-  //   memcpy(stream + CONFIG_SB_SIZE - audio_sbuf_flag, sbuf,
-  //          len_ctrl - (CONFIG_SB_SIZE - audio_sbuf_flag));
-  //   audio_sbuf_flag = len_ctrl - (CONFIG_SB_SIZE - audio_sbuf_flag);
-  // }
+  if (len_ctrl < len) {
+    memset(stream + len_ctrl, 0, len - len_ctrl);
+  }
+  if (len_ctrl + audio_sbuf_flag < CONFIG_SB_SIZE) {
+    memcpy(stream, sbuf + audio_sbuf_flag, len_ctrl);
+    audio_sbuf_flag += len_ctrl;
+  } else {
+    memcpy(stream, sbuf + audio_sbuf_flag, CONFIG_SB_SIZE - audio_sbuf_flag);
+    memcpy(stream + CONFIG_SB_SIZE - audio_sbuf_flag, sbuf,
+           len_ctrl - (CONFIG_SB_SIZE - audio_sbuf_flag));
+    audio_sbuf_flag = len_ctrl - (CONFIG_SB_SIZE - audio_sbuf_flag);
+  }
   SDL_UnlockAudio();
   audio_base[reg_count] -= len_ctrl;
 }
