55113af9a68bcbc9ea8efb707458621df9f7b1ac
tracer-ysyx <tracer@ysyx.org>
2025-08-27 13:20:20 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:20:20 up  1:32,  1 user,  load average: 0.40, 0.56, 0.46

--- am-kernels/kernels/bad-apple/bad-apple.c
+++ am-kernels/kernels/bad-apple/bad-apple.c
@@ -15,14 +15,14 @@ static void sleep_until(uint64_t next) {
     ;
 }
 
-// static uint8_t getbit(uint8_t *p, int idx) {
-//   int byte_idx = idx / 8;
-//   int bit_idx = idx % 8;
-//   bit_idx = 7 - bit_idx;
-//   uint8_t byte = p[byte_idx];
-//   uint8_t bit = (byte >> bit_idx) & 1;
-//   return bit;
-// }
+static uint8_t getbit(uint8_t *p, int idx) {
+  int byte_idx = idx / 8;
+  int bit_idx = idx % 8;
+  bit_idx = 7 - bit_idx;
+  uint8_t byte = p[byte_idx];
+  uint8_t bit = (byte >> bit_idx) & 1;
+  return bit;
+}
 
 int main() {
   extern uint8_t video_payload, video_payload_end;
@@ -46,14 +46,14 @@ int main() {
 
   uint64_t now = io_read(AM_TIMER_UPTIME).us;
   for (; f < fend; f++) {
-    // printf("\033[0;0H"); // reset cursor
-    // for (int y = 0; y < VIDEO_ROW; y++) {
-    //   for (int x = 0; x < VIDEO_COL; x++) {
-    //     uint8_t p = getbit(f->pixel, y * VIDEO_COL + x);
-    //     putch(p ? CHAR_BLACK : CHAR_WHITE);
-    //   }
-    //   putch('\n');
-    // }
+    printf("\033[0;0H"); // reset cursor
+    for (int y = 0; y < VIDEO_ROW; y++) {
+      for (int x = 0; x < VIDEO_COL; x++) {
+        uint8_t p = getbit(f->pixel, y * VIDEO_COL + x);
+        putch(p ? CHAR_BLACK : CHAR_WHITE);
+      }
+      putch('\n');
+    }
 
     if (has_audio) {
       int should_play = (AUDIO_FREQ / FPS) * sizeof(int16_t) * AUDIO_CHANNEL;
