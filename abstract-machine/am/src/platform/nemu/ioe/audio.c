a4792535736b427001051b53fea06996b0627ecb
tracer-ysyx <tracer@ysyx.org>
2025-08-27 13:50:39 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:50:39 up  2:02,  1 user,  load average: 0.32, 0.42, 0.77

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -11,27 +11,44 @@
 #define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)
 
-static int audio_sbuf_flag = 0;
-
 void __am_audio_init() {}
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
-  cfg->present = true;
+  cfg->present = cfg->bufsize > 0;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, true);
-  audio_sbuf_flag = 0;
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
+// void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+//   uint8_t *cur_addr = ctl->buf.start;
+//   uint32_t len = ctl->buf.end - ctl->buf.start;
+//   printf("%d %d %d\n", inl(AUDIO_SBUF_SIZE_ADDR), inl(AUDIO_COUNT_ADDR),
+//   len); while (inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR) < len)
+//     ;
+//   uint32_t offset = inl(AUDIO_COUNT_ADDR);
+//   while ((uint8_t *)ctl->buf.end - cur_addr >= 4) {
+//     outl(AUDIO_SBUF_ADDR + offset, *(uint32_t *)cur_addr);
+//     cur_addr += 4;
+//     offset += 4;
+//   }
+//   while ((uint8_t *)ctl->buf.end - cur_addr >= 1) {
+//     outb(AUDIO_SBUF_ADDR + offset, *(uint8_t *)cur_addr);
+//     cur_addr++;
+//     offset++;
+//   }
+//   outl(AUDIO_COUNT_ADDR, offset);
+// }
+static int audio_sbuf_flag = 0;
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   int data_len = ctl->buf.end - ctl->buf.start;
   int sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
@@ -53,41 +70,3 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   count += data_len;
   outl(AUDIO_COUNT_ADDR, count);
 }
-
-// void __am_audio_init() {}
-
-// void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-//   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
-//   cfg->present = cfg->bufsize > 0;
-// }
-
-// void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-//   outl(AUDIO_FREQ_ADDR, ctrl->freq);
-//   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-//   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-//   outl(AUDIO_INIT_ADDR, 1);
-// }
-
-// void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-//   stat->count = inl(AUDIO_COUNT_ADDR);
-// }
-
-// void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-//   uint8_t *cur_addr = ctl->buf.start;
-//   uint32_t len = ctl->buf.end - ctl->buf.start;
-//   printf("%d %d %d\n", inl(AUDIO_SBUF_SIZE_ADDR), inl(AUDIO_COUNT_ADDR),
-//   len); while (inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR) < len)
-//     ;
-//   uint32_t offset = inl(AUDIO_COUNT_ADDR);
-//   while ((uint8_t *)ctl->buf.end - cur_addr >= 4) {
-//     outl(AUDIO_SBUF_ADDR + offset, *(uint32_t *)cur_addr);
-//     cur_addr += 4;
-//     offset += 4;
-//   }
-//   while ((uint8_t *)ctl->buf.end - cur_addr >= 1) {
-//     outb(AUDIO_SBUF_ADDR + offset, *(uint8_t *)cur_addr);
-//     cur_addr++;
-//     offset++;
-//   }
-//   outl(AUDIO_COUNT_ADDR, offset);
-// }
