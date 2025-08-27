56cc230b2b4020277c0ea63b3af1855acba7e502
tracer-ysyx <tracer@ysyx.org>
2025-08-27 12:56:35 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:56:35 up  1:08,  1 user,  load average: 0.06, 0.07, 0.01

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -81,4 +81,5 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 }
