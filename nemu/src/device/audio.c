415bc279a8ee9938deeb8d2687744f587fe7dee6
tracer-ysyx <tracer@ysyx.org>
2025-08-27 15:17:37 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:17:37 up  3:29,  1 user,  load average: 5.05, 4.71, 2.24

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -17,6 +17,7 @@
 #include <SDL2/SDL_audio.h>
 #include <common.h>
 #include <device/map.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 enum {
@@ -41,7 +42,9 @@ static void fill_audio_callback(void *udata, Uint8 *stream, int len) {
   int next_pos = (last_pos + len > audio_base[reg_count] ? audio_base[reg_count]
                                                          : last_pos + len);
   SDL_LockAudio();
-  SDL_MixAudio(stream, sbuf + last_pos, next_pos - last_pos, SDL_MIX_MAXVOLUME);
+  printf("!!!!\n");
+  SDL_MixAudio(stream, udata + last_pos, next_pos - last_pos,
+               SDL_MIX_MAXVOLUME);
   last_pos = next_pos;
   SDL_UnlockAudio();
   if (last_pos == audio_base[reg_count])
@@ -61,7 +64,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
                                   .silence = 0,
                                   .samples = audio_base[reg_samples],
                                   .callback = fill_audio_callback,
-                                  .userdata = NULL};
+                                  .userdata = sbuf};
     if (SDL_OpenAudio(&sdlAudioSpec, NULL) < 0) {
       fprintf(stderr, "Can't open audio.\n");
       exit(-1);
