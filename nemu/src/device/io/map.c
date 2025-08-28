423da3bd8f6b3cd9d7f91868a1433375db54fed9
tracer-ysyx <tracer@ysyx.org>
2025-08-28 12:43:52 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:43:52 up 22 min,  1 user,  load average: 0.09, 0.25, 0.31

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -13,6 +13,7 @@
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
+#include <SDL2/SDL_audio.h>
 #include <device/map.h>
 #include <isa.h>
 #include <memory/host.h>
@@ -71,6 +72,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
+  SDL_LockAudio();
   host_write(map->space + offset, len, data);
+  SDL_UnlockAudio();
   invoke_callback(map->callback, offset, len, true);
 }
