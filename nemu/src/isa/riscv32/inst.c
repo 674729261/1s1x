37bbb99ee7486363d6fb7c0f23e2c96a1196924d
tracer-ysyx <tracer@ysyx.org>
2025-08-30 01:30:46 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:30:46 up  5:44,  1 user,  load average: 0.10, 0.08, 0.20

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -15,6 +15,7 @@
 #include "common.h"
 #include "local-include/reg.h"
 #include "macro.h"
+#include "memory/symbols.h"
 #include "ringbuffer.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
@@ -122,6 +123,7 @@ static int decode_exec(Decode *s) {
     decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type));           \
     __VA_ARGS__;                                                               \
   }
+  printf("At %s\n", find_symbol_name(find_symbol(s->pc)));
   //   printf("%08x %08x\n", s->pc, s->isa.inst);
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc, U,
