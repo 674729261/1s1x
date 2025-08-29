107c6427e6ba7d1a590c699a8829f0095aa25c77
tracer-ysyx <tracer@ysyx.org>
2025-08-29 20:25:47 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:25:47 up 39 min,  1 user,  load average: 0.05, 0.04, 0.00

---
+++ nemu/src/cpu/ringbuffer.c
@@ -0,0 +1,36 @@
+#include "ringbuffer.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+RingBuffer *createRingbuffer(int size) {
+  RingBuffer *rb = malloc(sizeof(RingBuffer));
+  rb->data = calloc(sizeof(InstrInfo), size);
+  rb->sz = size;
+  rb->begin = rb->end = rb->count = 0;
+  return rb;
+}
+void pushRingBuffer(RingBuffer *rb, vaddr_t pc, uint32_t inst) {
+  rb->data[rb->end].pc = pc;
+  rb->data[rb->end].inst = inst;
+  rb->end++;
+  if (rb->end == rb->sz)
+    rb->end = 0;
+  if (rb->count != rb->sz)
+    rb->count++;
+  else
+    rb->begin = rb->end;
+}
+void freeRingbuffer(RingBuffer *rb) {
+  free(rb->data);
+  free(rb);
+}
+
+void showRingBuffer(const RingBuffer *rb) {
+  for (int i = 0, pos = rb->begin; i < rb->count;
+       i++, pos = (pos + 1) % rb->sz) {
+    void fdisassemble(FILE * fp, uint64_t pc, uint8_t *code, int nbyte);
+    fdisassemble(stderr, rb->data[pos].pc, (uint8_t *)&(rb->data[pos].inst), 4);
+    fputc('\n', stderr);
+  }
+}
---
+++ nemu/src/cpu/ringbuffer.h
@@ -0,0 +1,24 @@
+#ifndef __RINGBUFFER_H__
+#define __RINGBUFFER_H__
+#include "common.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+  vaddr_t pc;
+  uint32_t inst;
+} InstrInfo;
+
+typedef struct {
+  int sz, count;
+  int begin, end;
+  InstrInfo *data;
+} RingBuffer;
+
+RingBuffer *createRingbuffer(int size);
+void pushRingBuffer(RingBuffer *rb, vaddr_t pc, uint32_t inst);
+void freeRingbuffer(RingBuffer *rb);
+
+void showRingBuffer(const RingBuffer *rb);
+
+#endif
