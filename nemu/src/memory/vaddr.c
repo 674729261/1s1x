6cad95c437104697dfd24b00891bc8f20bb24c37
tracer-ysyx <tracer@ysyx.org>
2025-08-29 21:27:21 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:27:21 up  1:41,  1 user,  load average: 4.55, 1.84, 1.61

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -28,7 +28,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACER
-  fprintf(stderr, "Write addr %08x, %d bytes, data : %08x", addr, len, data);
+  fprintf(stderr, "Write addr %08x, %d bytes, data : %08x\n", addr, len, data);
 #endif
   paddr_write(addr, len, data);
 }
