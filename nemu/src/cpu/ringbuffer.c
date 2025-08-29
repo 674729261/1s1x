d866cd106460ecaa934e4658117be2823b09823c
tracer-ysyx <tracer@ysyx.org>
2025-08-29 21:16:41 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:16:41 up  1:30,  1 user,  load average: 1.70, 2.51, 1.56

--- nemu/src/cpu/ringbuffer.c
+++ nemu/src/cpu/ringbuffer.c
@@ -39,11 +39,13 @@ void showRingBuffer(const RingBuffer *rb) {
   for (int i = 0, pos = rb->begin; i < rb->count;
        i++, pos = (pos + 1) % rb->sz) {
     if (i == rb->count - 1)
-      fprintf(stderr, "%s", "to be executed --> ");
+      fprintf(stderr, "%s\033[1;31m", "to be executed --> ");
     else
       fprintf(stderr, "%s", "                   ");
     void fdisassemble(FILE * fp, uint64_t pc, uint8_t *code, int nbyte);
     fdisassemble(stderr, rb->data[pos].pc, (uint8_t *)&(rb->data[pos].inst), 4);
+    if (i == rb->count - 1)
+      fprintf(stderr, "\033[0m");
     fputc('\n', stderr);
   }
 }
