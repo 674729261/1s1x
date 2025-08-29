cab5f21f4b9e3989caaa21fbdb3a8561108a097f
tracer-ysyx <tracer@ysyx.org>
2025-08-30 00:56:14 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:56:14 up  5:10,  1 user,  load average: 0.78, 1.33, 0.63

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -23,7 +23,7 @@ static const uint32_t img[] = {
     0x00000313, // li t1,zero
     0x01400393, // li t2,20
     0x02028223, // sb  zero,36(t0)
-    0x0102c503, // lbu a0,16(t0)
+    0x0242c503, // lbu a0,36(t0)
     0x00130313, // addi t1,t1,1
     0xfe639ae3, // bne t2,t1,-12
     // 0x01004503, // lbu a0,0(zero) : error
