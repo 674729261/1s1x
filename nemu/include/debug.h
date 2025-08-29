525a17538a0667507458ceed2b0537d991833417
tracer-ysyx <tracer@ysyx.org>
2025-08-29 20:56:29 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:56:29 up  1:10,  1 user,  load average: 0.02, 0.35, 0.27

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -36,7 +36,7 @@
       IFNDEF(CONFIG_TARGET_AM, extern FILE *log_fp; fflush(log_fp));           \
       extern void assert_fail_msg();                                           \
       assert_fail_msg();                                                       \
-      fprintf(stderr, "Last %d instructions :\n", CONFIG_RINGBUFFER_SIZE);     \
+      fprintf(stderr, "Last %d instructions :\n", inst_buffer.count);          \
       showRingBuffer(&inst_buffer);                                            \
       assert(cond);                                                            \
     }                                                                          \
