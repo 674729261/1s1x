db8026ea49d623f7e9d2ececbd28de21e6ac8137
tracer-ysyx <tracer@ysyx.org>
2025-08-30 20:17:06 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:17:06 up  6:06,  1 user,  load average: 0.17, 0.23, 0.35

--- am-kernels/tests/klib-tests/tests/test1.c
+++ am-kernels/tests/klib-tests/tests/test1.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 #define NR_DATA LENGTH(test_data)
+
+#define MY_CHECK(C)                                                            \
+  do {                                                                         \
+    if (!(C))                                                                  \
+      return -1;                                                               \
+  } while (0)
+
+#define MY_RANGE(A, B, F, T)                                                   \
+  do                                                                           \
+    for (int qwertqwert__ = (F); qwertqwert__ < (T); qwertqwert__++)           \
+      if ((A)[qwertqwert__] != (B)[qwertqwert__])                              \
+        return -1;                                                             \
+  while (0)
+
 const char *a = "1145141919810";
 const char *b = "1919810";
 const char *c = "1145141919810";
@@ -10,39 +24,29 @@ const char *d = "514";
 int test_strcmp() {
 
   int ret = strcmp(a, b);
-  if (ret == 0)
-    return ret;
+  MY_CHECK(ret != 0);
   ret = strcmp(a, c);
-  if (ret != 0)
-    return ret;
+  MY_CHECK(ret == 0);
   ret = strcmp(a, d);
-  if (ret == 0)
-    return ret;
+  MY_CHECK(ret != 0);
   ret = strcmp(a + 3, d);
-  if (ret == 0)
-    return ret;
+  MY_CHECK(ret != 0);
   ret = strcmp(c + 6, b);
-  if (ret != 0)
-    return ret;
+  MY_CHECK(ret == 0);
   return 0;
 }
 
 int test_memcmp() {
   int ret = memcmp(a, b, 4);
-  if (ret == 0)
-    return ret;
+  MY_CHECK(ret != 0);
   ret = memcmp(a, c, 10);
-  if (ret != 0)
-    return ret;
+  MY_CHECK(ret == 0);
   ret = memcmp(a, d, 3);
-  if (ret == 0)
-    return ret;
+  MY_CHECK(ret != 0);
   ret = memcmp(a + 3, d, 3);
-  if (ret != 0)
-    return ret;
+  MY_CHECK(ret == 0);
   ret = memcmp(c + 6, b, 4);
-  if (ret != 0)
-    return ret;
+  MY_CHECK(ret == 0);
   return 0;
 }
 
@@ -54,17 +58,11 @@ int test_memmove() {
   const int r2[8] = {3, 4, 5, 6, 7, 6, 7, 8};
   const int r3[8] = {1, 2, 1, 2, 3, 4, 5, 8};
   memmove(y, x + 2, 4 * sizeof(int));
-  for (int i = 0; i < 8; i++)
-    if (y[i] != r1[i])
-      return -1;
+  MY_RANGE(y, r1, 0, 8);
   memmove(x, x + 2, 5 * sizeof(int));
-  for (int i = 0; i < 8; i++)
-    if (x[i] != r2[i])
-      return -1;
+  MY_RANGE(x, r2, 0, 8);
   memmove(z + 2, z, 5 * sizeof(int));
-  for (int i = 0; i < 8; i++)
-    if (z[i] != r3[i])
-      return -1;
+  MY_RANGE(z, r3, 0, 8);
   return 0;
 }
 
