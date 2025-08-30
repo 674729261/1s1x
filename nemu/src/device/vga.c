a104c68bb1d8d15672c1ae90377065cba8fb92c1
tracer-ysyx <tracer@ysyx.org>
2025-08-30 19:20:45 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:20:45 up  5:10,  1 user,  load average: 0.04, 0.13, 0.09

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -45,7 +45,7 @@ SDL_Window *window = NULL;
 static void init_screen() {
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
-  SDL_Init(SDL_INIT_VIDEO);
+  SDL_InitSubSystem(SDL_INIT_VIDEO);
   SDL_CreateWindowAndRenderer(
       SCREEN_W * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
       SCREEN_H * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)), 0, &window,
