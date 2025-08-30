03843654d0ba8960b09351610ce6b06904cd2e08
tracer-ysyx <tracer@ysyx.org>
2025-08-30 15:04:18 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:04:18 up 54 min,  1 user,  load average: 0.13, 0.15, 0.90

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -122,14 +122,13 @@ static void check_jal(vaddr_t from_pc, vaddr_t to_pc, uint32_t inst) {
   if (rd == 1) {
     several_spaces(cnt_stack_ftrace * 2);
     int to_symbol = find_symbol(to_pc);
-    fprintf(stderr, "call %s at 0x%08x\n", find_symbol_name(to_symbol),
-            from_pc);
+    fprintf(stderr, "call [%s@0x%08x]\n", find_symbol_name(to_symbol), from_pc);
     push_stack_ftrace(from_pc, to_symbol);
 
   } else if (inst == 0x00008067) {
     Call ret_call = pop_stack_ftrace();
     several_spaces(cnt_stack_ftrace * 2);
-    fprintf(stderr, "ret  %s\n", find_symbol_name(ret_call.symbol));
+    fprintf(stderr, "ret  [%s]\n", find_symbol_name(ret_call.symbol));
   }
 }
 #endif
