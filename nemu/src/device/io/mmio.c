ebd73e843690f5411eb19b7aec541a1415055a60
tracer-ysyx <tracer@ysyx.org>
2025-08-30 23:01:06 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:01:06 up  8:50,  1 user,  load average: 0.20, 0.24, 0.39

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -15,6 +15,7 @@
 
 #include <device/map.h>
 #include <memory/paddr.h>
+#include <stdio.h>
 
 #define NR_MAP 16
 
@@ -65,5 +66,8 @@ word_t mmio_read(paddr_t addr, int len) {
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
+
   map_write(addr, len, data, fetch_mmio_map(addr));
+  fprintf(stderr, "Writing data %08x of length %d to MMIO %08x\n", data, len,
+          addr);
 }
