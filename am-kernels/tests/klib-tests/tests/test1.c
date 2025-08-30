d2431cb42d9b8042ea98be86d7413720a5e30ba4
tracer-ysyx <tracer@ysyx.org>
2025-08-30 20:32:26 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:32:26 up  6:22,  1 user,  load average: 0.26, 0.17, 0.21

--- am-kernels/tests/klib-tests/tests/test1.c
+++ am-kernels/tests/klib-tests/tests/test1.c
@@ -91,7 +91,7 @@ int test_sprintf() {
   unsigned long long b = 0x12345abcdef;
   const char *s = "foobar";
 
-  sprintf(x, "a is %+04d\nb is %016llx\ns is %s\n", a, b, s);
+  printf("a is %+04d\nb is %016llx\ns is %s\n", a, b, s);
   MY_RANGE(x, "a is +0042\nb is 0000012345abcdef\ns is foobar\n", 0, 42);
   return 0;
 }
