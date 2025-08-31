4dadbf0bd0f144209d14f851416e822b7ab536c4
ysyx_25080216-Wang Xiangyu <2292683261@qq.com>
2025-08-31 21:31:11 +0800

    reform code structure

---
+++ npc/csrc/NPCemu.cpp
@@ -0,0 +1,167 @@
+#include "Simulators/NPCemu.h"
+#include "Simulators/RISCV32.h"
+#include "VCPU___024root.h"
+#include <cstdint>
+#include <format>
+#include <iostream>
+#include <optional>
+#include <ostream>
+#include <stdexcept>
+#include <string_view>
+NPCemu::NPCemu(size_t MemSize, std::string_view program)
+    : RISCV32(MemSize, program, PC_Init), dut("DUT"), trapped(0),
+      inst_count(0) {}
+
+RISCV32::CPU_State NPCemu::getCPUState() { return cpu; }
+
+uint32_t NPCemu::getGPR(int idx) {
+  assert(idx >= 0 && idx < 32);
+  return cpu.gpr[idx];
+}
+RISCV32::addr_t NPCemu::getPC() { return cpu.pc; }
+
+void NPCemu::reset() {
+  dut.reset = 1;
+  dut.clock = 0;
+  dut.eval();
+  dut.clock = 1;
+  dut.eval();
+  dut.clock = 0;
+  dut.reset = 0;
+  dut.eval();
+}
+
+RISCV32::Interrupt NPCemu::step(std::size_t c) {
+  Interrupt state = RISCV32::Interrupt::NONE;
+  while (c--) {
+    addr_t pc = dut.io_pc;
+    if (pc < memOffset) {
+      throw std::logic_error(std::format("pc : {:08x} out of range", pc));
+    }
+    dut.io_instr = M[(pc - memOffset) / 4];
+    dut.clock = 0;
+    dut.eval();
+    dut.clock = 1;
+    dut.eval();
+    inst_count++;
+    if (trapped) {
+      state = RISCV32::Interrupt::EBREAK;
+      break;
+    }
+  }
+
+  cpu.gpr[0] = 0;
+  cpu.gpr[1] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_0_r;
+  cpu.gpr[2] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_1_r;
+  cpu.gpr[3] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_2_r;
+  cpu.gpr[4] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_3_r;
+  cpu.gpr[5] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_4_r;
+  cpu.gpr[6] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_5_r;
+  cpu.gpr[7] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_6_r;
+  cpu.gpr[8] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_7_r;
+  cpu.gpr[9] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_8_r;
+  cpu.gpr[10] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_9_r;
+  cpu.gpr[11] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_10_r;
+  cpu.gpr[12] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_11_r;
+  cpu.gpr[13] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_12_r;
+  cpu.gpr[14] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_13_r;
+  cpu.gpr[15] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_14_r;
+  cpu.gpr[16] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_15_r;
+  cpu.gpr[17] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_16_r;
+  cpu.gpr[18] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_17_r;
+  cpu.gpr[19] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_18_r;
+  cpu.gpr[20] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_19_r;
+  cpu.gpr[21] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_20_r;
+  cpu.gpr[22] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_21_r;
+  cpu.gpr[23] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_22_r;
+  cpu.gpr[24] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_23_r;
+  cpu.gpr[25] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_24_r;
+  cpu.gpr[26] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_25_r;
+  cpu.gpr[27] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_26_r;
+  cpu.gpr[28] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_27_r;
+  cpu.gpr[29] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_28_r;
+  cpu.gpr[30] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_29_r;
+  cpu.gpr[31] = dut.rootp->CPU__DOT__gpr__DOT__register_bank_regs_30_r;
+  cpu.pc = dut.io_pc;
+  return state;
+}
+int NPCemu::instrCount() { return inst_count; }
+
+void NPCemu::writeMemory(int waddr, int wdata, char wmask) {
+  for (int i = 0; i < 4; i++) {
+    if ((wmask >> i) & 0x1) {
+      uint32_t mask32 =
+          (uint32_t)((1ull << (8ull * (i + 1))) - (1ull << (8ull * i)));
+      uint32_t addr = (uint32_t)(waddr - memOffset) >> 2;
+      if (addr < M.size() && waddr >= memOffset) {
+        M[addr] &= ~mask32;
+        M[addr] |= wdata & mask32;
+      } else if (waddr >= deviceBase) {
+        writeMMIO(waddr & ~0x3, mask32, wdata);
+      }
+    }
+  }
+}
+uint32_t NPCemu::readMemory(int raddr) {
+  raddr &= ~0x3;
+  uint32_t addr = (uint32_t)(raddr - PC_Init) >> 2;
+  if (addr < M.size() && raddr >= PC_Init)
+    return M[addr];
+  if (raddr >= deviceBase) {
+    auto ret = readMMIO(raddr);
+    if (!ret.has_value()) {
+      return 0xdeafbeef;
+    }
+    return ret.value();
+  }
+
+  return 0xdeafbeef;
+}
+
+static void write_mask(uint32_t &dst, uint32_t mask32, uint32_t wdata) {
+  dst &= ~mask32;
+  dst |= wdata & mask32;
+}
+
+std::optional<uint32_t> NPCemu::readMMIO(int raddr) {
+  if (raddr >= RTCAddr && raddr < RTCAddrEnd) {
+    update_RTC();
+    if (raddr == RTCAddr)
+      return RTC.RTC_reg[0];
+    else
+      return RTC.RTC_reg[1];
+  }
+  return std::nullopt;
+}
+
+void NPCemu::update_RTC() {
+  using namespace std::chrono;
+  auto now_tick = steady_clock().now();
+  uint64_t duration = static_cast<uint64_t>(
+      duration_cast<microseconds>(now_tick - RTC.last_time).count());
+  uint64_t start_time =
+      RTC.RTC_reg[0] | (static_cast<uint64_t>(RTC.RTC_reg[1]) << 32);
+  uint64_t now_time = start_time + duration;
+  RTC.RTC_reg[0] = now_time & 0xFFFFFF;
+  RTC.RTC_reg[1] = now_time >> 32;
+  RTC.last_time = now_tick;
+}
+
+void NPCemu::writeMMIO(uint32_t waddr, uint32_t mask32, uint32_t wdata) {
+  using namespace std::chrono;
+  if (waddr >= RTCAddr && waddr < RTCAddrEnd) {
+    uint32_t RTC_id = (waddr - RTCAddr) >> 2;
+    update_RTC();
+    write_mask(RTC.RTC_reg[RTC_id], mask32, wdata);
+    RTC.last_time = steady_clock().now();
+    return;
+  }
+  if (waddr == SerialPort) {
+    if (mask32 != 0xFF)
+      throw std::logic_error(std::format(
+          "mask32 {:08x} is not 0xFF when writing serial port", mask32));
+    std::cout.put(wdata);
+    return;
+  }
+  throw std::logic_error(std::format("Writing to invalid MMIO {:08x}", waddr));
+}
