91a069b2c20bf109b99563b75970ac1f57f83913
tracer-ysyx <tracer@ysyx.org>
2025-09-02 01:28:49 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:28:49 up  4:04,  1 user,  load average: 2.57, 1.64, 0.88

--- npc/csrc/Expression.cpp
+++ npc/csrc/Expression.cpp
@@ -188,7 +188,7 @@ long long Expression::eval_sub(RISCV32 &dut, int l, int r) {
   if (parentheses[l] == r)
     return eval_sub(dut, l + 1, r - 1);
   int pos_main = main_token(dut, l, r);
-  println("{},{},{}", l, r, pos_main);
+
   if (pos_main == -1) {
     switch (tokens[pos_main].type) {
     case '+':
@@ -251,7 +251,7 @@ long long Expression::eval_sub(RISCV32 &dut, int l, int r) {
 int Expression::main_token(RISCV32 &, int l, int r) {
   int ret = -1, mn = 99999;
 
-  for (int i = 0; i < tokens.size(); i++) {
+  for (int i = l; i <= r; i++) {
     if (tokens[i].type == '(') {
       i = parentheses[i];
       continue;
