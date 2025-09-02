216153bb308be5ccd9459fac9e538d03d89cb326
tracer-ysyx <tracer@ysyx.org>
2025-09-02 17:12:32 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:12:32 up 20 min,  1 user,  load average: 0.52, 0.28, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -28,8 +28,7 @@ int main(int argc, char *argv[]) {
   program.add_argument("-i", "--image")
       .help("Path to log file")
       .nargs(1)
-      .default_value(string(""))
-      .required();
+      .default_value(string(""));
   program.add_argument("-l", "--log").help("The program image file").required();
   program.add_argument("-z", "--mem_size")
       .help("Size of memory")
