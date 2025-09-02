8f17873de059dd7a29f34c209f1d78a0dcd5f8ec
tracer-ysyx <tracer@ysyx.org>
2025-09-02 23:45:56 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:45:56 up  6:54,  1 user,  load average: 0.44, 0.34, 0.29

--- npc/csrc/SingleMonitor.cpp
+++ npc/csrc/SingleMonitor.cpp
@@ -93,6 +93,7 @@ bool SingleMonitor::process_trap() {
 void SingleMonitor::simulate(unsigned long cnt) {
   using namespace std::chrono;
   int n_inst = emu->instrCount();
+  println("{}", n_inst);
   auto start = steady_clock::now();
   if (watchers.empty()) {
     emu->step(cnt);
