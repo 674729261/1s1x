6198ef1cccbe017a9141f1f80e95989bf3428972
tracer-ysyx <tracer@ysyx.org>
2025-09-05 15:00:52 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:00:52 up  3:01,  1 user,  load average: 0.27, 0.12, 0.09

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -1,38 +1,49 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
+#include <assert.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
+#include <isa.h>
 #include <memory/paddr.h>
+#include <string.h>
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n,
+                              bool direction) {
+  if (direction == DIFFTEST_TO_REF) {
+    uint8_t *pos = guest_to_host(addr);
+    uint8_t *src = buf;
+    for (int i = 0; i < n; i++) {
+      *(pos + i) = *(src + i);
+    }
+  } else {
+    assert(0);
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    memcpy(&cpu, dut, sizeof(cpu));
+  } else {
+    memcpy(dut, &cpu, sizeof(cpu));
+  }
 }
 
-__EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
-}
+__EXPORT void difftest_exec(uint64_t n) { cpu_exec(n); }
 
-__EXPORT void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
+__EXPORT void difftest_raise_intr(word_t NO) { assert(0); }
 
 __EXPORT void difftest_init(int port) {
   void init_mem();
