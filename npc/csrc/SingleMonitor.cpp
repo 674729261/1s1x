da71a344beb597eb934a445b28dc6fada9a18fed
tracer-ysyx <tracer@ysyx.org>
2025-09-06 00:03:37 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:03:37 up 12:04,  1 user,  load average: 1.04, 1.49, 1.07

--- npc/csrc/SingleMonitor.cpp
+++ npc/csrc/SingleMonitor.cpp
@@ -70,7 +70,7 @@ void SingleMonitor::start() {
     e.reset();
   CommandState state = CommandState::NONE;
   bool finished = false;
-  spdlog::error("Cnt {}", (uintptr_t)emus.front().get());
+  spdlog::error("Cnt {}", (uintptr_t)emus.size());
   while (true) {
     if (batch)
       emus.front()->simulate(-1);
