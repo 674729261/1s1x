976a67d1d1feda990fb5692ac2f1f67b2dc4d402
tracer-ysyx <tracer@ysyx.org>
2025-09-06 00:12:19 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:12:19 up 12:13,  1 user,  load average: 0.19, 0.79, 1.01

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,14 +18,15 @@
 #include <difftest-def.h>
 #include <isa.h>
 #include <memory/paddr.h>
+#include <stdio.h>
 #include <string.h>
-
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n,
                               bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     uint8_t *pos = guest_to_host(addr);
     uint8_t *src = buf;
     for (int i = 0; i < n; i++) {
+      printf("%d\n", i);
       *(pos + i) = *(src + i);
     }
   } else {
