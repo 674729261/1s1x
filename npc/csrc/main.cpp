7fdf8112178b28d1726198ba31580fa0997b9408
tracer-ysyx <tracer@ysyx.org>
2025-09-08 01:57:05 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:57:05 up  2:43,  1 user,  load average: 0.40, 0.27, 0.21

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -106,6 +106,10 @@ int main(int argc, char *argv[]) {
   }
   if (use_ftracer) {
     auto path_elf = program.get("--elf");
+    if (path_elf.empty()) {
+      println(cerr, "ELF path not specified");
+      exit(1);
+    }
     load_symbols(path_elf.c_str());
     spdlog::info("Initialized symbols from elf : {}", path_elf);
   }
