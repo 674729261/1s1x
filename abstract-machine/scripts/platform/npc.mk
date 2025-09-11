852eedd7e418098374fd93a8905857d1a3ae579c
ysyx_25080216-Wang Xiangyu <2292683261@qq.com>
2025-09-12 00:14:25 +0800

    ?

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -26,6 +26,6 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	make -C $(NPC_HOME) sim PROG=$(IMAGE).bin TYPE=b
+	make -C $(NPC_HOME) sim_full_speed PROG=$(IMAGE).bin NPC_FLAGS="--batch"
 
 .PHONY: insert-arg
