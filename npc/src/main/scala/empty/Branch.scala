f02d43030ecb64783bb46a683b3bbee8ad1b0471
tracer-ysyx <tracer@ysyx.org>
2025-09-12 01:04:15 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:04:15 up 13:10,  1 user,  load average: 1.79, 1.12, 0.71

--- npc/src/main/scala/empty/Branch.scala
+++ npc/src/main/scala/empty/Branch.scala
@@ -18,31 +18,22 @@ class Branch(WIDTH: Int) extends RawModule {
   adder.io.B := ~io.B
   adder.io.Cin := true.B
 
-  io.jump := false.B
   val is_lt = Wire(Bool())
   val is_eq = Wire(Bool())
   is_lt := (io.A(31) & ~io.B(31)) | // - < +
     ((~(io.A(31) ^ io.B(31))) & adder.io.out(31)) // A - B < 0
   is_eq := adder.io.out === 0.U(32.W)
-  switch(io.funct3) {
-    is("b000".U(3.W)) { // beq
-      io.jump := is_eq
-    }
-    is("b001".U(3.W)) { // bne
-      io.jump := ~is_eq
-    }
-    is("b100".U(3.W)) { // blt
-      io.jump := is_lt
-    }
-    is("b101".U(3.W)) { // bge
-      io.jump := ~is_lt
-    }
-    is("b110".U(3.W)) { // bltu
-      io.jump := ~adder.io.Cout
-    }
-    is("b111".U(3.W)) { // bgeu
-      io.jump := adder.io.Cout
-    }
-  }
+
+  val is_unsigned = io.funct3(1)
+  val compared = Mux(is_unsigned, ~adder.io.Cout, is_lt)
+
+  io.jump := MuxLookup(Cat(io.funct3(2), io.funct3(0)), false.B)(
+    Seq(
+      "b00".U(3.W) -> is_eq,
+      "b01".U(3.W) -> ~is_eq,
+      "b10".U(3.W) -> compared,
+      "b11".U(3.W) -> ~compared
+    )
+  )
 
 }
