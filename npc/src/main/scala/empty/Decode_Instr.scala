17033eb080c74ef79c88ca9e6b9f65c47acb647a
tracer-ysyx <tracer@ysyx.org>
2025-09-11 18:19:01 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:19:01 up  6:24,  1 user,  load average: 2.39, 1.09, 0.99

--- npc/src/main/scala/empty/Decode_Instr.scala
+++ npc/src/main/scala/empty/Decode_Instr.scala
@@ -67,7 +67,7 @@ class DecodeInstr extends RawModule {
   io.is_alu_a_pc := is_B || is_J || io.is_auipc
   io.is_alu_b_reg := is_R
   io.is_alu_sub_sra :=
-    io.inst(5) && !(io.is_arithmetic_imm && io.funct3 === "b000".U(3.W))
+    io.funct7(5) && !(io.is_arithmetic_imm && io.funct3 === "b000".U(3.W))
   io.is_alu_force_add := io.is_store || io.is_load || io.is_branch || io.is_auipc || io.is_jal
   io.is_gpr_wdata_from_ram := io.is_load
   io.is_gpr_wdata_from_snpc := io.is_jal || io.is_jalr
