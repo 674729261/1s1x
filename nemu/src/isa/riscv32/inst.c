8074df21f7d091167605e062c777ca9bf3ec8dbe
tracer-ysyx <tracer@ysyx.org>
2025-10-03 03:02:03 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:02:03 up 14:55,  1 user,  load average: 0.20, 0.13, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -288,7 +288,9 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
           NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, N,
-          s->dnpc = isa_raise_intr(11, cpu.pc)); // R(10) is $a0
+          s->dnpc = isa_raise_intr(11, cpu.pc));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret, N,
+          s->dnpc = isa_raise_intr(11, cpu.pc));
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw, CSRR,
           word_t *dest_csr = csr_id(csr);
           R(rd) = *dest_csr; *dest_csr = src1;);
