c2e180c9bcb577bbdcc051e0008936151c5d34a0
tracer-ysyx <tracer@ysyx.org>
2025-10-03 02:11:54 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:11:54 up 14:05,  1 user,  load average: 0.06, 0.09, 0.09

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,7 +20,8 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (ref_r->pc != cpu.pc) {
-    fprintf(stderr, "PC differs\nShould be %08x but got %08x\n", ref_r->pc, pc);
+    fprintf(stderr, "PC differs\nShould be %08x but got %08x\n", ref_r->pc,
+            cpu.pc);
     return false;
   }
   for (int i = 0; i < 32; i++) {
