0bbb151e997493737ebba90d650e047930ca14ab
tracer-ysyx <tracer@ysyx.org>
2025-10-03 13:15:55 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:15:55 up  1:26,  1 user,  load average: 0.11, 0.12, 0.10

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -19,7 +19,7 @@ static void f(void *arg) {
 static Context *schedule(Event ev, Context *prev) {
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  printf("%p\n", current);
+  printf("%p %p %p\n", current, &pcb[0], &pcb[1]);
   return current->cp;
 }
 
