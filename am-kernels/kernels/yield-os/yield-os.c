f603579e3d57aecc3525988ab7c52ab4911c51bb
tracer-ysyx <tracer@ysyx.org>
2025-10-24 03:19:11 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:19:11 up  2:26,  1 user,  load average: 0.28, 0.24, 0.27

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -21,7 +21,7 @@ static void f(void *arg) {
 
 static Context *schedule(Event ev, Context *prev) {
   current->cp = prev;
-  current = (current == &(pcb[0]) ? &(pcb[0]) : &(pcb[0]));
+  current = (current == &(pcb[0]) ? &(pcb[1]) : &(pcb[0]));
   
   return current->cp;
 }
@@ -30,6 +30,8 @@ int main() {
   cte_init(schedule);
   pcb[0].cp = kcontext((Area){pcb[0].stack, &pcb[0] + 1}, f, (void *)1L);
   pcb[1].cp = kcontext((Area){pcb[1].stack, &pcb[1] + 1}, f, (void *)2L);
+  printf("%08x!!\n", (uint32_t)pcb[0].cp);
+  printf("%08x!!\n", (uint32_t)pcb[1].cp);
   yield();
   panic("Should not reach here!");
 }
