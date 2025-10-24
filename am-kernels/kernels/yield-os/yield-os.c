e2dc8dcf34e96e7c351920eef138403045115c0b
tracer-ysyx <tracer@ysyx.org>
2025-10-24 12:41:08 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:41:08 up 13 min,  1 user,  load average: 0.50, 1.14, 0.61

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -30,11 +30,11 @@ int main() {
   cte_init(schedule);
   pcb[0].cp = kcontext((Area){pcb[0].stack, &pcb[0] + 1}, f, (void *)1L);
   pcb[1].cp = kcontext((Area){pcb[1].stack, &pcb[1] + 1}, f, (void *)2L);
-  // printf("%08x!!\n", (uint32_t)&(pcb[0].cp));
-  // printf("%08x!!\n", (uint32_t)&(pcb[1].cp));
-  // printf("%08x??\n", (uint32_t)&(pcb[0].stack));
-  // printf("%08x??\n", (uint32_t)&(pcb[1].stack));
-  // printf("%08x??\n", (uint32_t)sizeof(pcb[0].stack));
+  printf("%08x!!\n", (uint32_t)&(pcb[0].cp));
+  printf("%08x!!\n", (uint32_t)&(pcb[1].cp));
+  printf("%08x??\n", (uint32_t)&(pcb[0].stack));
+  printf("%08x??\n", (uint32_t)&(pcb[1].stack));
+  printf("%08x??\n", (uint32_t)sizeof(pcb[0].stack));
   yield();
   panic("Should not reach here!");
 }
