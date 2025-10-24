6c2e265216bed202ab9b43e80b4c53bde197e365
tracer-ysyx <tracer@ysyx.org>
2025-10-24 12:34:05 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:34:05 up 6 min,  1 user,  load average: 0.21, 0.18, 0.09

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -32,6 +32,8 @@ int main() {
   pcb[1].cp = kcontext((Area){pcb[1].stack, &pcb[1] + 1}, f, (void *)2L);
   printf("%08x!!\n", (uint32_t)&(pcb[0].cp));
   printf("%08x!!\n", (uint32_t)&(pcb[1].cp));
+  printf("%08x??\n", (uint32_t)&(pcb[0].stack));
+  printf("%08x??\n", (uint32_t)&(pcb[1].stack));
   yield();
   panic("Should not reach here!");
 }
