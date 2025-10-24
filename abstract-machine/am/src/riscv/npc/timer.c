b46ab101201492cc1a91150107596c184222669d
tracer-ysyx <tracer@ysyx.org>
2025-10-24 21:04:28 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:04:28 up  8:36,  1 user,  load average: 0.75, 0.54, 0.42

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -13,7 +13,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t lower = inl(RTC_ADDR);
   uint32_t upper = inl(RTC_ADDR + 0x4);
   uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
-  static uint64_t last = 9999;
+  static uint64_t last = 1;
   if (last > uptime->us / 1000000) {
     printf("!!%lld\n", last);
     // last = uptime->us / 1000000;
