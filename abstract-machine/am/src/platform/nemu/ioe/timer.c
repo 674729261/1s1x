4d859590e525ef6275e9ba4bb419a235b157f8de
tracer-ysyx <tracer@ysyx.org>
2025-10-24 21:11:22 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:11:22 up  8:43,  1 user,  load average: 0.99, 0.90, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -13,7 +13,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t lower = inl(RTC_ADDR);
   uint32_t upper = inl(RTC_ADDR + 0x4);
   uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
-  printf("!!%lld\r", uptime->us);
+  // printf("!!%lld\r", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
