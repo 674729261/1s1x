0d94f0292d6a1a139ba93551bc11397c25e677e5
tracer-ysyx <tracer@ysyx.org>
2025-10-24 15:27:59 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:27:59 up  2:59,  1 user,  load average: 0.32, 0.34, 0.30

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -14,7 +14,7 @@ static void f(void *arg) {
   while (1) {
     putch("?AB"[(uintptr_t)arg > 2 ? 0 : (uintptr_t)arg]);
     for (int volatile i = 0; i < 100000; i++)
-      ;
+      printf("%d\n", i);
     yield();
   }
 }
