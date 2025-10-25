fba850ca373e51987c9cee4dda19d02d9785d3f2
tracer-ysyx <tracer@ysyx.org>
2025-10-25 15:29:39 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:29:39 up  2:47,  1 user,  load average: 3.00, 1.65, 0.89

--- am-kernels/kernels/bad-apple/bad-apple.c
+++ am-kernels/kernels/bad-apple/bad-apple.c
@@ -36,9 +36,8 @@ int main() {
 
   printf("\033[H\033[J"); // screan_clear
 
-  printf("!!!\n");
   bool has_audio = io_read(AM_AUDIO_CONFIG).present;
-  printf("???\n");
+
   // bool has_audio = false;
   if (has_audio) {
     io_write(AM_AUDIO_CTRL, AUDIO_FREQ, AUDIO_CHANNEL, 1024);
@@ -56,6 +55,7 @@ int main() {
       }
       putch('\n');
     }
+    printf("!!!\n");
     printf("%x", io_read(AM_AUDIO_CONFIG).bufsize);
     if (has_audio) {
       int should_play = (AUDIO_FREQ / FPS) * sizeof(int16_t) * AUDIO_CHANNEL;
@@ -70,6 +70,7 @@ int main() {
         should_play -= len;
       }
     }
+    printf("???\n");
     // printf("%d\n", 1);
     uint64_t next = now + (1000000 / FPS);
     sleep_until(next);
