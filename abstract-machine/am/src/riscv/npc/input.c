b44836641a7ee1a72123adcc5baf5a3f00ae87a6
tracer-ysyx <tracer@ysyx.org>
2025-10-26 01:30:56 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:30:56 up 12:48,  1 user,  load average: 0.53, 0.82, 0.66

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -11,5 +11,5 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t code = signal & ~KEYDOWN_MASK;
   kbd->keydown = (bool)(signal & KEYDOWN_MASK);
   kbd->keycode = code;
-  // printf("%d\n", signal);
+  printf("%d\n", signal);
 }
