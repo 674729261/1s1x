785191b7bad6a5c16c78715febaa2236b2f0b749
tracer-ysyx <tracer@ysyx.org>
2025-10-25 23:30:36 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:30:36 up 10:48,  1 user,  load average: 1.03, 1.93, 1.53

--- am-kernels/kernels/bad-apple/bad-apple.c
+++ am-kernels/kernels/bad-apple/bad-apple.c
@@ -51,7 +51,11 @@ int main() {
     for (int y = 0; y < VIDEO_ROW; y++) {
       for (int x = 0; x < VIDEO_COL; x++) {
         uint8_t p = getbit(f->pixel, y * VIDEO_COL + x);
-        putch(p ? CHAR_BLACK : CHAR_WHITE);
+        // putch(p ? CHAR_BLACK : CHAR_WHITE);
+        if (p)
+          putch(' ');
+        else
+          printf("█");
       }
       putch('\n');
     }
