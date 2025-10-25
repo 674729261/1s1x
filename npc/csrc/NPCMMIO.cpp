4bd492d047641af510470d1c4f37d4698a114846
tracer-ysyx <tracer@ysyx.org>
2025-10-26 01:29:23 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:29:23 up 12:47,  1 user,  load average: 0.93, 0.91, 0.67

--- npc/csrc/NPCMMIO.cpp
+++ npc/csrc/NPCMMIO.cpp
@@ -52,8 +52,10 @@ std::optional<uint32_t> NPCemu::readMMIO(int raddr) {
 
   if (device_settings.enable_keyboard && raddr == KeyboardPort) {
     uint32_t key;
-    if (key_queue.Pop(key))
+    if (key_queue.Pop(key)) {
+      std::println("--{:#08x}", key);
       return key;
+    }
 
     return 0;
   }
