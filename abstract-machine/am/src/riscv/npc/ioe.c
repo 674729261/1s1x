b7d1f8cc450b45ed7a8dd8fc7370da8370f4bfc8
tracer-ysyx <tracer@ysyx.org>
2025-10-26 01:09:33 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:09:33 up 12:27,  1 user,  load average: 0.56, 0.50, 0.46

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 void __am_timer_init();
 void __am_gpu_init();
@@ -54,5 +55,8 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_read(int reg, void *buf) {
+  printf("%d\n", reg);
+  ((handler_t)lut[reg])(buf);
+}
 void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
