3d5f7393154a5d0b8c9f1e9a70be6ca8b58d4f49
tracer-ysyx <tracer@ysyx.org>
2025-10-26 17:46:46 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:46:46 up  5:51,  1 user,  load average: 0.55, 0.57, 0.40

--- npc/csrc/NPCemu.cpp
+++ npc/csrc/NPCemu.cpp
@@ -55,7 +55,7 @@ void NPCemu::device_update_loop() {
   signal(SIGTERM, SIG_DFL);
   using namespace std::chrono;
   auto last = steady_clock::now();
-  while (device_running) {
+  while (device_alive) {
     if (device_running) {
       auto now = steady_clock::now();
       if (duration_cast<microseconds>(now - last).count() < 1'000'000 / 60)
