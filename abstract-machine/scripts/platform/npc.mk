cdd568c66e7e2e3ae495645ab7a8023a35663a7e
tracer-ysyx <tracer@ysyx.org>
2025-10-26 15:42:27 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:42:27 up  3:46,  1 user,  load average: 0.90, 0.82, 0.70

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -28,6 +28,6 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME) sim_full_speed PROG=$(IMAGE).bin 
+	$(MAKE) -C $(NPC_HOME) sim_full_speed PROG=$(IMAGE).bin NPC_FLAGS="--batch"
 
 .PHONY: insert-arg
