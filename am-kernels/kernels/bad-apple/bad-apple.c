e6c03125fc14698d84ef044987615f46aa54fe5b
tracer-ysyx <tracer@ysyx.org>
2025-10-26 22:42:49 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:42:49 up 10:47,  1 user,  load average: 0.94, 1.09, 1.11

--- am-kernels/kernels/bad-apple/bad-apple.c
+++ am-kernels/kernels/bad-apple/bad-apple.c
@@ -47,7 +47,9 @@ int main() {
     sbuf.start = &audio_payload;
   }
   uint64_t now = io_read(AM_TIMER_UPTIME).us;
-
+  if (has_keyboard)
+    while (io_read(AM_INPUT_KEYBRD).keycode != AM_KEY_NONE)
+      ;
   for (; f < fend; f++) {
     printf("\033[0;0H"); // reset cursor
     for (int y = 0; y < VIDEO_ROW; y++) {
