db2439e95aaf4a35a762da98ad1478ff267f4f60
tracer-ysyx <tracer@ysyx.org>
2025-10-27 18:14:27 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:14:27 up  5:19,  1 user,  load average: 0.25, 0.14, 0.16

--- npc/csrc/EMU/NPCemu/NPCMMIO.cpp
+++ npc/csrc/EMU/NPCemu/NPCMMIO.cpp
@@ -1,4 +1,5 @@
 #include <Simulators/NPCemu.h>
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 #include <print>
@@ -112,6 +113,8 @@ void NPCemu::writeMMIO(uint32_t waddr, uint32_t mask32, uint32_t wdata) {
       write_mask(tmp, mask32, wdata);
       if (tmp) {
         VideoBase.back_ptr = VideoBase.front_ptr.exchange(VideoBase.back_ptr);
+        uint8_t *f = VideoBase.front_ptr;
+        std::copy(f, f + NPCemu::VMemSize, VideoBase.back_ptr);
       }
       VideoBase.sync = tmp;
     }
