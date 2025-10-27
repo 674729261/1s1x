cc08548dfa7bc5ccec2b78af1e7517a9bc4f8626
tracer-ysyx <tracer@ysyx.org>
2025-10-27 18:00:10 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:00:10 up  5:05,  1 user,  load average: 0.29, 0.33, 0.27

--- npc/csrc/EMU/NPCemu/NPCemu.cpp
+++ npc/csrc/EMU/NPCemu/NPCemu.cpp
@@ -33,7 +33,8 @@ void NPCemu::init_ioe() {
   if (device_settings.enable_vga) {
     VideoBase.vmem1 = std::make_unique<uint8_t[]>(VMemSize);
     VideoBase.vmem2 = std::make_unique<uint8_t[]>(VMemSize);
-    VideoBase.front_ptr = VideoBase.vmem2.get();
+    VideoBase.front_ptr = VideoBase.vmem1.get();
+    VideoBase.back_ptr = VideoBase.vmem2.get();
     VideoBase.screen_size_info = (ScreenWidth << 16) | ScreenHeight;
     // init_vga();
   }
