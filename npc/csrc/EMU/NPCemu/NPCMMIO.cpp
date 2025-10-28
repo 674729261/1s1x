03025c68adc6a242d50251b4b32aded316159780
tracer-ysyx <tracer@ysyx.org>
2025-10-29 01:24:36 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:24:36 up  1:58,  1 user,  load average: 0.66, 0.46, 0.55

--- npc/csrc/EMU/NPCemu/NPCMMIO.cpp
+++ npc/csrc/EMU/NPCemu/NPCMMIO.cpp
@@ -113,13 +113,13 @@ void NPCemu::writeMMIO(uint32_t waddr, uint32_t mask32, uint32_t wdata) {
           waddr, AudioPort, AudioPort + sizeof(AudioBase.reg_ctl));
       AudioReg_id != -1) {
     ensure_audio_enabled();
+    log_and_throw<std::logic_error>("Init Audio {}", AudioReg_id);
     // spdlog::info("Writing to AudioBase[{}]", AudioReg_id);
     write_mask(std::bit_cast<std::array<uint32_t, AudioBase_t::n_regs>>(
                    AudioBase.reg_ctl)[AudioReg_id],
                mask32, wdata);
     if (AudioBase.reg_ctl.reg_init) {
       init_audio();
-      log_and_throw<std::logic_error>("Init Audio");
       AudioBase.reg_ctl.reg_init = 0;
     }
     return;
