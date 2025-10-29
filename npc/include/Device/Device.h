0ad7a4cee2914e2135b21d47e3977024587c9b27
tracer-ysyx <tracer@ysyx.org>
2025-10-29 21:35:02 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:35:02 up  9:36,  1 user,  load average: 2.69, 1.48, 0.86

--- npc/include/Device/Device.h
+++ npc/include/Device/Device.h
@@ -16,8 +16,7 @@ public:
   };
 
   Devices(DeviceSettings ds)
-      : KeyboardBase({}), AudioBase({}), VideoBase(), RTC({}),
-        device_settings(ds) {}
+      : KeyboardBase{}, AudioBase{}, VideoBase{}, RTC{}, device_settings(ds) {}
 
   void writeMMIO(uint32_t waddr, uint32_t mask32, uint32_t wdata);
   std::optional<uint32_t> readMMIO(int raddr);
