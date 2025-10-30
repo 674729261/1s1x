7c739907109c8b6a027741b0466961cea7bfbc27
tracer-ysyx <tracer@ysyx.org>
2025-10-30 23:58:19 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:58:19 up 11:25,  1 user,  load average: 1.69, 1.87, 1.46

--- npc/include/Device/Device.h
+++ npc/include/Device/Device.h
@@ -41,7 +41,7 @@ public:
   void ensure_audio_enabled();
   void ensure_vga_enabled();
 
-  bool is_quit() { return quit.load(std::memory_order_relaxed); }
+  bool is_quit() { return quit.load(std::memory_order_acquire); }
   void reset_quit() { quit.store(false); }
 
   const DeviceSettings device_settings;
