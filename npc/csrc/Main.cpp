cbfaa61d00aad9f21e4e22979ae5acce8a8b3be3
tracer-ysyx <tracer@ysyx.org>
2025-10-30 12:54:32 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:54:32 up 21 min,  1 user,  load average: 0.84, 0.93, 0.81

--- npc/csrc/Main.cpp
+++ npc/csrc/Main.cpp
@@ -32,8 +32,8 @@ int main(int argc, char *argv[]) {
                         config.itracer, config.mtracer, config.sz_irb,
                         config.use_ftracer, config.path_elf);
 
-  if (config.difftest)
-    monitor.addReference(nemu);
+  if (config.difftest) { // monitor.addReference(nemu);
+  }
   try {
     result = monitor.start();
 
@@ -43,9 +43,6 @@ int main(int argc, char *argv[]) {
   }
 
   emu = nullptr;
-  println(cerr, "!!");
-  nemu = nullptr;
-  println(cerr, "??");
 
   spdlog::shutdown();
   return result;
