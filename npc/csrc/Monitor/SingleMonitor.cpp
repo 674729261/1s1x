ca4dbe0ec88625e6fa1b46c5cc53e5b76def8442
tracer-ysyx <tracer@ysyx.org>
2025-10-31 17:05:13 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:05:13 up  3:07,  1 user,  load average: 0.35, 0.37, 0.35

--- npc/csrc/Monitor/SingleMonitor.cpp
+++ npc/csrc/Monitor/SingleMonitor.cpp
@@ -37,8 +37,8 @@ SingleMonitor::SingleMonitor(std::shared_ptr<RISCV32> emu,
   if (repl.history_load(tmp_path))
     spdlog::info("Loaded {} history commands from {}", repl.history_size(),
                  tmp_path.string());
+  tracer = std::make_shared<Tracer>(ftracer, elf_path, irb);
   if (ftracer || elf_path != "" || irb) {
-    tracer = std::make_shared<Tracer>(ftracer, elf_path, irb);
     emus.front()->tie_tracer(tracer);
   }
 
