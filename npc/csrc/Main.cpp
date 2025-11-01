a7b0eedcba40016489e2f17301c57d6c0ed8905a
tracer-ysyx <tracer@ysyx.org>
2025-11-01 20:32:49 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:32:49 up  6:12,  1 user,  load average: 0.38, 0.67, 0.96

--- npc/csrc/Main.cpp
+++ npc/csrc/Main.cpp
@@ -25,7 +25,8 @@ int main(int argc, char *argv[]) {
     std::println(std::cerr, "Error : {}", err.what());
     std::terminate();
   }
-
+  nemu = nullptr;
+  println(cerr, "!!!");
   emu = make_shared<NPCemu>();
   int result;
   SingleMonitor monitor(emu, config.mem_size, config.image_path,
@@ -44,7 +45,6 @@ int main(int argc, char *argv[]) {
     std::terminate();
   }
   nemu = nullptr;
-  println(cerr, "!!!");
 
   spdlog::shutdown();
   return result;
