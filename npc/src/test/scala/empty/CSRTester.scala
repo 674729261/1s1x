dec03fb0132e7edf1ec0ee7e229f1c888cd8710a
tracer-ysyx <tracer@ysyx.org>
2025-11-03 02:30:12 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:30:12 up  1:02,  1 user,  load average: 0.23, 0.69, 0.86

--- npc/src/test/scala/empty/CSRTester.scala
+++ npc/src/test/scala/empty/CSRTester.scala
@@ -61,6 +61,17 @@ class CSRTester extends AnyFlatSpec {
       dut.io.rdata.expect(4.U(32.W))
       dut.io.csr.poke(0x305.U(12.W))
       dut.io.rdata.expect("hdeadbeef".U(32.W))
+      dut.io.mtvec.expect("hdeadbeef".U(32.W))
+
+      dut.io.interruption.poke(true.B)
+      dut.io.new_cause.poke(0x12345678.U(32.W))
+      dut.io.cur_pc.poke(0x8000ff0.U(32.W))
+      dut.clock.step()
+      dut.io.csr.poke(0x342.U(12.W))
+      dut.io.rdata.expect(0x12345678.U(32.W))
+      dut.io.csr.poke(0x341.U(12.W))
+      dut.io.rdata.expect(0x8000ff0.U(32.W))
+      dut.io.mepc.expect(0x8000ff0.U(32.W))
     }
   }
 }
