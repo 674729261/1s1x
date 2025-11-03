2352654a606eb1cdc22779144090d894fb6e8e7c
tracer-ysyx <tracer@ysyx.org>
2025-11-03 17:35:25 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:35:25 up  3:59,  1 user,  load average: 0.78, 1.18, 1.20

--- npc/src/main/scala/empty/GPR.scala
+++ npc/src/main/scala/empty/GPR.scala
@@ -3,32 +3,31 @@ package empty
 import chisel3._
 import chisel3.util._
 
-class GPR(CNT: Int) extends Module {
-  val log_cnt = log2Ceil(CNT)
+class GPR extends Module {
   val io = IO(new Bundle {
-    val raddr1 = Input(UInt(log_cnt.W))
-    val raddr2 = Input(UInt(log_cnt.W))
-    val waddr = Input(UInt(log_cnt.W))
-    val wdata = Input(UInt(CNT.W))
+    val raddr1 = Input(UInt(5.W))
+    val raddr2 = Input(UInt(5.W))
+    val waddr = Input(UInt(5.W))
+    val wdata = Input(UInt(32.W))
     val wen = Input(Bool())
 
-    val rdata1 = Output(UInt(CNT.W))
-    val rdata2 = Output(UInt(CNT.W))
+    val rdata1 = Output(UInt(32.W))
+    val rdata2 = Output(UInt(32.W))
   })
 
   val register_bank = Wire(new Bundle {
-    val regs = Vec(CNT - 1, UInt(CNT.W))
-    val x0 = UInt(CNT.W)
+    val regs = Vec(31, UInt(32.W))
+    val x0 = UInt(32.W)
   })
-  val ENs = Wire(Vec(CNT, Bool()))
+  val ENs = Wire(Vec(32, Bool()))
 
-  for (i <- 1 until CNT) {
+  for (i <- 1 until 32) {
     register_bank
       .regs(i - 1) := RegEnable(io.wdata, io.wen && ENs(i))
   }
-  register_bank.x0 := 0.U(CNT.W)
+  register_bank.x0 := 0.U(32.W)
 
-  val data = register_bank.asTypeOf(Vec(CNT, UInt(CNT.W)))
+  val data = register_bank.asTypeOf(Vec(32, UInt(32.W)))
 
   io.rdata1 := data(io.raddr1)
   io.rdata2 := data(io.raddr2)
