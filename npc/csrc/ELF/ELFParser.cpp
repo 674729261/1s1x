dc021b0dd6c55e4ed29f9a454a75a1012ae3d8f8
tracer-ysyx <tracer@ysyx.org>
2025-11-03 23:15:36 +0800

    > sim RTL ftracer ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:15:36 up  9:39,  1 user,  load average: 0.64, 0.50, 0.43

--- npc/csrc/ELF/ELFParser.cpp
+++ npc/csrc/ELF/ELFParser.cpp
@@ -42,8 +42,10 @@ void ProgSymTab::init_and_parse(std::string_view elf_path) {
       for (unsigned int j = 0; j < symbols.get_symbols_num(); ++j) {
         symbols.get_symbol(j, name, value, size, bind, type, section_index,
                            other);
-        pc_offset = std::min(pc_offset, static_cast<uint32_t>(value));
-        max_addr = std::max(max_addr, static_cast<uint32_t>(value + size));
+        if (type == STT_FUNC) {
+          pc_offset = std::min(pc_offset, static_cast<uint32_t>(value));
+          max_addr = std::max(max_addr, static_cast<uint32_t>(value + size));
+        }
       }
       table.symbol_map.resize(max_addr - pc_offset);
       std::fill(table.symbol_map.begin(), table.symbol_map.end(), -1);
