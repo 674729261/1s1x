1228b1326a5552ed9d39c2f914941da0d2c8f6a9
tracer-ysyx <tracer@ysyx.org>
2025-11-03 17:36:42 +0800

    > sim RTL full speed ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:36:42 up  4:00,  1 user,  load average: 2.11, 1.52, 1.32

--- npc/src/main/scala/empty/ALU.scala
+++ npc/src/main/scala/empty/ALU.scala
@@ -8,14 +8,22 @@ class Adder(WIDTH: Int) extends RawModule {
     val Cin = Input(Bool())
     val out = Output(UInt(WIDTH.W))
     val Cout = Output(Bool())
+    val overflow = Output(Bool())
   })
   val result33 = Wire(UInt((WIDTH + 1).W))
-  result33 := Cat(0.U(1.W), io.A) + Cat(0.U(1.W), io.B) + Cat(
-    0.U(WIDTH.W),
-    io.Cin
-  )
+  // result33 := Cat(0.U(1.W), io.A) + Cat(0.U(1.W), io.B) + Cat(
+  //   0.U(WIDTH.W),
+  //   io.Cin
+  // )
+  result33 := io.A +& io.B + io.Cin
   io.out := result33((WIDTH - 1), 0)
   io.Cout := result33(WIDTH)
+
+  val signA = io.A(WIDTH - 1)
+  val signB = io.B(WIDTH - 1)
+  val signS = result33(WIDTH - 1)
+
+  io.overflow := (signA === signB) && (signA =/= signS)
 }
 
 class ALU(WIDTH: Int) extends RawModule {
@@ -37,12 +45,14 @@ class ALU(WIDTH: Int) extends RawModule {
   adder.io.Cin := false.B
   io.out := adder.io.out
 
-  var log_width = 0;
-  var upper = 1;
-  while (upper < WIDTH) {
-    upper *= 2
-    log_width += 1
-  }
+  // var log_width = 0;
+  // var upper = 1;
+  // while (upper < WIDTH) {
+  //   upper *= 2
+  //   log_width += 1
+  // }
+  val log_width = log2Ceil(WIDTH)
+
   when(~io.is_force_add) {
     switch(io.funct3) {
       is("b000".U(3.W)) { // add / sub / slt
@@ -54,12 +64,17 @@ class ALU(WIDTH: Int) extends RawModule {
       is("b010".U(3.W)) { // slt
         adder.io.B := ~io.B
         adder.io.Cin := true.B
+        // io.out := Cat(
+        //   0.U(31.W),
+        //   (io.A(WIDTH - 1) & ~io.B(WIDTH - 1)) | // - < +
+        //     ((~(io.A(WIDTH - 1) ^ io.B(WIDTH - 1))) & adder.io.out(
+        //       WIDTH - 1
+        //     )) // A - B < 0
+        //   // io.A.asSInt < io.B.asSInt
+        // )
         io.out := Cat(
-          0.U(31.W),
-          (io.A(WIDTH - 1) & ~io.B(WIDTH - 1)) | // - < +
-            ((~(io.A(WIDTH - 1) ^ io.B(WIDTH - 1))) & adder.io.out(
-              WIDTH - 1
-            )) // A - B < 0
+          0.U((WIDTH - 1).W),
+          adder.io.out(WIDTH - 1) ^ adder.io.overflow
         )
       }
       is("b011".U(3.W)) { // sltu
