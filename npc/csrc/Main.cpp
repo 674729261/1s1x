afc6243b92a95b57db3f6c0791cd7c7b445b2f91
tracer-ysyx <tracer@ysyx.org>
2025-11-04 17:40:13 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:40:12 up  6:02,  1 user,  load average: 0.78, 0.98, 0.96

--- npc/csrc/Main.cpp
+++ npc/csrc/Main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
   emu = make_shared<NPCemu>();
   int result;
-  SingleMonitor monitor(emu, config.mem_size, config.image_path,
+  SingleMonitor monitor(refemu, config.mem_size, config.image_path,
                         config.device_settings, config.batch_mode,
                         config.itracer, config.mtracer, config.sz_irb,
                         config.use_ftracer, config.path_elf);
