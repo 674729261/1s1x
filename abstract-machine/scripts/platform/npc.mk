04e7d11a12c945a1ebc8605c515fe183358efcab
tracer-ysyx <tracer@ysyx.org>
2025-11-05 00:12:57 +0800

    > sim swap ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:12:57 up  2:06,  1 user,  load average: 0.13, 0.82, 0.81

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -28,9 +28,11 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
+	rm -rf $(NPC_HOME)/obj_dir
 	$(MAKE) -C $(NPC_HOME) sim_full_speed PROG=$(IMAGE).bin NPC_FLAGS="--batch"
 
 run_difftest: insert-arg
+	rm -rf $(NPC_HOME)/obj_dir
 	$(MAKE) -C $(NPC_HOME) sim PROG=$(IMAGE).bin NPC_FLAGS="--batch"
 
 run_swap: insert-arg
