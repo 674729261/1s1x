2c938a053e2e2e5664c7ec621c5129b8f7bae965
tracer-ysyx <tracer@ysyx.org>
2025-11-13 21:52:59 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:52:59 up 38 min,  1 user,  load average: 0.72, 0.31, 0.28

--- npc/src/main/scala/empty/CPU.scala
+++ npc/src/main/scala/empty/CPU.scala
@@ -5,6 +5,7 @@ import empty.IFU
 import empty.IDU
 import empty.EXU
 import empty.WBU
+import _root_.empty.empty.LSU
 
 class CPU(init_pc: UInt) extends Module with RequireAsyncReset {
   val io = IO(new Bundle {
@@ -26,6 +27,7 @@ class CPU(init_pc: UInt) extends Module with RequireAsyncReset {
   val ifu = Module(new IFU)
   val idu = Module(new IDU)
   val exu = Module(new EXU)
+  val lsu = Module(new LSU)
   val wbu = Module(new WBU)
 
   val pc = RegNext(next = Cat(wbu.out.dnpc(31, 1), 0.U(1.W)), init = init_pc)
@@ -41,17 +43,19 @@ class CPU(init_pc: UInt) extends Module with RequireAsyncReset {
 
   idu.in <> ifu.out
   exu.in <> idu.out
-  wbu.in <> exu.out
+  lsu.in <> exu.out
+  wbu.in <> lsu.out
 
-  exu.fetch_port_in.csr_rdata := csrBank.io.rdata
-  exu.fetch_port_in.csr_mepc := csrBank.io.mepc
-  exu.fetch_port_in.csr_mtvec := csrBank.io.mtvec
-  exu.fetch_port_in.gpr_rdata1 := gpr.io.rdata1
-  exu.fetch_port_in.gpr_rdata2 := gpr.io.rdata2
-  exu.fetch_port_in.mem_rdata := ramLoader.io.out
+  idu.fetch_port_in.csr_rdata := csrBank.io.rdata
+  idu.fetch_port_in.csr_mepc := csrBank.io.mepc
+  idu.fetch_port_in.csr_mtvec := csrBank.io.mtvec
+  idu.fetch_port_in.gpr_rdata1 := gpr.io.rdata1
+  idu.fetch_port_in.gpr_rdata2 := gpr.io.rdata2
 
-  gpr.io.raddr1 := exu.fetch_port_out.gpr_raddr1
-  gpr.io.raddr2 := exu.fetch_port_out.gpr_raddr2
+  lsu.fetch_port_in.mem_rdata := ramLoader.io.out
+
+  gpr.io.raddr1 := idu.fetch_port_out.gpr_raddr1
+  gpr.io.raddr2 := idu.fetch_port_out.gpr_raddr2
   gpr.io.waddr := wbu.out.gpr_waddr
   gpr.io.wdata := wbu.out.gpr_wdata
   gpr.io.wen := wbu.out.gpr_wen
@@ -63,25 +67,25 @@ class CPU(init_pc: UInt) extends Module with RequireAsyncReset {
   csrBank.io.new_cause := wbu.out.csr_mcause
   csrBank.io.wen := wbu.out.csr_wen
 
-  ramWriter.io.word := wbu.out.mem_wdata
-  ramWriter.io.is_word := wbu.out.is_word
-  ramWriter.io.is_half := wbu.out.is_half
-  ramWriter.io.is_byte := wbu.out.is_byte
-  ramWriter.io.lower2bit := wbu.out.mem_lower2bit
+  ramWriter.io.word := lsu.fetch_port_out.mem_wdata
+  ramWriter.io.is_word := lsu.fetch_port_out.is_word
+  ramWriter.io.is_half := lsu.fetch_port_out.is_half
+  ramWriter.io.is_byte := lsu.fetch_port_out.is_byte
+  ramWriter.io.lower2bit := lsu.fetch_port_out.mem_lower2bit
   io.wdata := ramWriter.io.out
   io.wmask := ramWriter.io.mask
-  io.waddr := wbu.out.mem_waddr
-  io.raddr := exu.fetch_port_out.mem_raddr
-  io.valid := exu.fetch_port_out.mem_rvalid | wbu.out.mem_wen
-  io.wen := wbu.out.mem_wen
+  io.waddr := lsu.fetch_port_out.mem_waddr
+  io.raddr := lsu.fetch_port_out.mem_raddr
+  io.valid := lsu.fetch_port_out.mem_valid
+  io.wen := lsu.fetch_port_out.mem_wen
 
-  ramLoader.io.is_byte := exu.fetch_port_out.is_byte
-  ramLoader.io.is_half := exu.fetch_port_out.is_half
-  ramLoader.io.is_word := exu.fetch_port_out.is_word
-  ramLoader.io.is_unsigned := exu.fetch_port_out.is_unsigned
+  ramLoader.io.is_byte := lsu.fetch_port_out.is_byte
+  ramLoader.io.is_half := lsu.fetch_port_out.is_half
+  ramLoader.io.is_word := lsu.fetch_port_out.is_word
+  ramLoader.io.is_unsigned := lsu.fetch_port_out.is_unsigned
   // ramLoader.io.word := memory_proxy.io.rdata
   ramLoader.io.word := io.rdata
 
-  ramLoader.io.lower2bit := exu.fetch_port_out.mem_rlower2bit
+  ramLoader.io.lower2bit := lsu.fetch_port_out.mem_rlower2bit
   io.ebreak := wbu.out.ebreak
 }
