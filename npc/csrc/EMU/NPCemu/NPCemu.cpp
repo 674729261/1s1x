c0eef2b34aada5d32ccdffae95e2b7a184a13383
tracer-ysyx <tracer@ysyx.org>
2025-11-25 12:55:02 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:55:02 up 43 min,  1 user,  load average: 2.09, 1.79, 1.03

--- npc/csrc/EMU/NPCemu/NPCemu.cpp
+++ npc/csrc/EMU/NPCemu/NPCemu.cpp
@@ -55,7 +55,10 @@ void NPCemu::step() {
       }
 #endif
     }
+
     print("Inst : {:08x}\r", dut.io_instr);
+    if ((dut.io_instr & 0xFF) == 0x73)
+      println();
   } while (!dut.io_ok_to_step);
   inst_count++;
   if (dut.io_ebreak) [[unlikely]] {
