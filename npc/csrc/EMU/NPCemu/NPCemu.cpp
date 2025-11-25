a67cef0b1d101ddc60c4733cb53a2ff319e8cced
tracer-ysyx <tracer@ysyx.org>
2025-11-25 22:16:29 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:16:29 up  4:33,  1 user,  load average: 0.82, 1.30, 0.97

--- npc/csrc/EMU/NPCemu/NPCemu.cpp
+++ npc/csrc/EMU/NPCemu/NPCemu.cpp
@@ -92,14 +92,14 @@ void NPCemu::step() {
     dut.eval();
     if (dut.io_ebreak)
       is_ebreak = true;
-    // std::print("!");
+    std::print("!");
     // println("ok ! {}", ready_to_step);
   }
   inst_count++;
   if (is_ebreak) [[unlikely]] {
     EMUstate = RISCV32::Interrupt::EBREAK;
   }
-  // std::println();
+  std::println();
 }
 
 void NPCemu::syncCPUState() {
