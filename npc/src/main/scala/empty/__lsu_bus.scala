48e1b7b84840e03b5346291f4990bd90999b251d
tracer-ysyx <tracer@ysyx.org>
2025-11-27 19:37:20 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:37:20 up  7:14,  1 user,  load average: 2.84, 1.57, 1.10

--- npc/src/main/scala/empty/__lsu_bus.scala
+++ npc/src/main/scala/empty/__lsu_bus.scala
@@ -36,7 +36,7 @@ class __lsu_fetch_bus() extends Module with RequireAsyncReset {
 
   counter_next := Mux(
     (state === sWAIT_READY && state_next === sBUSY) || (state === sWAIT_REQ && state_next === sFETCH),
-    random_delay,
+    10.U,
     Mux(
       counter_delay === 0.U(32.W),
       0.U(32.W),
