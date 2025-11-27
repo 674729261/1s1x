9430f0b90aded5c851e6cafcf26bd3fd1cf1a427
tracer-ysyx <tracer@ysyx.org>
2025-11-27 18:20:43 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:20:43 up  5:58,  1 user,  load average: 1.63, 0.97, 0.77

--- npc/src/main/scala/empty/IFU.scala
+++ npc/src/main/scala/empty/IFU.scala
@@ -31,7 +31,7 @@ class IFU() extends Module with RequireAsyncReset {
   val inst_reg =
     RegEnable(fetch_port.instr, state === sWAIT_RESP && fetch_port.respValid)
 
-  fetch_port.ifu_addr := Mux(state === sIDLE, in.pc, 0.U(32.W))
+  fetch_port.ifu_addr := Mux(state === sIDLE, in.pc, 22.U(32.W))
   fetch_port.reqValid := state === sIDLE
   fetch_port.respReady := state === sWAIT_RESP
   out.valid := state === sWAIT
