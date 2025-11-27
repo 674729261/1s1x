8adad07c2c3d8b1e55112c9bee892d42a8bce0b4
tracer-ysyx <tracer@ysyx.org>
2025-11-27 13:39:17 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:39:17 up  1:16,  1 user,  load average: 1.19, 1.31, 1.51

--- npc/csrc/EMU/NPCemu/NPCemu.cpp
+++ npc/csrc/EMU/NPCemu/NPCemu.cpp
@@ -37,7 +37,8 @@ void NPCemu::step() {
   while (!ready_to_step) {
     ready_to_step = dut.io_ok_to_step;
     // addr_t pc = dut.io_pc;
-    println("XX = {:08x}, reqV = {}, reqR = {}", dut.io_inst_bus_ifu_addr,
+    println("s = {}, XX = {:08x}, reqV = {}, reqR = {}",
+            dut.rootp->CPU__DOT__ifu__DOT__state, dut.io_inst_bus_ifu_addr,
             dut.io_inst_bus_reqValid, dut.io_inst_bus_reqReady);
     proxy.fetch_inst(*devices, dut);
     proxy.fetch_ram(*devices, dut);
