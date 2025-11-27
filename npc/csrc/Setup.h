7c94dad3c44ca5736758fee897a7bdb62b9e7216
tracer-ysyx <tracer@ysyx.org>
2025-11-27 17:52:35 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:52:35 up  5:29,  1 user,  load average: 1.93, 1.06, 0.65

--- npc/csrc/Setup.h
+++ npc/csrc/Setup.h
@@ -3,7 +3,7 @@
 #include "Simulators/RISCV32.h"
 #include <Simulators/NEMUemu.h>
 #include <Simulators/NPCemu.h>
-#include <V__npc_cpu.h>
+#include <Vnpc_top.h>
 #include <argparse/argparse.hpp>
 #include <cstdint>
 #include <memory>
