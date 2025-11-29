deb009a5a8ee43dca6fa3c8151dddc9fec96da01
tracer-ysyx <tracer@ysyx.org>
2025-11-30 02:19:14 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:19:14 up  2:40,  1 user,  load average: 2.18, 1.07, 0.73

--- npc/src/main/scala/empty/__inst_fetch_bus.scala
+++ npc/src/main/scala/empty/__inst_fetch_bus.scala
@@ -1,29 +1,12 @@
 package empty.empty
 import chisel3._
 import chisel3.util._
+import empty.InstBus
+import _root_.empty.InstBus
 import chisel3.util.random.LFSR
-import empty.AXI_Lite
-
-object set_flipped_AXI_zero {
-  def apply(axi: AXI_Lite) = {
-    axi.ar.arready := 0.U
-
-    axi.r.rdata := 0.U
-    axi.r.rresp := 0.U
-    axi.r.rvalid := 0.U
-
-    axi.aw.awready := 0.U
-
-    axi.w.wready := 0.U
-
-    axi.b.bresp := 0.U
-    axi.b.bvalid := 0.U
-  }
-}
 
 class __inst_fetch_bus() extends Module with RequireAsyncReset {
-  val fetch_port = IO(Flipped(new AXI_Lite))
-  set_flipped_AXI_zero(fetch_port)
+  val fetch_port = IO(Flipped(new InstBus))
   val io = IO(new Bundle {
     val addr = Output(UInt(32.W))
     val valid = Output(Bool())
@@ -33,16 +16,16 @@ class __inst_fetch_bus() extends Module with RequireAsyncReset {
   val state_next = Wire(UInt(sBUSY.getWidth.W))
   val state = RegEnable(state_next, sWAIT_REQ, true.B)
 
-  val random_delay = Cat(0.U(29.W), LFSR(3))
+  val random_delay = Cat(0.U(28.W), LFSR(4))
   val counter_next = Wire(UInt(32.W))
   val counter_delay = RegEnable(counter_next, 0.U(32.W), true.B)
 
   state_next := MuxLookup(state, sWAIT_REQ)(
     Seq(
       sBUSY -> Mux(counter_delay === 0.U(32.W), sWAIT_REQ, sBUSY),
-      sWAIT_REQ -> Mux(fetch_port.ar.arvalid, sFETCH, sWAIT_REQ),
+      sWAIT_REQ -> Mux(fetch_port.reqValid, sFETCH, sWAIT_REQ),
       sFETCH -> Mux(counter_delay === 0.U(32.W), sWAIT_READY, sFETCH),
-      sWAIT_READY -> Mux(fetch_port.r.rready, sBUSY, sWAIT_READY)
+      sWAIT_READY -> Mux(fetch_port.respReady, sBUSY, sWAIT_READY)
     )
   )
 
@@ -56,11 +39,11 @@ class __inst_fetch_bus() extends Module with RequireAsyncReset {
     )
   )
 
-  fetch_port.ar.arready := state === sWAIT_REQ
-  fetch_port.r.rvalid := state === sWAIT_READY
+  fetch_port.reqReady := state === sWAIT_REQ
+  fetch_port.respValid := state === sWAIT_READY
 
-  io.addr := fetch_port.ar.araddr
+  io.addr := fetch_port.ifu_addr
   io.valid := state === sWAIT_REQ && state_next === sFETCH
-  fetch_port.r.rdata := Mux(state === sWAIT_READY, io.instr, 0.U(32.W))
+  fetch_port.instr := Mux(state === sWAIT_READY, io.instr, 0.U(32.W))
 
 }
