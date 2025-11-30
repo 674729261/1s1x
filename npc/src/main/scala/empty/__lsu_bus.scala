2378e484a865cbfb245293a38ee42476602dddf3
tracer-ysyx <tracer@ysyx.org>
2025-11-30 22:25:52 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:25:52 up  5:47,  1 user,  load average: 3.93, 2.05, 1.33

--- npc/src/main/scala/empty/__lsu_bus.scala
+++ npc/src/main/scala/empty/__lsu_bus.scala
@@ -17,11 +17,11 @@ class __lsu_fetch_bus() extends Module with RequireAsyncReset {
 
   val random_delay = LFSR(3)
 
-  val ar_timeup = random_delay === 6.U(3.W)
+  val ar_timeup = random_delay === 2.U(3.W)
   val r_timeup = random_delay === 3.U(3.W)
-  val aw_timeup = random_delay === 1.U(3.W)
+  val aw_timeup = random_delay === 4.U(3.W)
   val w_timeup = random_delay === 5.U(3.W)
-  val b_timeup = random_delay === 0.U(3.W)
+  val b_timeup = random_delay === 6.U(3.W)
 
   val ar_fire = fetch_port.ar.arvalid && fetch_port.ar.arready
   val r_fire = fetch_port.r.rready && fetch_port.r.rvalid
