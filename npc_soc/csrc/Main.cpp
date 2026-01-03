ccaf6d87baf6a593044e48a29d52f91b0d3b54ea
tracer-ysyx <tracer@ysyx.org>
2026-01-03 23:44:40 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:44:40 up  1:22,  1 user,  load average: 2.21, 1.59, 1.06

--- npc_soc/csrc/Main.cpp
+++ npc_soc/csrc/Main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]) {
   dut->trace(m_trace.get(), 5);
   m_trace->open("waveform.vcd");
   static vluint64_t sim_time = 0;
-  while (!contextp->gotFinish() && sim_time <= 2 * 1000000) {
+  while (!contextp->gotFinish() && sim_time <= 2 * 10000) {
     dut->clock = 0;
     dut->eval();
     m_trace->dump(sim_time);
