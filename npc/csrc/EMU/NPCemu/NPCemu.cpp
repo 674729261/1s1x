8029ca47f5e1808e38dbe8fe6aaaa0dbdd0fb304
tracer-ysyx <tracer@ysyx.org>
2026-01-03 17:52:50 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:52:50 up  3:41,  1 user,  load average: 0.51, 0.59, 0.47

--- npc/csrc/EMU/NPCemu/NPCemu.cpp
+++ npc/csrc/EMU/NPCemu/NPCemu.cpp
@@ -18,7 +18,7 @@ NPCemu::NPCemu()
     : RISCV32(), trapped(0), context(), dut(&context), inst_count(0) {
   Verilated::traceEverOn(true);
   m_trace = new VerilatedVcdC;
-  dut.trace(m_trace, 3);
+  dut.trace(m_trace, 5);
   m_trace->open("waveform.vcd");
 }
 
