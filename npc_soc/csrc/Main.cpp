7a5e3f0279fc7d9753a63a407d1b4ee6923d41d1
tracer-ysyx <tracer@ysyx.org>
2026-01-04 13:46:59 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:46:59 up  3:14,  1 user,  load average: 0.72, 0.90, 0.76

--- npc_soc/csrc/Main.cpp
+++ npc_soc/csrc/Main.cpp
@@ -24,7 +24,7 @@ int simulate(int argc, char *argv[], Config config) {
 
   Dut dut(config, contextp.get());
   dut.reset();
-  while (!contextp->gotFinish() && dut.sim_time <= 2 * 50) {
+  while (!contextp->gotFinish()) {
     dut.step_one_cycle();
     dut.print_all_gpr();
   }
