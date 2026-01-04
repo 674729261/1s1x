384b48d1592cf9fea537646e2bfad74866aef625
tracer-ysyx <tracer@ysyx.org>
2026-01-04 11:41:35 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:41:35 up  1:08,  1 user,  load average: 1.23, 0.65, 0.35

--- abstract-machine/scripts/platform/soc.mk
+++ abstract-machine/scripts/platform/soc.mk
@@ -12,7 +12,7 @@ AM_SRCS := riscv/soc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker_soc.ld
-LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_sram_start=0x10000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_sram_start=0x10000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 
 MAINARGS_MAX_LEN = 64
