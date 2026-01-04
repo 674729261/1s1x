ad7fbd98ab2e9fdddff9c6cf80c74a715220ab97
tracer-ysyx <tracer@ysyx.org>
2026-01-04 20:10:25 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:10:25 up  9:37,  1 user,  load average: 0.57, 0.56, 0.62

--- soc_kernal/mem_test/tests/test1.c
+++ soc_kernal/mem_test/tests/test1.c
@@ -13,8 +13,8 @@
 #define TEST_ADDR_END 0x0f001000
 
 int test_word() {
-  uint32_t *start = (uint32_t *)(0x0f000000);
-  uint32_t *end = (uint32_t *)(0x0f001000);
+  uint32_t *start = (uint32_t *)(TEST_ADDR_START);
+  uint32_t *end = (uint32_t *)(TEST_ADDR_END);
   for (volatile uint32_t *p = start; p < end; p++) {
     *p = (uint32_t)((uint32_t)p * 23);
   }
@@ -24,8 +24,35 @@ int test_word() {
   return 0;
 }
 
+int test_half() {
+  uint16_t *start = (uint16_t *)(TEST_ADDR_START);
+  uint16_t *end = (uint16_t *)(TEST_ADDR_END);
+  for (volatile uint16_t *p = start; p < end; p++) {
+    *p = (uint16_t)((uint32_t)p * 42);
+  }
+  for (volatile uint16_t *p = start; p < end; p++) {
+    MY_CHECK(*p == (uint16_t)((uint32_t)p * 42));
+  }
+  return 0;
+}
+
+int test_byte() {
+  uint8_t *start = (uint8_t *)(TEST_ADDR_START);
+  uint8_t *end = (uint8_t *)(TEST_ADDR_END);
+  for (volatile uint8_t *p = start; p < end; p++) {
+    *p = (uint8_t)((uint32_t)p * 3);
+  }
+  for (volatile uint8_t *p = start; p < end; p++) {
+    MY_CHECK(*p == (uint8_t)((uint32_t)p * 3));
+  }
+  return 0;
+}
+
 int main() {
   int return_value = test_word();
+  if (return_value)
+    return -1;
+  return_value = test_half();
   if (return_value)
     return -1;
   return 0;
