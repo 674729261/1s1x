843255275a1a34c73eb3fb62cfa0866cdf7d327f
tracer-ysyx <tracer@ysyx.org>
2026-01-04 15:41:43 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:41:43 up  5:08,  1 user,  load average: 2.78, 1.23, 0.99

--- npc_soc/src/main/scala/empty/LSU.scala
+++ npc_soc/src/main/scala/empty/LSU.scala
@@ -70,10 +70,7 @@ class LSU() extends Module {
   val rdata_reg = RegEnable(ramLoader.io.out, r_fire)
 
   fetch_port.ar.valid := should_mem_access_r && state_r === sIDLE_r
-  fetch_port.ar.addr := Cat(
-    in.bits.write_info.alu_out(31, 2),
-    "b00".U(2.W)
-  )
+  fetch_port.ar.addr := in.bits.write_info.alu_out(31, 2)
   fetch_port.ar.size := Mux1H(
     Seq(
       in.bits.controls.is_ram_byte -> "b000".U(3.W),
