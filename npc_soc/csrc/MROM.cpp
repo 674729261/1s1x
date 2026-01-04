dc4f5c9bf414ef4b177fa9fa0cb7538d6547f136
tracer-ysyx <tracer@ysyx.org>
2026-01-04 22:23:30 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:23:30 up 11:50,  1 user,  load average: 0.49, 0.69, 0.58

--- npc_soc/csrc/MROM.cpp
+++ npc_soc/csrc/MROM.cpp
@@ -9,8 +9,6 @@
 std::vector<uint32_t> mrom_content;
 
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
-  if ((addr & 0x0FFFFFFF) == 0x00000250)
-    spdlog::info("!!!!");
   *data = mrom_content[(addr & 0x0FFFFFFF) >> 2];
 }
 
