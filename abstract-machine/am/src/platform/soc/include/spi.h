6398d7e7c8a3a5efa138f976338cd0cb76b3981c
tracer-ysyx <tracer@ysyx.org>
2026-01-05 21:48:01 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:48:01 up 10:27,  1 user,  load average: 0.84, 0.57, 0.55

--- abstract-machine/am/src/platform/soc/include/spi.h
+++ abstract-machine/am/src/platform/soc/include/spi.h
@@ -60,6 +60,7 @@ static inline uint32_t flash_read(uintptr_t addr) {
   spi_tx_neg(true);
   spi_rx_neg(true);
   outl(SPI_BASE + 0x4, (0x03 << 24) | (addr & 0x00ffffff));
+  outl(SPI_BASE, 0xffffffff);
   set_spi_ss(SPI_SLAVE_FLASH);
   set_spi_len(64);
   set_spi_busy();
