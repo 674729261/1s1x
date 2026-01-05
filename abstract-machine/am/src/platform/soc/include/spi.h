04570200c3e1864a92ee28ab30b90d1000d21d3a
tracer-ysyx <tracer@ysyx.org>
2026-01-05 23:02:26 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:02:26 up 11:41,  1 user,  load average: 1.12, 0.93, 0.75

--- abstract-machine/am/src/platform/soc/include/spi.h
+++ abstract-machine/am/src/platform/soc/include/spi.h
@@ -8,7 +8,7 @@
 static inline void spi_init() {
   uint32_t ctrl = inl(SPI_BASE + SPI_CTRL);
   ctrl = 0x2640;
-  outl(SPI_BASE + SPI_DIV, 0x00000001);
+  outl(SPI_BASE + SPI_DIV, 0x00000000);
   outl(SPI_BASE + SPI_CTRL, ctrl);
 }
 
