20b8898a73975b53e3fbfd14842a9a54184f13be
tracer-ysyx <tracer@ysyx.org>
2026-01-05 13:10:15 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:10:15 up  1:49,  1 user,  load average: 0.53, 0.34, 0.39

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -17,9 +17,9 @@ static const char mainargs[MAINARGS_MAX_LEN] =
     TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
-  // uint8_t line_status = 0x00;
-  // while (!(line_status & (1 << 5)))
-  //   line_status = inb(SERIAL_PORT + SERIAL_LSR_OFFSET);
+  uint8_t line_status = 0x00;
+  while (!(line_status & (1 << 5)))
+    line_status = inb(SERIAL_PORT + SERIAL_LSR_OFFSET);
 
   outb(SERIAL_PORT, ch);
 }
@@ -30,7 +30,19 @@ void halt(int code) {
     ;
 }
 
-void _init_uart() { outb(SERIAL_INT_OFFSET + SERIAL_PORT, 0x00); }
+void _init_uart() {
+  uint8_t lcr = inb(SERIAL_PORT + SERIAL_LCR_OFFSET);
+  lcr |= (1 << 7);
+  outb(SERIAL_PORT + SERIAL_LCR_OFFSET, lcr);
+
+  outb(SERIAL_PORT + 1, 0x01);
+  outb(SERIAL_PORT, 0x00);
+
+  lcr = inb(SERIAL_PORT + SERIAL_LCR_OFFSET);
+  lcr &= ~(1 << 7);
+  outb(SERIAL_PORT + SERIAL_LCR_OFFSET, lcr);
+  outb(SERIAL_PORT + SERIAL_INT_OFFSET, 0x00);
+}
 void _show_motd(uint32_t vendorid, uint32_t archid) {
   const char *first_part = "\033[31mmvendorid\033[0m : 0x";
   const char *second_part = "\n\033[31mmarchid\033[0m : ";
