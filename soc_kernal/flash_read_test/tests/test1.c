b4fb2c5a08c61817b0e6c9ad6b0d8022c31dc643
tracer-ysyx <tracer@ysyx.org>
2026-01-05 23:28:13 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:28:13 up 12:07,  1 user,  load average: 0.39, 0.39, 0.47

--- soc_kernal/flash_read_test/tests/test1.c
+++ soc_kernal/flash_read_test/tests/test1.c
@@ -9,17 +9,16 @@
   } while (0)
 
 int main() {
-  for (int i = 0; i < 0xff0; i += 4) {
-    uint32_t recv = flash_read(i);
-    MY_CHECK(recv == i);
-  } // char buffer[16] = {};
-    // int cnt = 0;
-    // while (recv) {
-    //   int dig = recv % 16;
-    //   buffer[cnt++] = (dig < 10 ? '0' + dig : 'a' + dig - 10);
-    //   recv /= 16;
-    // }
-    // for (int i = cnt - 1; i >= 0; i--)
-    //   putch(buffer[i]);
-    // putch('\n');
+  uint32_t addr = 0x0000fff0;
+  uint32_t recv = flash_read(addr);
+  char buffer[16] = {};
+  int cnt = 0;
+  while (recv) {
+    int dig = recv % 16;
+    buffer[cnt++] = (dig < 10 ? '0' + dig : 'a' + dig - 10);
+    recv /= 16;
+  }
+  for (int i = cnt - 1; i >= 0; i--)
+    putch(buffer[i]);
+  putch('\n');
 }
