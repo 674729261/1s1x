c0f6091622b8e46f59ac4681b31b08ca05b7d29c
tracer-ysyx <tracer@ysyx.org>
2026-01-06 20:53:26 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:53:26 up  9:39,  1 user,  load average: 0.47, 0.61, 0.63

--- npc_soc/include/Ref.h
+++ npc_soc/include/Ref.h
@@ -100,7 +100,7 @@ inline void Ref::step() {
         cpu.pc);
   }
   const uint32_t inst = ifnst_fetch.data;
-  std::println("PC = {:08x}, inst = {:08x}", cpu.pc, inst);
+  // std::println("PC = {:08x}, inst = {:08x}", cpu.pc, inst);
   Decoded d = decode(inst);
 
   uint32_t dnpc = cpu.pc + 4;
