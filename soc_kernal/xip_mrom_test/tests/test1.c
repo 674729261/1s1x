d0769f3ffb535625669c6493c0696ece8ecc2294
tracer-ysyx <tracer@ysyx.org>
2026-01-06 11:55:21 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:55:21 up 41 min,  1 user,  load average: 0.23, 0.22, 0.30

---
+++ soc_kernal/xip_mrom_test/tests/test1.c
@@ -0,0 +1,26 @@
+#include <klib.h>
+#include <spi.h>
+#include <stdint.h>
+
+#define MY_CHECK(C)                                                            \
+  do {                                                                         \
+    if (!(C))                                                                  \
+      return -1;                                                               \
+  } while (0)
+#define XIP_BASE 0x30000000
+int main() {
+  for (uint32_t i = 0; i < 0x100; i += 4) {
+    uint32_t recv = inl(XIP_BASE + i);
+
+    char buffer[16] = {};
+    int cnt = 0;
+    while (recv) {
+      int dig = recv % 16;
+      buffer[cnt++] = (dig < 10 ? '0' + dig : 'a' + dig - 10);
+      recv /= 16;
+    }
+    for (int j = cnt - 1; j >= 0; j--)
+      putch(buffer[j]);
+    putch('\n');
+  }
+}
