85dd42a7a2c847794c729ccef2a522f7973c4b4f
tracer-ysyx <tracer@ysyx.org>
2026-01-06 12:45:48 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:45:48 up  1:31,  1 user,  load average: 0.21, 0.21, 0.18

---
+++ abstract-machine/scripts/riscv32e-socflash.mk
@@ -0,0 +1,11 @@
+include $(AM_HOME)/scripts/isa/riscv.mk
+include $(AM_HOME)/scripts/platform/socflash.mk
+CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
+LDFLAGS       += -melf32lriscv                    # overwrite
+
+AM_SRCS += riscv/soc/libgcc/div.S \
+           riscv/soc/libgcc/muldi3.S \
+           riscv/soc/libgcc/multi3.c \
+           riscv/soc/libgcc/ashldi3.c \
+           riscv/soc/libgcc/unused.c
