becb658c1c27e15604dbfec5ca8a4be1991f4574
tracer-ysyx <tracer@ysyx.org>
2026-01-06 11:40:55 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:40:55 up 27 min,  1 user,  load average: 0.19, 0.29, 0.38

--- npc_soc/csrc/Flash.cpp
+++ npc_soc/csrc/Flash.cpp
@@ -13,6 +13,8 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
 }
 
 void init_flash(std::string_view image_path) {
+  image_path = "/home/shitful/1s1x/ysyx-workbench/soc_kernal/mrom_test/build/"
+               "mromtest.bin";
   using std::ifstream;
   using std::ios;
 
