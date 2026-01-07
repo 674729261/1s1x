f74b72b13eb5c276e1dd4f5c50f9c540461567cf
tracer-ysyx <tracer@ysyx.org>
2026-01-07 22:16:13 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:16:13 up  2:16,  1 user,  load average: 0.87, 1.13, 1.15

--- soc_kernal/sdram_test/tests/test1.c
+++ soc_kernal/sdram_test/tests/test1.c
@@ -9,8 +9,8 @@
       return -1;                                                               \
   } while (0)
 
-#define TEST_ADDR_START 0xa000a000
-#define TEST_ADDR_END 0xa000af00
+#define TEST_ADDR_START 0xa003a000
+#define TEST_ADDR_END 0xa003af00
 
 int test_word() {
   uint32_t *start = (uint32_t *)(TEST_ADDR_START);
