4b76ed82035ed7b4cb0a783f55d5b1c853130816
tracer-ysyx <tracer@ysyx.org>
2026-01-07 11:51:49 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:51:49 up 38 min,  1 user,  load average: 1.05, 0.62, 0.35

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -86,9 +86,9 @@ void _trm_init() {
 }
 
 __attribute__((section(".fsbl"))) void fstbootloader() {
-  extern char __ssbl_load_start, __ssbl_load_end, __ssbl_start;
-  char *src = &__ssbl_load_start;
-  char *dst = &__ssbl_start;
+  extern uint32_t __ssbl_load_start, __ssbl_load_end, __ssbl_start;
+  uint32_t *src = &__ssbl_load_start;
+  uint32_t *dst = &__ssbl_start;
   while (src < &__ssbl_load_end) {
     *dst = *src;
     ++dst;
