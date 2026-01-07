e29d59ea1b69dd313c1445a8ff1ebdf38a7c7da7
tracer-ysyx <tracer@ysyx.org>
2026-01-08 02:37:45 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:37:45 up  6:38,  1 user,  load average: 0.45, 0.66, 0.76

--- npc_soc/include/VirtualBus.h
+++ npc_soc/include/VirtualBus.h
@@ -10,7 +10,8 @@
 #include <vector>
 
 struct VirtualBus {
-  VirtualBus() : sram(2048), psram(1024 * 1024 * 1), sdram(1024 * 1024 * 8) {}
+  VirtualBus()
+      : sram(2048), psram(1024 * 1024 * 1), sdram(1024 * 1024 * 8 * 2) {}
 
   struct ReadResult {
     uint32_t data;
@@ -39,6 +40,10 @@ struct VirtualBus {
     } else if (check_range(psram_field)) {
       return {psram[(addr & 0x00FFFFFF) >> 2], false};
     } else if (check_range(sdram_field)) {
+      if (((addr & 0x0FFFFFFF) >> 2) >= sdram.size()) {
+        log_and_throw<std::logic_error>("SDRAM read addr {:08x} overflow",
+                                        addr);
+      }
       return {sdram[(addr & 0x0FFFFFFF) >> 2], false};
     } else {
       log_and_throw<std::logic_error>(
@@ -71,11 +76,15 @@ struct VirtualBus {
       // no action
     } else if (check_range(flash_field)) {
       log_and_throw<std::logic_error>(
-          "Failed to write to address {:08x} : flash can not be written", addr);
+          "Failed to write to address {:} : flash can not be written", addr);
     } else if (check_range(psram_field)) {
       psram[(addr & 0x00FFFFFF) >> 2] &= ~mask32;
       psram[(addr & 0x00FFFFFF) >> 2] |= wdata & mask32;
     } else if (check_range(sdram_field)) {
+      if (((addr & 0x0FFFFFFF) >> 2) >= sdram.size()) {
+        log_and_throw<std::logic_error>("SDRAM write addr {:08x} overflow",
+                                        addr);
+      }
       sdram[(addr & 0x0FFFFFFF) >> 2] &= ~mask32;
       sdram[(addr & 0x0FFFFFFF) >> 2] |= wdata & mask32;
     } else {
