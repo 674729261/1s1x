c8bdd0268bcb9e7f8439e3e18910664c77880cbd
tracer-ysyx <tracer@ysyx.org>
2026-01-08 01:47:42 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:47:42 up  5:48,  1 user,  load average: 0.34, 0.62, 0.64

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -598,7 +598,7 @@ module sdram_axi_core (
     if (rst_i) ack_q <= 1'b0;
     else begin
       if (state_q == STATE_WRITE0) ack_q <= 1'b1;
-      else if (rd_q[SDRAM_READ_LATENCY+1]) ack_q <= 1'b1;
+      else if (rd_q[SDRAM_READ_LATENCY+1-1]) ack_q <= 1'b1;
       else ack_q <= 1'b0;
     end
 
