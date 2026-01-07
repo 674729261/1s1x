86bc7346f6ad27f1ce1fab504938c736e5003410
tracer-ysyx <tracer@ysyx.org>
2026-01-08 01:44:33 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:44:33 up  5:45,  1 user,  load average: 0.64, 0.57, 0.61

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -73,7 +73,7 @@ module sdram_axi_core (
   parameter SDRAM_MHZ = 50;
   parameter SDRAM_ADDR_W = 24;
   parameter SDRAM_COL_W = 9;
-  parameter SDRAM_READ_LATENCY = 2;
+  parameter SDRAM_READ_LATENCY = 1;
 
   //-----------------------------------------------------------------
   // Defines / Local params
