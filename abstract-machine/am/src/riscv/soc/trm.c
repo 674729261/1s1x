b2fdd51273787b0fa1e5d94c4df8a34c051ebf70
tracer-ysyx <tracer@ysyx.org>
2026-01-07 12:39:11 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:39:11 up  1:25,  1 user,  load average: 0.89, 0.84, 0.59

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -155,10 +155,10 @@ __attribute__((section(".ssbl"))) void secbootloader() {
   extern char __bss_start, __bss_end;
   __ssbl_memset(&__bss_start, 0, &__bss_end - &__bss_start);
 
-  // #ifdef RT_THREAD_LOAD
-  extern char rt_load_begin, rt_load_end, rt_begin;
+#ifdef __RTTHREAD__
+  s extern char rt_load_begin, rt_load_end, rt_begin;
   src = &rt_load_begin;
   dst = &rt_begin;
   __ssbl__memcpy(dst, src, &rt_load_end - &rt_load_begin);
-  // #endif
+#endif
 }
