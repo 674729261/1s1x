669c456c5a9da9a695cb1a437d56da8a7fe68602
tracer-ysyx <tracer@ysyx.org>
2026-01-07 13:09:09 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:09:09 up  1:55,  1 user,  load average: 0.58, 0.41, 0.38

--- abstract-machine/scripts/platform/soc.mk
+++ abstract-machine/scripts/platform/soc.mk
@@ -11,13 +11,13 @@ AM_SRCS := riscv/soc/start.S \
            platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
-CFLAGS    += -I$(AM_HOME)/am/src/platform/soc/include -I$(AM_HOME)/am/src
+CFLAGS    += -D__RTTHREAD__ -I$(AM_HOME)/am/src/platform/soc/include -I$(AM_HOME)/am/src
 LDSCRIPTS += $(AM_HOME)/scripts/linker_soc.ld
 LDFLAGS   += --gc-sections -e _start --defsym=_pmem_size=1M --defsym=_SOC_=1
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
-CFLAGS += -D__RTTHREAD__ -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
+CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
