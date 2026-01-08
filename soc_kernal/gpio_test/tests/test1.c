60547f2f5bc9c0fe2834da48c4be5dadba7f2400
tracer-ysyx <tracer@ysyx.org>
2026-01-08 17:40:16 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:40:16 up  6:07,  1 user,  load average: 1.29, 0.97, 0.90

--- soc_kernal/gpio_test/tests/test1.c
+++ soc_kernal/gpio_test/tests/test1.c
@@ -10,7 +10,7 @@ int main() {
   for (int i = 0; i < 100; i++) {
     for (uint16_t p = 0; p < 16; p++)
       *(volatile uint16_t *)LED_GPIO = (1 << p);
-    for (volatile int j = 0; j < 100; j++)
+    for (volatile int j = 0; j < 1000; j++)
       ;
   }
   return 0;
