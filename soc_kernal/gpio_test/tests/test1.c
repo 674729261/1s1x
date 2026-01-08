76e8a25ff23cb5a17da6cd08f42ccbb56c76c853
tracer-ysyx <tracer@ysyx.org>
2026-01-08 18:07:03 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:07:03 up  6:33,  1 user,  load average: 0.60, 0.76, 0.87

--- soc_kernal/gpio_test/tests/test1.c
+++ soc_kernal/gpio_test/tests/test1.c
@@ -6,7 +6,18 @@
 #define SWITCH_GPIO 0x10002004
 #define DIGIT_GPIO 0x10002008
 
+const uint8_t hex_2_digit[16] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d,
+                                 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c,
+                                 0x39, 0x5e, 0x79, 0x71};
+
 int main() {
+  uint32_t marchid;
+  asm volatile("csrr %0, marchid" : "=r"(marchid));
+  for (int i = 0; i < 8; i++) {
+    *(volatile uint8_t *)(DIGIT_GPIO + i) = hex_2_digit[marchid % 10];
+    marchid /= 10;
+  }
+
   for (int i = 0; i < 100; i++) {
     for (uint16_t p = 0; p < 16; p++) {
       *(volatile uint16_t *)LED_GPIO = (1 << p);
