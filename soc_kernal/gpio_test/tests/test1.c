b112ce843581ec0ac46e359f65ecb587e71feeae
tracer-ysyx <tracer@ysyx.org>
2026-01-08 16:23:39 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:23:39 up  4:50,  1 user,  load average: 0.23, 0.68, 0.87

--- soc_kernal/gpio_test/tests/test1.c
+++ soc_kernal/gpio_test/tests/test1.c
@@ -6,8 +6,9 @@
 #define DIGIT_GPIO 0x10002008
 
 int main() {
-  uint16_t switches = *(volatile uint16_t *)SWITCH_GPIO;
-  *(volatile uint16_t *)DIGIT_GPIO = switches;
+  // uint16_t switches = *(volatile uint16_t *)SWITCH_GPIO;
+  *(volatile uint32_t *)DIGIT_GPIO = 0xdeadbeef;
+  *(volatile uint32_t *)(DIGIT_GPIO + 0x4) = 0x12345678;
 
   return 0;
 }
