183672824461917612c2235f7c25f8fb3aa8e077
tracer-ysyx <tracer@ysyx.org>
2026-01-08 20:50:45 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:50:45 up  9:17,  1 user,  load average: 1.52, 1.49, 1.04

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -37,7 +37,7 @@ void _init_uart() {
   outb(SERIAL_PORT + SERIAL_LCR_OFFSET, lcr);
 
   outb(SERIAL_PORT + 1, 0x00);
-  outb(SERIAL_PORT, 0x04);
+  outb(SERIAL_PORT, 0x02);
 
   lcr = inb(SERIAL_PORT + SERIAL_LCR_OFFSET);
   lcr &= ~(1 << 7);
