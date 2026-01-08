fe859b778e507a0641b690944c748f0aa5f07668
tracer-ysyx <tracer@ysyx.org>
2026-01-08 20:06:09 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:06:09 up  8:33,  1 user,  load average: 1.52, 0.73, 0.55

--- npc_soc/scripts/capstone.mk
+++ npc_soc/scripts/capstone.mk
@@ -1,6 +1,6 @@
 LIBCAPSTONE = tools/capstone/repo/libcapstone.so.6
 SO_PATH_CAPSTONE = $(SOC_NPC_HOME)/$(LIBCAPSTONE)
-CXXFLAGS += -CFLAGS "-I$(SOC_NPC_HOME)/tools/capstone/repo/include -DSO_PATH_CAPSTONE=$(SO_PATH_CAPSTONE)"
+V_CXXFLAGS += -CFLAGS "-I$(SOC_NPC_HOME)/tools/capstone/repo/include -DSO_PATH_CAPSTONE=$(SO_PATH_CAPSTONE)"
 
 $(LIBCAPSTONE):
 	$(MAKE) -C tools/capstone
--- npc_soc/scripts/difftest.mk
+++ npc_soc/scripts/difftest.mk
@@ -1,5 +1,5 @@
 SO_PATH_NEMU = $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
-CXXFLAGS += -CFLAGS "-DSO_PATH_NEMU=$(SO_PATH_NEMU)"
+V_CXXFLAGS += -CFLAGS "-DSO_PATH_NEMU=$(SO_PATH_NEMU)"
 
 $(SO_PATH_NEMU):
 	make -C $(NEMU_HOME)
