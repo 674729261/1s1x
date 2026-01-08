802549504545ecba13651582134115ba2184f3ce
tracer-ysyx <tracer@ysyx.org>
2026-01-08 11:37:15 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:37:15 up 4 min,  1 user,  load average: 1.39, 1.10, 0.48

--- npc_soc/include/DUT.h
+++ npc_soc/include/DUT.h
@@ -40,11 +40,11 @@ struct Dut {
   void step_one_cycle() {
     top->clock = 0;
     top->eval();
-    m_trace->dump(sim_time);
+    // m_trace->dump(sim_time);
     sim_time++;
     top->clock = 1;
     top->eval();
-    m_trace->dump(sim_time);
+    // m_trace->dump(sim_time);
     sim_time++;
   }
 
