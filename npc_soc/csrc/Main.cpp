6ec929a64f5301e36d0da901a23adc66b0a13ed6
tracer-ysyx <tracer@ysyx.org>
2026-01-10 13:34:27 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:34:27 up 27 min,  1 user,  load average: 0.18, 0.57, 0.63

--- npc_soc/csrc/Main.cpp
+++ npc_soc/csrc/Main.cpp
@@ -102,7 +102,7 @@ int simulate(int argc, char *argv[], Config config) {
     result = -3;
   }
   dut.print_all_gpr();
-  spdlog::info("Total simulation time : {}", end_time - start_time);
+  spdlog::info("Total simulation time : {:%H:%M:%S}", end_time - start_time);
   return result;
 }
 
