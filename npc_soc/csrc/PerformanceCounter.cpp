171b727da21e616e2a53d4c6d8d4f92bed52263a
tracer-ysyx <tracer@ysyx.org>
2026-01-10 14:52:19 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:52:19 up  1:45,  1 user,  load average: 2.06, 0.91, 0.68

--- npc_soc/csrc/PerformanceCounter.cpp
+++ npc_soc/csrc/PerformanceCounter.cpp
@@ -1,4 +1,6 @@
 
+#include <iostream>
+#include <ostream>
 long long ifu_event;
 long long lsu_event;
 long long exu_event;
@@ -6,7 +8,10 @@ long long idu_event;
 long long inst_count;
 long long clock_count;
 
-extern "C" void notify_ifu_event() { ifu_event++; }
+extern "C" void notify_ifu_event() {
+  std::println(std::cout, "!!");
+  ifu_event++;
+}
 extern "C" void notify_lsu_event() { lsu_event++; }
 extern "C" void notify_exu_event() { exu_event++; }
 extern "C" void notify_idu_event() { idu_event++; }
