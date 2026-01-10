e737fd5e57659a5b4220c1975219c036c650e8ad
tracer-ysyx <tracer@ysyx.org>
2026-01-10 17:19:40 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:19:40 up  4:13,  1 user,  load average: 0.20, 0.40, 0.55

--- npc_soc/csrc/PerformanceCounter.cpp
+++ npc_soc/csrc/PerformanceCounter.cpp
@@ -1,3 +1,5 @@
+#include <PerformanceCounter.h>
+#include <array>
 long long ifu_event;
 long long lsu_event;
 long long exu_event;
@@ -6,6 +8,36 @@ long long wbu_event;
 long long inst_count;
 long long clock_count;
 
+std::array<InstTypeItem, 13> inst_type_event = {
+    {{0, "Arithmetic immediate"},
+     {0, "Arithmetic register"},
+     {0, "Store"},
+     {0, "Load"},
+     {0, "Branch"},
+     {0, "Jump and link"},
+     {0, "Jump and link register"},
+     {0, "Load upper immediate"},
+     {0, "Add upper immediate PC"},
+     {0, "Environment break"},
+     {0, "Environment call"},
+     {0, "Machine mode returen"},
+     {0, "Control state register operation"}}};
+
+enum {
+  TYPE_arithmetic_imm,
+  TYPE_arithmetic_reg,
+  TYPE_store,
+  TYPE_load,
+  TYPE_branch,
+  TYPE_jal,
+  TYPE_jalr,
+  TYPE_lui,
+  TYPE_auipc,
+  TYPE_ebreak,
+  TYPE_ecall,
+  TYPE_mret,
+  TYPE_csrop
+};
 extern "C" void notify_ifu_r_event() { ifu_event++; }
 extern "C" void notify_lsu_r_event() { lsu_event++; }
 extern "C" void notify_ifu_ar_event() {}
@@ -13,16 +45,38 @@ extern "C" void notify_lsu_ar_event() {}
 extern "C" void notify_exu_event() { exu_event++; }
 extern "C" void notify_idu_event() { idu_event++; }
 extern "C" void notify_wbu_event() { wbu_event++; }
-extern "C" void notify_inst_type_is_arithmetic_imm() {}
-extern "C" void notify_inst_type_is_arithmetic_reg() {}
-extern "C" void notify_inst_type_is_store() {}
-extern "C" void notify_inst_type_is_load() {}
-extern "C" void notify_inst_type_is_branch() {}
-extern "C" void notify_inst_type_is_jal() {}
-extern "C" void notify_inst_type_is_jalr() {}
-extern "C" void notify_inst_type_is_lui() {}
-extern "C" void notify_inst_type_is_auipc() {}
-extern "C" void notify_inst_type_is_ebreak() {}
-extern "C" void notify_inst_type_is_ecall() {}
-extern "C" void notify_inst_type_is_mret() {}
-extern "C" void notify_inst_type_is_csrop() {}
+extern "C" void notify_inst_type_is_arithmetic_imm() {
+  inst_type_event[TYPE_arithmetic_imm].count++;
+}
+extern "C" void notify_inst_type_is_arithmetic_reg() {
+  inst_type_event[TYPE_arithmetic_reg].count++;
+}
+extern "C" void notify_inst_type_is_store() {
+  inst_type_event[TYPE_store].count++;
+}
+extern "C" void notify_inst_type_is_load() {
+  inst_type_event[TYPE_load].count++;
+}
+extern "C" void notify_inst_type_is_branch() {
+  inst_type_event[TYPE_branch].count++;
+}
+extern "C" void notify_inst_type_is_jal() { inst_type_event[TYPE_jal].count++; }
+extern "C" void notify_inst_type_is_jalr() {
+  inst_type_event[TYPE_jalr].count++;
+}
+extern "C" void notify_inst_type_is_lui() { inst_type_event[TYPE_lui].count++; }
+extern "C" void notify_inst_type_is_auipc() {
+  inst_type_event[TYPE_auipc].count++;
+}
+extern "C" void notify_inst_type_is_ebreak() {
+  inst_type_event[TYPE_ebreak].count++;
+}
+extern "C" void notify_inst_type_is_ecall() {
+  inst_type_event[TYPE_ecall].count++;
+}
+extern "C" void notify_inst_type_is_mret() {
+  inst_type_event[TYPE_mret].count++;
+}
+extern "C" void notify_inst_type_is_csrop() {
+  inst_type_event[TYPE_csrop].count++;
+}
--- npc_soc/include/PerformanceCounter.h
+++ npc_soc/include/PerformanceCounter.h
@@ -1,6 +1,15 @@
 #pragma once
+#include "my_utils.h"
+#include <algorithm>
 #include <chrono>
+#include <ranges>
 #include <spdlog/spdlog.h>
+
+struct InstTypeItem {
+  long long count;
+  const std::string name;
+};
+
 extern long long ifu_event;
 extern long long lsu_event;
 extern long long exu_event;
@@ -8,14 +17,19 @@ extern long long idu_event;
 extern long long wbu_event;
 extern long long inst_count;
 extern long long clock_count;
+extern std::array<InstTypeItem, 13> inst_type_event;
 
 inline void clear_performance_count() {
   ifu_event = 0;
   lsu_event = 0;
   exu_event = 0;
   idu_event = 0;
+  wbu_event = 0;
   inst_count = 0;
   clock_count = 0;
+
+  for (InstTypeItem &item : inst_type_event)
+    item.count = 0;
 }
 
 inline void display_performance(auto start_time, auto end_time) {
@@ -28,6 +42,12 @@ inline void display_performance(auto start_time, auto end_time) {
   spdlog::info("Total exu events : {}", exu_event);
   spdlog::info("Total idu events : {}", idu_event);
   spdlog::info("Total wbu events : {}", wbu_event);
+
+  spdlog::info("------------Instruction Type Statistics------------");
+  for (InstTypeItem &item : inst_type_event) {
+    spdlog::info("{:20} : {}", item.name, item.count);
+  }
+  spdlog::info("---------------------------------------------------");
 
   spdlog::info("Total simulated clock periods : {}", clock_count);
   spdlog::info("Clocks per instruction : {:.3f}",
