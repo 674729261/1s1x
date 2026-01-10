6f408ab102250572e1f31c19af2447be7a542611
tracer-ysyx <tracer@ysyx.org>
2026-01-10 17:37:14 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:37:13 up  4:30,  1 user,  load average: 1.33, 1.07, 0.81

--- npc_soc/include/PerformanceCounter.h
+++ npc_soc/include/PerformanceCounter.h
@@ -43,7 +43,7 @@ inline void display_performance(auto start_time, auto end_time) {
   spdlog::info("------------Instruction Type Statistics------------");
   long long sum_recorded_inst = 0;
   for (InstTypeItem &item : inst_type_event) {
-    spdlog::info("{:40} : {}", item.name, item.count);
+    spdlog::info("{:25} :      {}", item.name, item.count);
     sum_recorded_inst += item.count;
   }
   spdlog::info("Total : {}", sum_recorded_inst);
