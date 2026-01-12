fd79985fc33a3d1b14f2d1859ff247129a007e46
tracer-ysyx <tracer@ysyx.org>
2026-01-12 19:52:20 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:52:20 up  4:24,  1 user,  load average: 1.92, 1.50, 1.35

--- ysyxSoC/src/amba/APBDelayer.scala
+++ ysyxSoC/src/amba/APBDelayer.scala
@@ -12,7 +12,9 @@ import freechips.rocketchip.util._
 class APBDelayerIO extends Bundle {
   val clock = Input(Clock())
   val reset = Input(Reset())
-  val in = Flipped(new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32)))
+  val in = Flipped(
+    new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32))
+  )
   val out = new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32))
 }
 
@@ -20,9 +22,51 @@ class apb_delayer extends BlackBox {
   val io = IO(new APBDelayerIO)
 }
 
-class APBDelayerChisel extends Module {
+class APBDelayerChisel(ratio: Double = 6.87, scale_2pow: Long = 6)
+    extends Module {
   val io = IO(new APBDelayerIO)
+  val countup_amount = math.round(ratio * math.pow(2.0, scale_2pow))
+  val countdown_amount = (1 << scale_2pow)
+  val counter = RegInit(UInt(32.W), 0.U)
+  val sIDLE :: sWAIT :: sDELAY :: sRESP :: Nil = Enum(4)
+  val state = RegInit(sIDLE)
+
+  val trigger = io.in.penable && io.in.psel
+  val out_fire = io.out.psel && io.out.penable && io.out.pready
+  state := MuxLookup(state, sDELAY)(
+    Seq(
+      sIDLE -> Mux(trigger, sWAIT, sIDLE),
+      sWAIT -> Mux(out_fire, sDELAY, sWAIT),
+      sDELAY -> Mux(counter < countdown_amount.U, sRESP, sDELAY),
+      sRESP -> sIDLE
+    )
+  )
+  counter := MuxCase(
+    counter,
+    Seq(
+      (state === sWAIT && !out_fire) -> (counter + countup_amount.U),
+      (state === sDELAY && counter >= countdown_amount.U) -> (counter - countdown_amount.U)
+    )
+  )
+
+  val prdata_r = RegEnable(io.out.prdata, out_fire)
+  val pslverr_r = RegEnable(io.out.pslverr, out_fire)
+
   io.out <> io.in
+  io.in.prdata := prdata_r
+  io.in.pslverr := pslverr_r
+  io.out.penable := Mux(
+    state === sIDLE || state === sWAIT,
+    io.in.penable,
+    false.B
+  )
+  io.out.psel := Mux(
+    state === sIDLE || state === sWAIT,
+    io.in.psel,
+    false.B
+  )
+  io.in.pready := state === sRESP
+
 }
 
 class APBDelayerWrapper(implicit p: Parameters) extends LazyModule {
@@ -31,7 +75,7 @@ class APBDelayerWrapper(implicit p: Parameters) extends LazyModule {
   lazy val module = new Impl
   class Impl extends LazyModuleImp(this) {
     (node.in zip node.out) foreach { case ((in, edgeIn), (out, edgeOut)) =>
-      val delayer = Module(new apb_delayer)
+      val delayer = Module(new APBDelayerChisel)
       delayer.io.clock := clock
       delayer.io.reset := reset
       delayer.io.in <> in
