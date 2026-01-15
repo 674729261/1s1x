2656394d618fc6c2af42b664d054342be12d9448
tracer-ysyx <tracer@ysyx.org>
2026-01-16 00:00:33 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:00:33 up  2:08,  1 user,  load average: 3.83, 1.32, 0.85

--- npc_soc/include/DUT.h
+++ npc_soc/include/DUT.h
@@ -40,10 +40,11 @@ struct Dut {
   void step_one_cycle() {
     top->clock = 0;
     top->eval();
-    m_trace->dump(2 * sim_time);
+    // m_trace->dump(2 * sim_time);
     top->clock = 1;
     top->eval();
-    m_trace->dump(2 * sim_time + 1);
+    if (getPC() >= 0xa0000000)
+      m_trace->dump(sim_time);
     sim_time++;
   }
 
