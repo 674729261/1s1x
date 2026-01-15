540bb7139cd190b0c75a5260ef4398ebdef793fe
tracer-ysyx <tracer@ysyx.org>
2026-01-15 19:28:18 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:28:18 up  9:11,  1 user,  load average: 2.94, 1.08, 0.90

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -145,7 +145,7 @@ module sdram_axi_core (
 
   assign sdram_addr_o = (state == ST_MODE) ? MODE_REG : (state == ST_READ_ACTIVATE || state == ST_WRITE_ACTIVATE ? addr_row_w : addr_col_w);
 
-  reg [3:0] msk_r = ~inport_wr_i;
+  wire [3:0] msk_r = ~inport_wr_i;
   assign sdram_dqm0_low_o = which_chip ? 2'b11 : msk_r[1:0];
   assign sdram_dqm0_high_o = which_chip ? 2'b11 : msk_r[3:2];
   assign sdram_dqm1_low_o = which_chip ? msk_r[1:0] : 2'b11;
