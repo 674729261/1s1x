da0baece0d1d95b2a9c93b06de55c003c101e1f7
tracer-ysyx <tracer@ysyx.org>
2026-01-15 22:50:23 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:50:23 up 57 min,  1 user,  load average: 1.38, 1.35, 0.86

--- npc_soc/src/main/scala/empty/ICache.scala
+++ npc_soc/src/main/scala/empty/ICache.scala
@@ -94,9 +94,13 @@ class ICache(linesize_2pow: Int, linecount_2pow: Int) extends Module {
     axi_rdata_latched_next(i) := axi_rdata_latched(i + 1)
 
   io.rdata := Mux(
-    in_cache,
-    cache_rdata.data(input_index_inside_cacheline),
-    axi_rdata_latched(input_index_inside_cacheline)
+    should_cache,
+    Mux(
+      in_cache,
+      cache_rdata.data(input_index_inside_cacheline),
+      axi_rdata_latched(input_index_inside_cacheline)
+    ),
+    axi_rdata_latched_next(words - 1)
   )
   io.ready := io.valid && (has_r || in_cache)
 
@@ -109,12 +113,16 @@ class ICache(linesize_2pow: Int, linecount_2pow: Int) extends Module {
     valid_flags(input_cache_index) := true.B
   }
 
-  fetch_port.ar.addr := Cat(io.addr(31, linesize_2pow), 0.U(linesize_2pow.W))
+  fetch_port.ar.addr := Mux(
+    should_cache,
+    Cat(io.addr(31, linesize_2pow), 0.U(linesize_2pow.W)),
+    io.addr
+  )
 
   fetch_port.aw.id := "b0000".U(4.W)
   fetch_port.ar.id := "b0000".U(4.W)
   fetch_port.w.last := true.B
-  fetch_port.ar.len := (words - 1).U(8.W)
+  fetch_port.ar.len := Mux(should_cache, (words - 1).U(8.W), 0.U(8.W))
 
   block(PerformanceCounterLayer) {
     val performancecounter_icache = Module(new PerformanceCounter_ICache)
