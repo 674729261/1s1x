55e6314a24061c5a0262e7f64fb97d89a0e4d7ae
tracer-ysyx <tracer@ysyx.org>
2026-02-21 17:03:05 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:03:05 up 3 min,  1 user,  load average: 0.39, 0.45, 0.20

--- npc/csrc/Simulate.cpp
+++ npc/csrc/Simulate.cpp
@@ -56,6 +56,7 @@ int simulate(int argc, char *argv[]) {
       ref.reset(*dut);
       ref.sync_state();
     }
+    std::println("PC = {:08x}", dut->getPC());
     dut->step_one_cycle();
 
     if (retire) {
