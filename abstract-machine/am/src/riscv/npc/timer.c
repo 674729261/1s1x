8f31b5640af5177085f3e636ba34e0067d2289f0
tracer-ysyx <tracer@ysyx.org>
2026-02-22 20:07:57 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:07:57 up 10:31,  1 user,  load average: 0.27, 0.37, 0.48

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -10,11 +10,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("!!\n");
   uint32_t lower = inl(RTC_ADDR);
-  printf("xx\n");
   uint32_t upper = inl(RTC_ADDR + 0x4);
-  printf("??\n");
 
   uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
   // static uint64_t last = 0;
