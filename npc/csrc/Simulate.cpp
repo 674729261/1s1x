90dbeddc5f228a9ebe68308c31bfa211aa1cba41
tracer-ysyx <tracer@ysyx.org>
2026-02-22 19:41:52 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:41:52 up 10:05,  1 user,  load average: 0.34, 0.58, 0.62

--- npc/csrc/Simulate.cpp
+++ npc/csrc/Simulate.cpp
@@ -64,7 +64,7 @@ int simulate(int argc, char *argv[]) {
     if (contextp.gotFinish())
       sim_state = SimulationState::HALT;
     if (retire) {
-      // std::println("{:08x}", ref.getPC());
+      std::println("{:08x}", ref.getPC());
       if (config.difftest) {
         ref.step();
         difftest_state = check_difftest(*dut, ref);
