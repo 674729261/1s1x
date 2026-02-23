04f0cc8adb9deb243227cbe5d52825631ab69c92
tracer-ysyx <tracer@ysyx.org>
2026-02-23 10:41:16 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:41:16 up 23 min,  1 user,  load average: 1.80, 1.59, 1.11

--- npc/csrc/Device.cpp
+++ npc/csrc/Device.cpp
@@ -10,32 +10,6 @@ static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 static SDL_Window *window = NULL;
 
-static void fill_audio_callback(void *udata, Uint8 *stream, int len) {
-  SDL_memset(stream, 0, len);
-  static int last_pos = 0;
-  if (len == 0) {
-    return;
-  }
-  uint32_t cnt = Audio.curAudioBase->reg_ctl.reg_count;
-  if (len > cnt)
-    len = cnt;
-  if (last_pos + len <= AudioBase_t::SoundBufferSize) {
-    SDL_MixAudio(stream, static_cast<uint8_t *>(udata) + last_pos, len,
-                 SDL_MIX_MAXVOLUME);
-    last_pos += len;
-  } else {
-    SDL_MixAudio(stream, static_cast<uint8_t *>(udata) + last_pos,
-                 AudioBase_t::SoundBufferSize - last_pos, SDL_MIX_MAXVOLUME);
-    SDL_MixAudio(stream + AudioBase_t::SoundBufferSize - last_pos,
-                 static_cast<uint8_t *>(udata),
-                 len - AudioBase_t::SoundBufferSize + last_pos,
-                 SDL_MIX_MAXVOLUME);
-    last_pos = len - AudioBase_t::SoundBufferSize + last_pos;
-  }
-
-  Audio.curAudioBase->reg_ctl.reg_count -= len;
-}
-
 static void VGA_init() {
   SDL_Init(SDL_INIT_VIDEO);
   SDL_CreateWindowAndRenderer(VideoBase_t::ScreenWidth * 2,
@@ -46,38 +20,6 @@ static void VGA_init() {
       renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC,
       VideoBase_t::ScreenWidth, VideoBase_t::ScreenHeight);
   SDL_RenderPresent(renderer);
-}
-
-static void audio_init_event() {
-  if (SDL_InitSubSystem(SDL_INIT_AUDIO)) {
-    log_and_throw<std::runtime_error>("Could not initialize SDL - {}\n",
-                                      SDL_GetError());
-  }
-  SDL_CloseAudio();
-  if (Audio.reg_ctl.reg_samples >= (1 << 16)) {
-    log_and_throw<std::logic_error>(
-        "Audio.reg_samples = {} is bigger than 65535",
-        Audio.reg_ctl.reg_samples);
-  }
-  if (Audio.reg_ctl.reg_channels >= (1 << 8)) {
-    log_and_throw<std::logic_error>(
-        "Audio.reg_channels = {} is bigger than 255",
-        Audio.reg_ctl.reg_channels);
-  }
-  SDL_AudioSpec sdlAudioSpec = {
-      .freq = static_cast<int>(Audio.reg_ctl.reg_freq),
-      .format = AUDIO_S16SYS,
-      .channels = static_cast<Uint8>(Audio.reg_ctl.reg_channels),
-      .silence = 0,
-      .samples = static_cast<Uint16>(Audio.reg_ctl.reg_samples),
-      .callback = fill_audio_callback,
-      .userdata = Audio.sbuf.get()};
-  if (SDL_OpenAudio(&sdlAudioSpec, NULL) < 0) {
-    log_and_throw<std::runtime_error>("Can't open audio - %s\n",
-                                      SDL_GetError());
-  }
-  Audio.curAudioBase = &Audio;
-  SDL_PauseAudio(0);
 }
 
 static void keyboard_update() {
