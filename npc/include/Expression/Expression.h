c971c557b7995e66909baec8501f62ed19cbaf33
tracer-ysyx <tracer@ysyx.org>
2026-02-25 13:59:27 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:59:27 up  3:24,  1 user,  load average: 1.38, 1.49, 1.33

--- npc/include/Expression/Expression.h
+++ npc/include/Expression/Expression.h
@@ -59,8 +59,8 @@ build_expr_tree(const std::vector<Token> &vtk) {
         break;
       case Catagory::OPERATOR_2: {
         while (!stk_node.empty() &&
-               stk_node.top().cata != Catagory::OPERATOR_1 &&
-               token_types[stk_node.top().type].priority >= tt_cur.priority) {
+               (stk_node.top().cata == Catagory::OPERATOR_1 ||
+                token_types[stk_node.top().type].priority >= tt_cur.priority)) {
           ret.push_back(stk_node.top());
           stk_node.pop();
         }
