16ad20f5f12c2032fef44eb8ecc1cb94be322da3
tracer-ysyx <tracer@ysyx.org>
2026-02-25 16:22:53 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:22:53 up  5:47,  1 user,  load average: 1.43, 1.55, 1.16

--- npc/csrc/Simulate.cpp
+++ npc/csrc/Simulate.cpp
@@ -221,6 +221,7 @@ int simulate(int argc, char *argv[]) {
   if (device_thread)
     device_thread->request_stop();
   show_efficiency(simulation_clocks, simulation_instructions, simulation_time);
-  SDL_CloseAudio();
+  if (config.enable_audio)
+    SDL_CloseAudio();
   return result;
 }
