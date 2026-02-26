f6813939b9a20ba31f240940079ce62f0dec75ad
tracer-ysyx <tracer@ysyx.org>
2026-02-26 18:10:30 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:10:30 up 11 min,  1 user,  load average: 0.30, 0.52, 0.41

--- npc/include/Expression/ExpressionToken.h
+++ npc/include/Expression/ExpressionToken.h
@@ -6,9 +6,9 @@
 namespace Expr {
 using namespace ctre::literals;
 using TokenizorType = std::function<std::string_view(std::string_view)>;
-TokenizorType make_token_re(auto re) {
-  return [re](std::string_view str) -> std::string_view {
-    auto m = re.search(str);
+template <ctll::fixed_string re> TokenizorType make_lexer() {
+  return [](std::string_view str) -> std::string_view {
+    auto m = ctre::search<re>(str);
     if (m)
       return m.to_view();
     else
@@ -38,29 +38,28 @@ const struct {
   int priority = -1;
   Catagory cata = Catagory::OPERATOR_2;
 } token_types[] = {
-    {make_token_re(R"(^(?:0x[a-fA-F0-9]*|0[0-7]+|[1-9][0-9]*|0))"_ctre), TK_NUM,
-     -1, Catagory::OPERAND},
-    {make_token_re(R"(^\$[a-z]{0,2}[0-9]?)"_ctre), TK_REG, -1,
+    {make_lexer<R"(^(?:0x[a-fA-F0-9]*|0[0-7]+|[1-9][0-9]*|0))">(), TK_NUM, -1,
      Catagory::OPERAND},
-    {make_token_re(R"(^\+)"_ctre), '+', 40},
-    {make_token_re(R"(^-)"_ctre), '-', 40},
-    {make_token_re(R"(^\*)"_ctre), '*', 100},
-    {make_token_re(R"(^/)"_ctre), '/', 100},
-    {make_token_re(R"(^%)"_ctre), '%', 100},
-    {make_token_re(R"(^==)"_ctre), TK_EQ, 20},
-    {make_token_re(R"(^!=)"_ctre), TK_NEQ, 20},
-    {make_token_re(R"(^>=)"_ctre), TK_GE, 30},
-    {make_token_re(R"(^<=)"_ctre), TK_LE, 30},
-    {make_token_re(R"(^>)"_ctre), '>', 30},
-    {make_token_re(R"(^<)"_ctre), '<', 30},
-    {make_token_re(R"(^!)"_ctre), '!', -1, Catagory::OPERATOR_1},
-    {make_token_re(R"(^&&)"_ctre), TK_BOOL_AND, 4},
-    {make_token_re(R"(^\|\|)"_ctre), TK_BOOL_OR, 3},
-    {make_token_re(R"(^~)"_ctre), '~', -1, Catagory::OPERATOR_1},
-    {make_token_re(R"(^&)"_ctre), '&', 7},
-    {make_token_re(R"(^\|)"_ctre), '|', 5},
-    {make_token_re(R"(^\^)"_ctre), '^', 6},
-    {make_token_re(R"(^\()"_ctre), '(', -1, Catagory::OPERAND},
-    {make_token_re(R"(^\))"_ctre), ')', -1, Catagory::OPERATOR_1}};
+    {make_lexer<R"(^\$[a-z]{0,2}[0-9]?)">(), TK_REG, -1, Catagory::OPERAND},
+    {make_lexer<R"(^\+)">(), '+', 40},
+    {make_lexer<R"(^-)">(), '-', 40},
+    {make_lexer<R"(^\*)">(), '*', 100},
+    {make_lexer<R"(^/)">(), '/', 100},
+    {make_lexer<R"(^%)">(), '%', 100},
+    {make_lexer<R"(^==)">(), TK_EQ, 20},
+    {make_lexer<R"(^!=)">(), TK_NEQ, 20},
+    {make_lexer<R"(^>=)">(), TK_GE, 30},
+    {make_lexer<R"(^<=)">(), TK_LE, 30},
+    {make_lexer<R"(^>)">(), '>', 30},
+    {make_lexer<R"(^<)">(), '<', 30},
+    {make_lexer<R"(^!)">(), '!', -1, Catagory::OPERATOR_1},
+    {make_lexer<R"(^&&)">(), TK_BOOL_AND, 4},
+    {make_lexer<R"(^\|\|)">(), TK_BOOL_OR, 3},
+    {make_lexer<R"(^~)">(), '~', -1, Catagory::OPERATOR_1},
+    {make_lexer<R"(^&)">(), '&', 7},
+    {make_lexer<R"(^\|)">(), '|', 5},
+    {make_lexer<R"(^\^)">(), '^', 6},
+    {make_lexer<R"(^\()">(), '(', -1, Catagory::OPERAND},
+    {make_lexer<R"(^\))">(), ')', -1, Catagory::OPERATOR_1}};
 constexpr size_t NR_TOKEN_TYPES = sizeof(token_types) / sizeof(token_types[0]);
 } // namespace Expr
