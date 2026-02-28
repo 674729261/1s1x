3f642a7dc1d51955f4eddd5c36f4aae98e4a3746
tracer-ysyx <tracer@ysyx.org>
2026-02-28 23:52:09 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:52:09 up 10:18,  1 user,  load average: 0.55, 0.79, 0.73

--- npc/include/Tracer.h
+++ npc/include/Tracer.h
@@ -1,8 +1,10 @@
 #pragma once
+#include "spdlog/spdlog.h"
 #include <ELFParser.h>
 #include <Simulate.h>
 #include <cstddef>
 #include <cstdint>
+#include <format>
 inline std::array<uint32_t, 16> ftracer_gpr_last;
 
 inline void sync_ftracer() {
@@ -29,19 +31,20 @@ inline void update_ftracer(uint32_t inst, uint32_t pc) {
     imm |= -(imm & 0x800);
     dnxt_pc = (imm + ftracer_gpr_last[rs1]) & ~0x1;
   }
-
+  std::string indent = "", info = "";
   if ((opcode == 0x67 || opcode == 0x6f) && rd == 1) {
     for (int i = 0; i < sym_table->stack_cnt(); i++)
-      std::print(" ");
+      indent += "  ";
     int to_symbol = sym_table->find_symbol_by_addr(dnxt_pc);
-    std::println("call {}@{:#010x}", sym_table->find_symbol_name(to_symbol),
-                 pc);
+    info = std::format("{}call {}@{:#010x}", indent,
+                       sym_table->find_symbol_name(to_symbol), pc);
     sym_table->push_call_stack(to_symbol, pc);
   } else if (inst == 0x00008067) {
     ProgSymTab::Call top = sym_table->pop_call_stack();
     for (int i = 0; i < sym_table->stack_cnt(); i++)
-      std::print(" ");
-    std::println("ret  {}@{:#010x}", sym_table->find_symbol_name(top.symbol),
-                 pc);
+      indent += "  ";
+    info = std::format("{}ret  {}@{:#010x}", indent,
+                       sym_table->find_symbol_name(top.symbol), pc);
   }
+  spdlog::info(info);
 }
