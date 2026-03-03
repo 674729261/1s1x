14f46ce4a628c7b2a90b8421859b56a3569ae150
tracer-ysyx <tracer@ysyx.org>
2026-03-04 00:28:20 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:28:20 up  4:21,  1 user,  load average: 1.04, 0.94, 0.83

--- npc_soc/src/main/scala/empty/IDU.scala
+++ npc_soc/src/main/scala/empty/IDU.scala
@@ -305,7 +305,7 @@ class IDU() extends Module {
   has_inst := MuxCase(
     has_inst,
     Seq(
-      (in.fire) -> true.B,
+      (in.fire && !flush.valid) -> true.B,
       (out.fire || flush.valid) -> false.B
     )
   )
