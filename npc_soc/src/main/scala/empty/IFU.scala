0681ab45df658fe581759f31e3b8fb6a9e309383
tracer-ysyx <tracer@ysyx.org>
2026-03-04 00:52:06 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:52:06 up  4:45,  1 user,  load average: 3.04, 1.72, 1.31

--- npc_soc/src/main/scala/empty/IFU.scala
+++ npc_soc/src/main/scala/empty/IFU.scala
@@ -23,30 +23,27 @@ class IFU(init_pc: UInt) extends Module {
   val has_inst = RegInit(Bool(), false.B)
 
   val fetch_pc = RegInit(UInt(32.W), init_pc)
-  val out = IO(DecoupledIO(new MessageIFU2IDU))
-  val exu_dnpc_fire = in.exu_dnpc_valid && in.exu_dnpc_ready
 
   fetch_port <> icache.fetch_port
-  icache.io.valid := (out.fire || !has_inst)
-  icache.io.addr := Mux(
-    has_inst && !exu_dnpc_fire,
-    fetch_pc + 4.U(32.W),
-    fetch_pc
-  )
+  icache.io.valid := !has_inst
+  icache.io.addr := fetch_pc
   icache.io.clear := in.clear_icache_valid
   in.clear_icache_ok := icache.io.clear_ok
+
+  val out = IO(DecoupledIO(new MessageIFU2IDU))
 
   val cache_fire = icache.io.valid && icache.io.ready
   has_inst := MuxCase(
     has_inst,
     Seq(
-      (exu_dnpc_fire || (out.fire && !cache_fire)) -> false.B,
-      (cache_fire && !out.fire) -> true.B
+      cache_fire -> true.B,
+      out.fire -> false.B
     )
   )
 
   val inst_reg =
     RegEnable(icache.io.rdata, cache_fire)
+  val exu_dnpc_fire = in.exu_dnpc_valid && in.exu_dnpc_ready
   fetch_pc := MuxCase(
     fetch_pc,
     Seq(
@@ -57,7 +54,7 @@ class IFU(init_pc: UInt) extends Module {
 
   in.exu_dnpc_ready := has_inst
 
-  out.valid := has_inst && !exu_dnpc_fire
+  out.valid := has_inst
 
   out.bits.inst := inst_reg
   out.bits.pc := fetch_pc
