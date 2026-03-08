e90419ab723dae7d30a6fda1d354bdd16b3c0937
tracer-ysyx <tracer@ysyx.org>
2026-03-09 02:32:49 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:32:49 up 58 min,  1 user,  load average: 5.06, 2.23, 1.58

--- npc_soc/src/main/scala/empty/EXU.scala
+++ npc_soc/src/main/scala/empty/EXU.scala
@@ -44,9 +44,6 @@ class ConflictInfoRD extends Bundle {
   val interruption = Output(Bool())
   val rd_id = Output(UInt(5.W))
   val csr_id = Output(UInt(12.W))
-
-  val ok_to_forward_rd = Output(Bool())
-  val rd_data = Output(UInt(32.W))
 }
 
 class EXU() extends Module {
@@ -123,8 +120,6 @@ class EXU() extends Module {
   conf.csr_dest_valid := has_signal && in.bits.controls.is_csr_visit
   conf.csr_id := in.bits.controls.csrd
   conf.interruption := in.bits.itype.is_ecall
-  conf.ok_to_forward_rd := !in.bits.controls.is_gpr_wdata_from_ram
-  conf.rd_data := out.bits.write_info.gpr_wdata
 
   out.bits.itype := in.bits.itype
   out.bits.rd_valid := in.bits.rd_valid
