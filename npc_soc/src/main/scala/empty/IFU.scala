6812b68f8f87f9657fceb80338c4854aabdd19a0
tracer-ysyx <tracer@ysyx.org>
2026-03-09 20:20:13 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:20:13 up  3:07,  1 user,  load average: 3.68, 1.37, 0.74

--- npc_soc/src/main/scala/empty/IFU.scala
+++ npc_soc/src/main/scala/empty/IFU.scala
@@ -56,7 +56,7 @@ class IFU(init_pc: UInt) extends Module {
   fetch_pc_r := MuxCase(
     fetch_pc_r,
     Seq(
-      (in.flush_valid) -> in.exu_dnpc,
+      (in.flush_valid) -> (in.exu_dnpc + 4.U(32.W)),
       (cache_afire) -> pc_next_predicted
     )
   )
