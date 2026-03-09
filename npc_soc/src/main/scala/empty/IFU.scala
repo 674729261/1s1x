c220f94baece43b01c432a06cfd15f2387c998f9
tracer-ysyx <tracer@ysyx.org>
2026-03-09 21:37:21 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:37:21 up  4:24,  1 user,  load average: 2.87, 1.22, 0.62

--- npc_soc/src/main/scala/empty/IFU.scala
+++ npc_soc/src/main/scala/empty/IFU.scala
@@ -19,53 +19,60 @@ class IFU(init_pc: UInt) extends Module {
   val icache = Module(new ICache(5, 2))
   val has_inst_r = RegInit(Bool(), false.B)
 
-  val fetch_pc = RegInit(UInt(32.W), init_pc)
+  val fetch_pc_r = RegInit(UInt(32.W), init_pc)
+  val fetch_pc = Mux(in.flush_valid, in.exu_dnpc, fetch_pc_r)
   val out = IO(DecoupledIO(new MessageIFU2IDU))
-  val cache_fire = icache.io.valid && icache.io.ready
+  val cache_afire = icache.io.avalid && icache.io.aready
+  val cache_rfire = icache.io.rvalid && icache.io.rready
 
-  val pc_next_predicted = fetch_pc + 4.U(32.W)
-  val pending_flush_r = RegInit(Bool(), false.B)
-  pending_flush_r := MuxCase(
-    pending_flush_r,
-    Seq(
-      in.flush_valid -> true.B,
-      cache_fire -> false.B
-    )
-  )
-  val pending_flush = pending_flush_r || in.flush_valid
+  val timestamp_r = RegInit(UInt(3.W), 0.U(3.W))
+  val timestamp = Mux(in.flush_valid, timestamp_r + 1.U(3.W), timestamp_r)
 
-  val has_inst = has_inst_r && !pending_flush
+  val pc_next_predicted = fetch_pc_r + 4.U(32.W)
+
+  val has_inst = has_inst_r && !in.flush_valid
 
   fetch_port <> icache.fetch_port
-  icache.io.valid := (out.fire || !has_inst)
-  icache.io.addr := Mux(
-    has_inst,
-    pc_next_predicted,
-    fetch_pc
-  )
-  icache.io.clear := false.B
+  icache.io.addr := fetch_pc
+  icache.io.timestamp_req := timestamp
+  icache.io.avalid := true.B
+  icache.io.rready := out.fire || !has_inst
+
+  val cache_rfire_and_up_to_date =
+    cache_rfire && (icache.io.timestamp_res === timestamp)
 
   has_inst_r := MuxCase(
     has_inst_r,
     Seq(
-      (cache_fire && !out.fire && !pending_flush) -> true.B,
-      (pending_flush || (out.fire && !cache_fire)) -> false.B
+      (cache_rfire_and_up_to_date && !out.fire) -> true.B,
+      (in.flush_valid || (out.fire && !cache_rfire_and_up_to_date)) -> false.B
     )
   )
 
   val inst_reg =
-    RegEnable(icache.io.rdata, cache_fire)
-  fetch_pc := MuxCase(
-    fetch_pc,
+    RegEnable(icache.io.rdata, cache_rfire)
+  val inst_rpc =
+    RegEnable(icache.io.rpc, cache_rfire)
+  fetch_pc_r := MuxCase(
+    fetch_pc_r,
     Seq(
-      (out.fire) -> pc_next_predicted,
-      (pending_flush && cache_fire) -> in.exu_dnpc
+      (in.flush_valid) -> Mux(
+        cache_afire,
+        in.exu_dnpc + 4.U(32.W),
+        in.exu_dnpc
+      ),
+      (cache_afire) -> pc_next_predicted
     )
+  )
+
+  timestamp_r := MuxCase(
+    timestamp_r,
+    Seq(in.flush_valid -> (timestamp_r + 1.U(3.W)))
   )
 
   out.valid := has_inst
 
   out.bits.inst := inst_reg
-  out.bits.pc := fetch_pc
+  out.bits.pc := inst_rpc
 
 }
