7993e20a8a72bd15f312c0a24715429afe2c4e0a
tracer-ysyx <tracer@ysyx.org>
2026-03-10 19:18:43 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:18:43 up  3:47,  1 user,  load average: 4.28, 1.50, 0.81

--- npc_soc/src/main/scala/empty/ICache.scala
+++ npc_soc/src/main/scala/empty/ICache.scala
@@ -113,7 +113,7 @@ class ICache(linesize_2pow: Int, linecount_2pow: Int) extends Module {
   val cache_wdata = Wire(new CacheLine(linesize_2pow, linecount_2pow))
   cache_wdata.data := axi_rdata_latched.asTypeOf(Vec(words, UInt(32.W)))
   cache_wdata.tag := input_tag
-  when(!in_cache && ifu_rfire && !io.rdiscard && should_cache) {
+  when(!in_cache && ifu_rfire && should_cache) {
     content.write(input_cache_index, cache_wdata)
     valid_flags(input_cache_index) := true.B
   }
