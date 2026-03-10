8fc1149aba5f9ac6f489506530d1768a6535bac9
tracer-ysyx <tracer@ysyx.org>
2026-03-11 00:28:16 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:28:16 up 13 min,  1 user,  load average: 5.78, 2.15, 1.05

--- npc_soc/src/main/scala/empty/EXU.scala
+++ npc_soc/src/main/scala/empty/EXU.scala
@@ -122,12 +122,12 @@ class EXU() extends Module {
   val should_flush = (in.bits.nxtpc_predicted =/= out_pc.dnpc)
   val should_branch = in.bits.controls.is_branch && branch.io.jump
 
-  out_pc.dnpc := Mux1H(
+  out_pc.dnpc := MuxCase(
+    snpc,
     Seq(
-      in.bits.controls.is_branch -> Mux(branch.io.jump, alu.io.out, snpc),
+      should_branch -> alu.io.out,
       in.bits.controls.is_dnpc_jal_or_jalr -> alu.io.out,
-      in.bits.controls.is_dnpc_csr_jump -> in.bits.sources.alu_a_or_mepc_or_mtvec,
-      in.bits.controls.is_dnpc_snpc -> snpc
+      in.bits.controls.is_dnpc_csr_jump -> in.bits.sources.alu_a_or_mepc_or_mtvec
     )
   )
 
