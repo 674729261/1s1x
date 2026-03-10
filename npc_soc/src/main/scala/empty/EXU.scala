840404758bb249fe045535f391cecaef3bcced76
tracer-ysyx <tracer@ysyx.org>
2026-03-10 15:40:14 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:40:14 up 9 min,  1 user,  load average: 1.06, 1.21, 0.72

--- npc_soc/src/main/scala/empty/EXU.scala
+++ npc_soc/src/main/scala/empty/EXU.scala
@@ -135,9 +135,9 @@ class EXU() extends Module {
   out.bits.rd_valid := in.bits.rd_valid
   out.valid := has_signal
   val is_first_cycle = RegNext(in.fire, false.B)
-  // btb.wen := ((in.bits.controls.is_branch && in.bits.inst(
-  //   31
-  // )) || in.bits.itype.is_jal) && is_first_cycle
+  btb.wen := ((in.bits.controls.is_branch && in.bits.inst(
+    31
+  )) || in.bits.itype.is_jal) && is_first_cycle
   btb.wen := in.bits.itype.is_jal && is_first_cycle
   btb.write_pc := in.bits.pc
   btb.target := alu.io.out
