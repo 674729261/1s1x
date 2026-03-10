22cd172613ea48384dd189c1cf35bd4d3d295323
tracer-ysyx <tracer@ysyx.org>
2026-03-10 20:21:53 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:53 up  4:50,  1 user,  load average: 0.69, 0.43, 0.58

--- npc_soc/src/main/scala/empty/EXU.scala
+++ npc_soc/src/main/scala/empty/EXU.scala
@@ -150,10 +150,11 @@ class EXU() extends Module {
   btb.target := alu.io.out
 
   out.bits.write_info.dnpc := out_pc.dnpc
-  out_pc.ifu_flush_valid := (should_flush && is_first_cycle)
-  out_pc.idu_flush_valid := (should_flush && is_first_cycle)
+  val flush_high = (should_flush && is_first_cycle)
+  out_pc.ifu_flush_valid := flush_high
+  out_pc.idu_flush_valid := flush_high
 
-  in.ready := (out.fire || !has_signal)
+  in.ready := (out.fire || !has_signal) && !flush_high
 
   block(PerformanceCounterLayer) {
     val performancecounter_icache = Module(new PerformanceCounter_ICache)
