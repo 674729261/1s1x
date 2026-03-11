f77139ed1e948cb59eec0de46bb767025eade875
ysyx_25080216-Wang Xiangyu <2292683261@qq.com>
2026-03-12 00:49:51 +0800

    一个fence.i的反例

---
+++ soc_kernal/fenceitest/tests/test.c
@@ -0,0 +1,38 @@
+#include <klib.h>
+#include <stdint.h>
+int v = 0;
+__attribute__((noinline)) __attribute__((aligned(32))) int f() {
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  asm volatile("nop");
+  return 0;
+}
+__attribute__((noinline)) void g() { asm volatile("li a0, 456"); }
+
+int main() {
+  f();
+  volatile uint32_t *p = (uint32_t *)f;
+  for (int i = 0; i < 16; i++) {
+    *p = *(uint32_t *)g;
+    p++;
+  }
+  asm volatile("fence.i");
+  int r = f();
+  printf("r is %d, should be 456\n", r);
+  if (r == 456)
+    return 0;
+  else
+    return -1;
+}
