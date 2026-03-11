23a613f843029ac2584f1fdfad8b35f9b9cc3f78
tracer-ysyx <tracer@ysyx.org>
2026-03-12 00:11:14 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:11:14 up 22 min,  1 user,  load average: 0.19, 0.14, 0.10

--- soc_kernal/fenceitest/tests/test.c
+++ soc_kernal/fenceitest/tests/test.c
@@ -2,9 +2,9 @@
 #include <stdint.h>
 int v = 0;
 __attribute__((noinline)) void f() { asm volatile("ret"); }
-
 __attribute__((noinline)) void g() { asm volatile("call h"); }
-__attribute__((noinline)) void h() { v++; }
+__attribute__((noinline)) void h() { asm volatile("call i"); }
+__attribute__((noinline)) void i() { v++; }
 
 int main() {
   uint32_t *p_f = (uint32_t *)f;
@@ -12,4 +12,9 @@ int main() {
   asm volatile("call g");
   *(volatile uint32_t *)p_g = *p_f;
   g();
+  printf("V is %d, should be 1", v);
+  if (v == 1)
+    return -1;
+  else
+    return 0;
 }
