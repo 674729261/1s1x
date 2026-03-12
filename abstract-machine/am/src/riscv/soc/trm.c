229a9e9de598fbc78ea51c7092edb2539301c899
tracer-ysyx <tracer@ysyx.org>
2026-03-13 01:03:38 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:03:38 up  2:39,  1 user,  load average: 0.19, 0.79, 0.74

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -85,15 +85,40 @@ void _trm_init() {
   halt(ret);
 }
 
+__attribute__((section(".fsbl"))) static void *
+__fsbl__memcpy(void *out, const void *in, size_t n) {
+  unsigned char *d = (unsigned char *)out;
+  const unsigned char *s = (const unsigned char *)in;
+
+  while ((((uintptr_t)d) & 0x3) && n) {
+    *d++ = *s++;
+    n--;
+  }
+
+  while (n >= 4) {
+    *(uint32_t *)d = *(const uint32_t *)s;
+    d += 4;
+    s += 4;
+    n -= 4;
+  }
+
+  while (n-- > 0) {
+    *d++ = *s++;
+  }
+  return out;
+}
+
 __attribute__((section(".fsbl"))) void fstbootloader() {
   extern uint32_t __ssbl_load_start, __ssbl_load_end, __ssbl_start;
   uint32_t *src = &__ssbl_load_start;
   uint32_t *dst = &__ssbl_start;
-  while (src < &__ssbl_load_end) {
-    *dst = *src;
-    ++dst;
-    ++src;
-  }
+  __fsbl__memcpy(dst, src, &__ssbl_load_end - &__ssbl_load_start);
+
+  // while (src < &__ssbl_load_end) {
+  //   *dst = *src;
+  //   ++dst;
+  //   ++src;
+  // }
 }
 
 __attribute__((section(".ssbl"))) static void *
