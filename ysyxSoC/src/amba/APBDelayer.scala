3248210253d86e41a2c853ba43522bf8cd43ffa4
tracer-ysyx <tracer@ysyx.org>
2026-03-12 11:47:57 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:47:57 up 14 min,  1 user,  load average: 3.28, 1.45, 0.84

--- ysyxSoC/src/amba/APBDelayer.scala
+++ ysyxSoC/src/amba/APBDelayer.scala
@@ -22,7 +22,7 @@ class apb_delayer extends BlackBox {
   val io = IO(new APBDelayerIO)
 }
 
-class APBDelayerChisel(ratio: Double = 9.89032, scale_2pow: Long = 6)
+class APBDelayerChisel(ratio: Double = 30, scale_2pow: Long = 6)
     extends Module {
   val io = IO(new APBDelayerIO)
   val countup_amount = math.round((ratio - 1.0) * math.pow(2.0, scale_2pow))
