76a6c0ebff975771aa0d00f070294e7b4c23e7c8
tracer-ysyx <tracer@ysyx.org>
2026-03-13 00:45:00 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:45:00 up  2:20,  1 user,  load average: 0.78, 0.41, 0.32

--- abstract-machine/am/src/riscv/soc/timer.c
+++ abstract-machine/am/src/riscv/soc/timer.c
@@ -1,23 +1,23 @@
 #include "riscv/riscv.h"
 #include <am.h>
 #include <klib.h>
+#include <soc.h>
 #include <stdint.h>
-
 void __am_timer_init() {
   // outl(RTC_ADDR, 0);
   // outl(RTC_ADDR + 4, 0);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // uint32_t lower = inl(RTC_ADDR);
-  // uint32_t upper = inl(RTC_ADDR + 0x4);
-  // uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
-  // static uint64_t last = 0;
-  // if (last > uptime->us / 1000000) {
-  //   printf("!!%lld\r", last);
-  //   last = uptime->us / 1000000;
-  // }
-  // printf("!!%lld\r", uptime->us);
+  uint32_t lower = inl(RTC_ADDR);
+  uint32_t upper = inl(RTC_ADDR + 0x4);
+  uptime->us = ((uint64_t)upper << 32ull) | (uint64_t)lower;
+  static uint64_t last = 0;
+  if (last > uptime->us / 1000000) {
+    printf("!!%lld\r", last);
+    last = uptime->us / 1000000;
+  }
+  printf("!!%lld\r", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
