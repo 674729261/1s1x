797c8b10aceef306d886c4e9798fe680da9a5dce
tracer-ysyx <tracer@ysyx.org>
2026-03-12 23:10:10 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:10:10 up 45 min,  1 user,  load average: 4.45, 2.45, 2.19

--- ysyxSoC/src/amba/AXI4Delayer.scala
+++ ysyxSoC/src/amba/AXI4Delayer.scala
@@ -26,7 +26,7 @@ class axi4_delayer extends BlackBox {
   val io = IO(new AXI4DelayerIO)
 }
 
-class AXI4DelayerChisel(ratio: Double = 9.89032 , scale_2pow: Long = 6)
+class AXI4DelayerChisel(ratio: Double = 11.13785, scale_2pow: Long = 6)
     extends Module {
   val io = IO(new AXI4DelayerIO)
   val countup_amount = math.round((ratio - 1.0) * math.pow(2.0, scale_2pow))
