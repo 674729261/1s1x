3d3969437173646a0100851dd7f7418f49bbd15e
tracer-ysyx <tracer@ysyx.org>
2026-03-13 01:53:35 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:53:35 up  3:29,  1 user,  load average: 1.20, 1.00, 0.83

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -96,7 +96,7 @@ module sdram_axi_core (
     else if (req_fire) ok_to_accept <= 1'b0;
     else if (state == ST_IDLE) ok_to_accept <= 1'b1;
   end
-  assign inport_accept_o = ok_to_accept && in_req && ((inport_rd_i && (state == ST_READ_WAIT || state == ST_READ)) || (state == ST_WRITE && inport_wr_i != 4'b0));
+  assign inport_accept_o = ok_to_accept && (in_req && ((inport_rd_i && (state == ST_READ_WAIT || state == ST_READ)) || (state == ST_WRITE && inport_wr_i != 4'b0)));
   wire req_fire = in_req && inport_accept_o;
 
   wire which_chip = inport_addr_i[SDRAM_ADDR_W+1+1];
