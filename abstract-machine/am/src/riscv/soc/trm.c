1c29dc2fd686547b02598cf56388097e7d061848
tracer-ysyx <tracer@ysyx.org>
2026-03-13 01:08:41 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:08:41 up  2:44,  1 user,  load average: 0.52, 0.52, 0.62

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -87,29 +87,29 @@ void _trm_init() {
 
 __attribute__((section(".fsbl"))) void fstbootloader() {
   extern uint32_t __ssbl_load_start, __ssbl_load_end, __ssbl_start;
-  uint32_t *src = &__ssbl_load_start;
-  uint32_t *dst = &__ssbl_start;
-  // size_t n = &__ssbl_load_end - &__ssbl_load_start;
-  // while ((((uintptr_t)dst) & 0x3) && n) {
-  //   *dst++ = *src++;
-  //   n--;
-  // }
-
-  // while (n >= 4) {
-  //   *(uint32_t *)dst = *(const uint32_t *)src;
-  //   dst += 4;
-  //   src += 4;
-  //   n -= 4;
-  // }
-
-  // while (n-- > 0) {
-  //   *dst++ = *src++;
-  // }
-  while (src < &__ssbl_load_end) {
-    *dst = *src;
-    ++dst;
-    ++src;
+  uint8_t *src = (uint8_t *)&__ssbl_load_start;
+  uint8_t *dst = (uint8_t *)&__ssbl_start;
+  size_t n = &__ssbl_load_end - &__ssbl_load_start;
+  while ((((uintptr_t)dst) & 0x3) && n) {
+    *dst++ = *src++;
+    n--;
   }
+
+  while (n >= 4) {
+    *(uint32_t *)dst = *(const uint32_t *)src;
+    dst += 4;
+    src += 4;
+    n -= 4;
+  }
+
+  while (n-- > 0) {
+    *dst++ = *src++;
+  }
+  // while (src < &__ssbl_load_end) {
+  //   *dst = *src;
+  //   ++dst;
+  //   ++src;
+  // }
 }
 
 __attribute__((section(".ssbl"))) static void *
