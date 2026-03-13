4458cea84ffdfe180d1006107d9f013461956241
tracer-ysyx <tracer@ysyx.org>
2026-03-13 22:38:34 +0800

    > sim RTL with nvboard ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:38:34 up 3 min,  1 user,  load average: 0.37, 0.29, 0.12

--- am-kernels/kernels/typing-game/game.c
+++ am-kernels/kernels/typing-game/game.c
@@ -10,7 +10,7 @@
 #define COL_WHITE 0xeeeeee
 #define COL_RED 0xff0033
 #define COL_GREEN 0x00cc33
-#define COL_PURPLE 0x2a0a29
+#define COL_PURPLE 0x43aebd
 
 enum { WHITE = 0, RED, GREEN, PURPLE };
 struct character {
