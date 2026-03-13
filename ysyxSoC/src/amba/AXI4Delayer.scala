dc55a635844ef948c579af16ce52feed3b9ccac3
tracer-ysyx <tracer@ysyx.org>
2026-03-14 01:20:50 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:20:50 up  2:45,  1 user,  load average: 4.15, 1.58, 1.27

--- ysyxSoC/src/amba/AXI4Delayer.scala
+++ ysyxSoC/src/amba/AXI4Delayer.scala
@@ -35,10 +35,11 @@ class AXI4DelayerChisel(ratio: Double = 10, scale_2pow: Long = 6)
   val sIDLE :: sDELAY :: sWAIT :: Nil = Enum(3)
   val state_ar = RegInit(sIDLE)
   val trigger_ar = io.in.ar.valid
-  val fire_ar = io.in.ar.valid && io.in.ar.ready
+  val fire_ar = io.out.ar.valid && io.out.ar.ready
   val counter_ar = RegInit(UInt(32.W), 0.U)
 
   io.out.ar.valid := state_ar === sWAIT
+  io.in.ar.ready := (state_ar === sWAIT) && io.out.ar.ready
 
   state_ar := MuxLookup(state_ar, sDELAY)(
     Seq(
@@ -58,10 +59,11 @@ class AXI4DelayerChisel(ratio: Double = 10, scale_2pow: Long = 6)
 
   val state_aw = RegInit(sIDLE)
   val trigger_aw = io.in.aw.valid
-  val fire_aw = io.in.aw.valid && io.in.aw.ready
+  val fire_aw = io.out.aw.valid && io.out.aw.ready
   val counter_aw = RegInit(UInt(32.W), 0.U)
 
   io.out.aw.valid := state_aw === sWAIT
+  io.in.aw.ready := (state_aw === sWAIT) && io.out.aw.ready
 
   state_aw := MuxLookup(state_aw, sDELAY)(
     Seq(
