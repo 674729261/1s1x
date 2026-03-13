dbd68927362f860f028993822a0b0d43d2281930
tracer-ysyx <tracer@ysyx.org>
2026-03-13 13:11:09 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:11:09 up  1:14,  1 user,  load average: 0.98, 0.69, 0.57

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -142,9 +142,9 @@ module sdram_axi_core (
       else state_nxt = ST_IDLE;
       ST_READ_ACTIVATE: state_nxt = ST_READ_WAIT;
       ST_READ_WAIT: state_nxt = (wait_count == 'd0) ? ST_READ : ST_READ_WAIT;
-      ST_READ: state_nxt = (read_count == 'd0) ? ST_SLEEP1 : ST_READ;
+      ST_READ: state_nxt = (read_count == 'd0) ? ST_IDLE : ST_READ;
       ST_WRITE_ACTIVATE: state_nxt = ST_WRITE;
-      ST_WRITE: state_nxt = ST_SLEEP1;
+      ST_WRITE: state_nxt = ST_IDLE;
       default: state_nxt = state;
     endcase
   end
