3c705eaa867ab8ec4920eced087f6d3ee96618fc
tracer-ysyx <tracer@ysyx.org>
2026-03-14 01:00:26 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:00:26 up  2:24,  1 user,  load average: 3.19, 3.10, 1.61

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -105,6 +105,8 @@ __attribute__((section(".fsbl"))) void fstbootloader() {
   while (n-- > 0) {
     *dst++ = *src++;
   }
+
+  asm volatile("fence.i");
   // while (src < &__ssbl_load_end) {
   //   *dst = *src;
   //   ++dst;
@@ -182,4 +184,5 @@ __attribute__((section(".ssbl"))) void secbootloader() {
   //   dst = &rt_begin;
   //   __ssbl__memcpy(dst, src, &rt_load_end - &rt_load_begin);
   // #endif
+  asm volatile("fence.i");
 }
