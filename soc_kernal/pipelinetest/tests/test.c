da4d4b78d45e5e1ff7fe4b6569170a62bcf751e6
tracer-ysyx <tracer@ysyx.org>
2026-03-16 00:42:29 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:42:29 up  2:21,  1 user,  load average: 0.35, 0.39, 0.48

--- soc_kernal/pipelinetest/tests/test.c
+++ soc_kernal/pipelinetest/tests/test.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-  asm volatile("lui a0, 0xf\n"
+  asm volatile("lui a0, 0x8\n"
                "begin_test_loop:\n"
                "addi t0, t0, 1\n"
                "add t0, t0, t0\n"
