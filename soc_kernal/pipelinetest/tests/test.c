a763765ff0a14c614b32f30d6ebac595d79fe4e9
tracer-ysyx <tracer@ysyx.org>
2026-03-16 00:14:17 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:14:17 up  1:53,  1 user,  load average: 0.66, 0.79, 0.79

--- soc_kernal/pipelinetest/tests/test.c
+++ soc_kernal/pipelinetest/tests/test.c
@@ -2,16 +2,17 @@
 #include <stdint.h>
 
 int main() {
-  for (int i = 0; i < 65536; i++) {
-    asm volatile("addi t0, t0, 1");
-    asm volatile("add t0, t0, t0");
-    asm volatile("addi t1, t1, 2");
-    asm volatile("add t1, t1, t1");
-    asm volatile("sub t0, t1, t0");
-    asm volatile("addi t0, t0, 1");
-    asm volatile("add t0, t0, t0");
-    asm volatile("addi t1, t1, 2");
-    asm volatile("add t1, t1, t1");
-    asm volatile("sub t0, t1, t0");
-  }
+
+  asm volatile("begin:\n"
+               "addi t0, t0, 1\n"
+               "add t0, t0, t0\n"
+               "addi t1, t1, 2\n"
+               "add t1, t1, t1\n"
+               "sub t0, t1, t0\n"
+               "addi t0, t0, 1\n"
+               "add t0, t0, t0\n"
+               "addi t1, t1, 2\n"
+               "add t1, t1, t1\n"
+               "sub t0, t1, t0\n"
+               "j begin");
 }
