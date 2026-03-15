d707466146d907d77dd50dcf30fc789f3d2f1273
tracer-ysyx <tracer@ysyx.org>
2026-03-15 23:51:24 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:51:24 up  1:30,  1 user,  load average: 0.34, 0.48, 0.43

--- soc_kernal/pipelinetest/tests/test.c
+++ soc_kernal/pipelinetest/tests/test.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 int main() {
-  for (int i = 0; i < 32768; i++) {
+  for (int i = 0; i < 65536; i++) {
     asm volatile("addi a1, a1, 1");
     asm volatile("add a1, a1, a1");
   }
