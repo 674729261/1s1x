ea3869adfc412690a56e281629c3644e152a2ca6
tracer-ysyx <tracer@ysyx.org>
2026-03-16 00:11:53 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:11:53 up  1:50,  1 user,  load average: 2.68, 1.02, 0.86

--- npc_soc/csrc/Setup.cpp
+++ npc_soc/csrc/Setup.cpp
@@ -20,8 +20,7 @@ void register_logger(argparse::ArgumentParser &program) {
   if (provided_logfile) {
     string log_path = program.get("--log");
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_pattern(
-        "[%Y-%m-%d %H:%M:%S.%e] [thread %t] [%^%l%$] - %v");
+    console_sink->set_pattern("[%Y-%m-%d %H:%M:%S] [%t] [%^%l%$] %v");
 
     auto file_sink =
         std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_path, true);
