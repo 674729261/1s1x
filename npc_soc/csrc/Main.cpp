47bd2d418d8fc6fd2deb42e9d1b1d1b62f44e66d
tracer-ysyx <tracer@ysyx.org>
2026-03-15 23:09:12 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:09:12 up 47 min,  1 user,  load average: 2.95, 1.20, 0.67

--- npc_soc/csrc/Main.cpp
+++ npc_soc/csrc/Main.cpp
@@ -7,7 +7,6 @@
 #include <MROM.h>
 #include <PerformanceCounter.h>
 #include <Ref.h>
-#include <Simulate.h>
 #include <VysyxSoCFull.h>
 #include <VysyxSoCFull___024root.h>
 #include <chrono>
@@ -21,33 +20,10 @@
 
 using std::string;
 
+std::unique_ptr<Dut> dut;
 static bool retire;
 
 extern "C" void notify_retire(int32_t pc, int32_t inst) { retire = true; }
-
-int show_trap_info() {
-  if (dut->getGPR(10) == 0) {
-    spdlog::info("HIT GOOD TRAP");
-    return 0;
-  } else {
-    spdlog::warn("HIT BAD TRAP with a0 = {:010x}", dut->getGPR(10));
-    return -1;
-  }
-}
-
-void show_efficiency(long long clocks, long long instrs,
-                     long long microseconds) {
-  spdlog::info("Simulated clocks : {}", clocks);
-  spdlog::info("Simulated instructions : {}", instrs);
-  if (microseconds == 0 || clocks == 0)
-    return;
-  spdlog::info("Clocks per instruction : {:.3f}",
-               static_cast<double>(clocks) / instrs);
-  spdlog::info("Clocks per second : {:.3f}",
-               static_cast<double>(clocks) / microseconds * 1e6);
-  spdlog::info("Instructions per second : {:.3f}",
-               static_cast<double>(instrs) / microseconds * 1e6);
-}
 
 bool check_difftest(Dut &dut, Ref &ref) {
   bool ret = false;
@@ -67,66 +43,23 @@ bool check_difftest(Dut &dut, Ref &ref) {
   return ret;
 }
 
-void run(unsigned long long steps) {
-  if (sim_state == SimulationState::HALT) {
-    spdlog::info("Program has hit trap @ PC = {:#010x}, a0 = {:#010x}",
-                 dut->getPC(), dut->getGPR(10));
-    return;
-  }
-  if (sim_state == SimulationState::DIFFTEST_FAILED) {
-    check_difftest(*dut, *ref);
-    return;
-  }
-  long long simulation_instructions_steped = 0;
-  long long simulation_clocks_steped = 0;
-  long long simulation_time_steped = 0;
-  auto start_time = std::chrono::steady_clock::now();
-  unsigned long long cur_step = 0;
-  while (sim_state == SimulationState::RUNNING && steps != cur_step) {
-    retire = false;
-    dut->step_one_cycle();
-    if (contextp->gotFinish()) {
-      sim_state = SimulationState::HALT;
-      show_trap_info();
-    }
-    simulation_clocks_steped++;
-    if (retire) {
-      cur_step++;
-      simulation_instructions_steped++;
-      check_difftest(*dut, *ref);
-    }
-  }
-  auto end_time = std::chrono::steady_clock::now();
-  simulation_time_steped =
-      std::chrono::duration_cast<std::chrono::microseconds>(end_time -
-                                                            start_time)
-          .count();
-  if (!config.batch_mode)
-    show_efficiency(simulation_clocks_steped, simulation_instructions_steped,
-                    simulation_time_steped);
-  performance_statistics.simulation_instructions +=
-      simulation_instructions_steped;
-  performance_statistics.simulation_clocks += simulation_clocks_steped;
-  performance_statistics.simulation_time += simulation_time_steped;
-}
-
-int simulate() {
+int simulate(int argc, char *argv[], Config config) {
   // init_mrom(config.image_path);
   init_flash(config.image_path);
-  // Verilated::commandArgs(argc, argv);
+  Verilated::commandArgs(argc, argv);
   std::unique_ptr<VerilatedContext> contextp =
       std::make_unique<VerilatedContext>();
-  // contextp->commandArgs(argc, argv);
+  contextp->commandArgs(argc, argv);
 
   Verilated::traceEverOn(true);
 
   dut = std::make_unique<Dut>(config, contextp.get());
-  ref = std::make_unique<Ref>(config, *dut, 3, 1);
+  Ref ref(config, *dut, 3, 1);
   if (config.nvboard) {
     dut->nvboard_bind();
     nvboard_init();
   }
-  ref->reset(*dut);
+  ref.reset(*dut);
   dut->reset();
   clear_performance_count();
   bool difftest_state = false;
@@ -134,8 +67,8 @@ int simulate() {
   while (!contextp->gotFinish()) {
     retire = false;
     if (dut->getResetCore()) {
-      ref->reset(*dut);
-      ref->sync_state();
+      ref.reset(*dut);
+      ref.sync_state();
       clear_performance_count();
     }
     if (config.nvboard)
@@ -147,8 +80,8 @@ int simulate() {
       // std::println("{:08x}", dut->getPC());
       inst_count++;
       if (config.difftest) {
-        ref->step();
-        difftest_state = check_difftest(*dut, *ref);
+        ref.step();
+        difftest_state = check_difftest(*dut, ref);
         if (difftest_state)
           break;
       }
@@ -173,8 +106,8 @@ int simulate() {
     result = -3;
   }
   dut->print_all_gpr();
-  spdlog::info("Reference cache hit count : {}", ref->getCacheHit());
-  spdlog::info("Reference instruction count : {}", ref->instrCount());
+  spdlog::info("Reference cache hit count : {}", ref.getCacheHit());
+  spdlog::info("Reference instruction count : {}", ref.instrCount());
 
   display_performance(start_time, end_time);
   dut = nullptr;
@@ -182,10 +115,10 @@ int simulate() {
 }
 
 int main(int argc, char *argv[]) {
-  config = process_args(argc, argv);
+  Config config = process_args(argc, argv);
   int return_value = -1;
   try {
-    return_value = simulate();
+    return_value = simulate(argc, argv, config);
   } catch (std::exception e) {
     std::println(std::cerr, "Error : {}", e.what());
   }
