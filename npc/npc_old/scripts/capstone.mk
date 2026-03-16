c2e31b5945cf5ab8f2b3edd0960120e62305984a
tracer-ysyx <tracer@ysyx.org>
2026-03-16 22:43:44 +0800

    > compile NEMU ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:43:44 up 10:48,  1 user,  load average: 0.10, 0.26, 0.43

--- npc/npc_old/scripts/capstone.mk
+++ npc/npc_old/scripts/capstone.mk
@@ -1,5 +1,5 @@
 LIBCAPSTONE = tools/capstone/repo/libcapstone.so.6
-SO_PATH_CAPSTONE = $(NPC_HOME)/$(LIBCAPSTONE)
+SO_PATH_CAPSTONE = $(NPC_HOME)/npc_old/$(LIBCAPSTONE)
 V_CXXFLAGS += -CFLAGS "-I$(NPC_HOME)/tools/capstone/repo/include -DSO_PATH_CAPSTONE=$(SO_PATH_CAPSTONE)"
 
 $(LIBCAPSTONE):
