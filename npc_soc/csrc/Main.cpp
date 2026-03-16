ed6d630396069e536f803db380fa49b64fabf279
tracer-ysyx <tracer@ysyx.org>
2026-03-16 13:31:52 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux DESKTOP-ESHKUM8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:31:52 up  1:36,  1 user,  load average: 3.28, 1.55, 0.83

--- npc_soc/csrc/Main.cpp
+++ npc_soc/csrc/Main.cpp
@@ -29,15 +29,14 @@ extern "C" void notify_retire(int32_t pc, int32_t inst) { retire = true; }
 extern "C" void notify_bus_read(int id, uint32_t addr, int len, int rsize,
                                 uint64_t timestamp) {
   if (config.mtracer)
-    spdlog::info("Read {:#010x}, arid = {}, arlen = {}, arsize = {}, time = {}",
-                 addr, id, len, rsize, timestamp);
+    println("Read {:#010x}, arid = {}, arlen = {}, arsize = {}, time = {}",
+            addr, id, len, rsize, timestamp);
 }
 extern "C" void notify_bus_write(int id, uint32_t addr, int len, int wsize,
                                  uint64_t timestamp) {
   if (config.mtracer)
-    spdlog::info(
-        "Write {:#010x}, arid = {}, arlen = {}, arsize = {}, time = {}", addr,
-        id, len, wsize, timestamp);
+    println("Write {:#010x}, arid = {}, arlen = {}, arsize = {}, time = {}",
+            addr, id, len, wsize, timestamp);
 }
 
 bool check_difftest(Dut &dut, Ref &ref) {
