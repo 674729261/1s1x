5bd6154f1e85d1c2f5f1c7ddd7f059d24b79839c
tracer-ysyx <tracer@ysyx.org>
2026-06-05 12:10:04 +0800

    > sim RTL ysyx_25080216 王翔宇 Linux fedora-shitful 7.0.10-201.fc44.x86_64 #1 SMP PREEMPT_DYNAMIC Wed May 27 13:57:41 UTC 2026 x86_64 GNU/Linux  12:10:04 up 18 min,  1 user,  load average: 1.61, 1.18, 0.85

--- npc/npc_soc/include/DUT.h
+++ npc/npc_soc/include/DUT.h
@@ -43,13 +43,16 @@ public:
   }
 
   void step_one_cycle() {
+    static bool start_record_wave = false;
+    if (getPC() == 0xa00008c4)
+      start_record_wave = true;
     top->clock = 0;
     top->eval();
-    if (getPC() >= 0xa0000000)
+    if (getPC() >= 0xa0000000 && start_record_wave)
       m_trace->dump(2 * sim_time);
     top->clock = 1;
     top->eval();
-    if (getPC() >= 0xa0000000)
+    if (getPC() >= 0xa0000000 && start_record_wave)
       m_trace->dump(2 * sim_time + 1);
     sim_time++;
     // println("PC : {:08x}, a5 = {:08x}", getPC(), getGPR(15));
