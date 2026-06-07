c217b261c07d4f93b616abc349ac7e746de9d883
Shitful <2292683261@qq.com>
2026-06-07 15:04:54 +0800

    修改SOC_HOME环境变量

--- npc/npc_soc/scripts/capstone.mk
+++ npc/npc_soc/scripts/capstone.mk
@@ -1,6 +1,6 @@
 LIBCAPSTONE = tools/capstone/repo/libcapstone.so.6
-SO_PATH_CAPSTONE = $(SOC_NPC_HOME)/$(LIBCAPSTONE)
-V_CXXFLAGS += -CFLAGS "-I$(SOC_NPC_HOME)/tools/capstone/repo/include -DSO_PATH_CAPSTONE=$(SO_PATH_CAPSTONE)"
+SO_PATH_CAPSTONE = $(NPC_HOME)/npc_soc/$(LIBCAPSTONE)
+V_CXXFLAGS += -CFLAGS "-I$(NPC_HOME)/npc_soc/tools/capstone/repo/include -DSO_PATH_CAPSTONE=$(SO_PATH_CAPSTONE)"
 
 $(LIBCAPSTONE):
 	$(MAKE) -C tools/capstone
