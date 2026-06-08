0afe9c7e145fa992c081c9a436a6c2ae476c87b4
Shitful <2292683261@qq.com>
2026-06-08 14:41:42 +0800

    启用nvboard

--- abstract-machine/scripts/platform/soc.mk
+++ abstract-machine/scripts/platform/soc.mk
@@ -28,7 +28,7 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME)/npc_soc sim PROG=$(IMAGE).bin NPC_FLAGS="--batch"
+	$(MAKE) -C $(NPC_HOME)/npc_soc nvboard PROG=$(IMAGE).bin NPC_FLAGS="--batch"
 
 nvboard: insert-arg
 	$(MAKE) -C $(NPC_HOME)/npc_soc nvboard PROG=$(IMAGE).bin NPC_FLAGS="--batch"
